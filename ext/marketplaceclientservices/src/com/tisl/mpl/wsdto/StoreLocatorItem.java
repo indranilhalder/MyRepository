cbffba96e4d1cd427cbd0c7621c0c10110a838b9
Jayantkumar <jayant.k@techouts.com>
2016-02-18 19:13:14 +0530

    added new fields for store request call to oms

--- ext/marketplaceclientservices/src/com/tisl/mpl/wsdto/StoreLocatorItem.java
+++ ext/marketplaceclientservices/src/com/tisl/mpl/wsdto/StoreLocatorItem.java
@@ -19,13 +19,91 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "Item")
 @XmlType(propOrder =
-{ "ussId", "storeId" })
+{ "ussId", "storeId","price","fulfillmentType","sellerID","transportMode"})
 public class StoreLocatorItem
 {
 	@XmlElement(name = "ussId")
 	private String ussId;
 	@XmlElement(name = "storeId")
 	private List<String> storeId;
+	
+	@XmlElement(name = "sellerID")
+	private String sellerID;
+	
+	@XmlElement(name = "fulfillmentType")
+	private String fulfillmentType;
+	
+	@XmlElement(name = "transportMode")
+	private String transportMode;
+	
+	@XmlElement(name = "price")
+	private double price;
+
+	
+	/**
+	 * @return the fulfillmentType
+	 */
+	public String getFulfillmentType()
+	{
+		return fulfillmentType;
+	}
+
+	/**
+	 * @param fulfillmentType the fulfillmentType to set
+	 */
+	public void setFulfillmentType(String fulfillmentType)
+	{
+		this.fulfillmentType = fulfillmentType;
+	}
+
+	/**
+	 * @return the transportMode
+	 */
+	public String getTransportMode()
+	{
+		return transportMode;
+	}
+
+	/**
+	 * @param transportMode the transportMode to set
+	 */
+	public void setTransportMode(String transportMode)
+	{
+		this.transportMode = transportMode;
+	}
+
+	
+	/**
+	 * @return the sellerID
+	 */
+	public String getSellerID()
+	{
+		return sellerID;
+	}
+
+	/**
+	 * @param sellerID the sellerID to set
+	 */
+	public void setSellerID(String sellerID)
+	{
+		this.sellerID = sellerID;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice()
+	{
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
 
 	/**
 	 * @return the ussId
