7fc1b84ba49839fd9cb4e5f8884d1240fc413995
tislgitadmin <sriram.veeraraghavan@tcs.com>
2016-02-24 14:42:00 +0530

    updated - TISEEII-656:Code Merge ISSUE - Replace file

--- ext/marketplacecheckoutaddon/acceleratoraddon/web/webroot/_ui/responsive/common/js/marketplacecheckoutaddon.js
+++ ext/marketplacecheckoutaddon/acceleratoraddon/web/webroot/_ui/responsive/common/js/marketplacecheckoutaddon.js
@@ -138,8 +138,8 @@ function displayCODForm()
 	$("#paymentMode").val("COD");
 	var paymentMode=$("#paymentMode").val();
 	$("#COD, #paymentDetails, #otpNUM, #sendOTPNumber, #sendOTPButton").css("display","block");
-	$("#enterOTP, #submitPaymentFormButton, #submitPaymentFormCODButton, .make_payment, #paymentFormButton, #otpSentMessage").css("display","block");	
-	/*$("#enterOTP, #submitPaymentFormButton, #submitPaymentFormCODButton, .make_payment, #paymentFormButton, #otpSentMessage").css("display","none");*//*modified for pprd testing*/
+	/*$("#enterOTP, #submitPaymentFormButton, #submitPaymentFormCODButton, .make_payment, #paymentFormButton, #otpSentMessage").css("display","block");*/	//Modified back as erroneously pushed by performance team
+	$("#enterOTP, #submitPaymentFormButton, #submitPaymentFormCODButton, .make_payment, #paymentFormButton, #otpSentMessage").css("display","none");/*modified for pprd testing -- changing back*/
 	//setCellNo();
 	if(codEligible=="BLACKLISTED")
 	{
