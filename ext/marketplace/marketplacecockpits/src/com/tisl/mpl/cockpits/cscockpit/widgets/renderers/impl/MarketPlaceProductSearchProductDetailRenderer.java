b64f6b2a41ffb5f6bed32ff7dfdbda506832be2b
prasad <prasad.v@Techouts.com>
2016-03-31 17:06:03 +0530

    pinCode servicibility for BUG defect "TISOMSII-97"

--- ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/cscockpit/widgets/renderers/impl/MarketPlaceProductSearchProductDetailRenderer.java
+++ ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/cscockpit/widgets/renderers/impl/MarketPlaceProductSearchProductDetailRenderer.java
@@ -700,10 +700,12 @@ public class MarketPlaceProductSearchProductDetailRenderer extends
 				deliverymodeLabel = new Label(
 						MarketplaceCockpitsConstants.delNameMap
 								.get(deliveryMode));
+				if(null != deliveryMode) {
 				if(deliveryMode.equals("CNC")) {
 					deliverymodeLabel = new Label(
 						MarketplaceCockpitsConstants.delNameMap
 						.get("CnC"));
+				}
 				}
 				deliverymodeLabel.setStyle(LABELSTYLE);
 				deliverymodeLabel.setParent(cell);
