26055bd244eea1c6618a380d2552e9854301e061
590728 <590728@01HW600568.India.TCS.com>
2016-04-05 22:22:40 +0530

    TISPRD-938 : 590728 : Query Change

--- ext/marketplacecommerceservices/src/com/tisl/mpl/promotion/dao/impl/UpdatePromotionalPriceDaoImpl.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/promotion/dao/impl/UpdatePromotionalPriceDaoImpl.java
@@ -13,6 +13,8 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
+
 import com.tisl.mpl.promotion.dao.UpdatePromotionalPriceDao;
 
 
@@ -26,35 +28,83 @@ public class UpdatePromotionalPriceDaoImpl implements UpdatePromotionalPriceDao
 	@Resource(name = "flexibleSearchService")
 	private FlexibleSearchService flexibleSearchService;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.tisl.mpl.promotion.dao.UpdatePromotionalPriceDao#fetchPricedData(java.util.List)
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger(UpdatePromotionalPriceDaoImpl.class.getName());
+
+	/**
+	 * Fetch Price Row Details for Product(Staged + Online)
+	 *
+	 * @param productList
 	 */
 	@Override
-	public List<PriceRowModel> fetchPricedData(final List<String> product)
+	public List<PriceRowModel> fetchPricedData(final List<String> productList)
 	{
+		final int oracleAlloweLimit = 1000;
+		int startIndex = 0;
+		int endIndex = oracleAlloweLimit;
 
-		// YTODO Auto-generated method stub
 		List<PriceRowModel> priceRow = null;
-		//final StringBuilder query = new StringBuilder("SELECT {pri." + PriceRowModel.PK + "} ");
-		final StringBuilder query = new StringBuilder(48);
-		query.append("SELECT {pri." + PriceRowModel.PK + "} ");
-		query.append("FROM {" + PriceRowModel._TYPECODE + " AS pri} ");
-		query.append("WHERE {pri." + PriceRowModel.PRODUCT + "} in (?" + PriceRowModel.PRODUCT + ")");
-
+		final StringBuilder query = new StringBuilder(100);
+		final String queryPart1 = "SELECT {pri." + PriceRowModel.PK + "} ";
+		final String queryPart2 = "FROM {" + PriceRowModel._TYPECODE + " AS pri} ";
+		final String queryPart3 = "WHERE {pri." + PriceRowModel.PRODUCT + "} in (?";
+		final String queryPart4 = ")";
+		final String queryHead = "SELECT * FROM (";
+		final String queryTail = "	) query";
 		final Map<String, Object> queryParams = new HashMap<String, Object>();
-		queryParams.put(PriceRowModel.PRODUCT, product);
+		int count = 0;
+
+		if (productList.size() > oracleAlloweLimit)
+		{
+			query.append(queryHead);
+			while (true)
+			{
+				if (startIndex != 0)
+				{
+					query.append(" UNION ALL ");
+				}
+				query.append("{{");
+				final List<String> subList = productList.subList(startIndex, endIndex);
+				final String paramName = "param" + (++count);
+				queryParams.put(paramName, subList);
+				query.append(queryPart1).append(queryPart2).append(queryPart3).append(paramName).append(queryPart4);
+				startIndex = endIndex;
+				if ((endIndex + oracleAlloweLimit) > productList.size())
+				{
+					endIndex = productList.size();
+				}
+				else
+				{
+					endIndex = endIndex + oracleAlloweLimit;
+				}
+
+				query.append("}}");
+
+				if (startIndex == endIndex)
+				{
+					break;
+				}
+
+				LOG.debug("Query--------------------->:" + query.toString());
+			}
+
+			query.append(queryTail);
+			LOG.debug("--------FINAL-----------Query-->:" + query.toString());
+		}
+		else
+		{
+			query.append("SELECT {priMdl." + PriceRowModel.PK + "} ");
+			query.append("FROM {" + PriceRowModel._TYPECODE + " AS priMdl} ");
+			query.append("WHERE {priMdl." + PriceRowModel.PRODUCT + "} in (?" + PriceRowModel.PRODUCT + ")");
+
+			queryParams.put(PriceRowModel.PRODUCT, productList);
+		}
+
 		final SearchResult<PriceRowModel> searchRes = flexibleSearchService.search(query.toString(), queryParams);
 		if (searchRes != null)
 		{
 			priceRow = searchRes.getResult();
 		}
-
 		return priceRow;
 	}
-
-
-
-
 }
