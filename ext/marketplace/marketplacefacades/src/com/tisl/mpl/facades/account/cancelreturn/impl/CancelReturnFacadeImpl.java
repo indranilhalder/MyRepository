59ad5feb0135237281049b48aa749453a39ca0ba
prasad1 <prasad1@Techouts>
2016-04-11 16:56:40 +0530

    TISOMSII-128
    Committed code to handle SSHIP and TSHIP Return from webSite

--- ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/account/cancelreturn/impl/CancelReturnFacadeImpl.java
+++ ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/account/cancelreturn/impl/CancelReturnFacadeImpl.java
@@ -38,6 +38,7 @@ import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 import de.hybris.platform.servicelayer.exceptions.ModelSavingException;
 import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.servicelayer.session.SessionService;
 import de.hybris.platform.servicelayer.user.UserService;
 import de.hybris.platform.store.services.BaseStoreService;
 
@@ -130,7 +131,8 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 	private ReturnService returnService;
 	@Autowired
 	private ConfigurationService configurationService;
-
+	@Autowired
+	private SessionService sessionService;
 	@Autowired
 	private MplCheckoutFacade mplCheckoutFacade;
 	private Converter<AbstractOrderEntryModel, OrderEntryData> orderEntryConverter;
@@ -691,7 +693,7 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 
 		return returnReqCreated;
 	}
-	
+
 	/**
 	 * @author Techouts
 	 * @param subOrderModel
@@ -713,7 +715,7 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 			returnRequestModel.setRMA(returnService.createRMA(returnRequestModel));
 			//TISEE-5471
 			final OrderData subOrderDetails = mplCheckoutFacade.getOrderDetailsForCode(subOrderModel.getCode());
-			final List<ReturnLogisticsResponseData> returnLogisticsRespList = checkReturnLogistics(subOrderDetails,pinCode);
+			final List<ReturnLogisticsResponseData> returnLogisticsRespList = checkReturnLogistics(subOrderDetails, pinCode);
 			if (CollectionUtils.isNotEmpty(returnLogisticsRespList))
 			{
 				for (final ReturnLogisticsResponseData response : returnLogisticsRespList)
@@ -1813,6 +1815,8 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 			final List<OrderEntryData> entries = orderDetails.getEntries();
 			final OrderModel orderModel = orderModelService.getOrder(orderDetails.getCode());
 			final List<ReturnLogistics> returnLogisticsList = new ArrayList<ReturnLogistics>();
+			String returningTransactionId;
+			returningTransactionId = sessionService.getAttribute("transactionId");
 			String transactionId = "";
 			for (final OrderEntryData eachEntry : entries)
 			{
@@ -1859,24 +1863,32 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 						}
 						if (null != orderLine.getIsReturnLogisticsAvailable())
 						{
-							returnLogRespData.setIsReturnLogisticsAvailable(orderLine.getIsReturnLogisticsAvailable());
-							if (orderLine.getIsReturnLogisticsAvailable().equalsIgnoreCase("Y"))
+
+							if (orderLine.getTransactionId().trim().equalsIgnoreCase(returningTransactionId.trim()))
 							{
-								returnLogRespData
-										.setResponseMessage(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_AVAILABLE_RESPONSE_MESSAGE);
-								returnLogRespData
-										.setResponseDescription(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_AVAILABLE_RESPONSE_DESC);
-							}
-							else
-							{
-								returnLogRespData
-										.setResponseMessage(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_NOT_AVAILABLE_RESPONSE_MESSAGE);
-								returnLogRespData
-										.setResponseDescription(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_NOT_AVAILABLE_RESPONSE_DESC);
+
+								returnLogRespData.setIsReturnLogisticsAvailable(orderLine.getIsReturnLogisticsAvailable());
+								
+								if (orderLine.getIsReturnLogisticsAvailable().equalsIgnoreCase("Y"))
+								{
+									returnLogRespData
+											.setResponseMessage(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_AVAILABLE_RESPONSE_MESSAGE);
+									returnLogRespData
+											.setResponseDescription(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_AVAILABLE_RESPONSE_DESC);
+								}
+								else
+								{
+									returnLogRespData
+											.setResponseMessage(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_NOT_AVAILABLE_RESPONSE_MESSAGE);
+									returnLogRespData
+											.setResponseDescription(MarketplacecommerceservicesConstants.REVERSE_LOGISTIC_NOT_AVAILABLE_RESPONSE_DESC);
+								}
+
+								returnLogRespDataList.add(returnLogRespData);
+								responseList.add(orderLine);
+
 							}
 
-							returnLogRespDataList.add(returnLogRespData);
-							responseList.add(orderLine);
 						}
 					}
 				}
