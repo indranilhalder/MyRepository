ce74f0109fe80068271d6e91dfae4d51c31a92d9
sandipan das <sandipan8.d@tcs.com>
2016-04-28 14:05:37 +0530

    <> :397907 : Order dto changed to xml for Queue

--- ext/marketplaceomsservices/src/com/tisl/mpl/integration/oms/order/service/impl/OrderToQueue.java
+++ ext/marketplaceomsservices/src/com/tisl/mpl/integration/oms/order/service/impl/OrderToQueue.java
@@ -3,8 +3,8 @@ package com.tisl.mpl.integration.oms.order.service.impl;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.ObjectMessage;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.apache.log4j.Logger;
 import org.springframework.jms.core.JmsTemplate;
@@ -22,6 +22,7 @@ public class OrderToQueue
 {
 	private static final Logger LOG = Logger.getLogger(OrderToQueue.class);
 	private JmsTemplate jmsTemplate;
+	private CustomOmsOrderService omsOrderService;
 
 	public void setJmsTemplate(final JmsTemplate jmsTemplate)
 	{
@@ -31,16 +32,48 @@ public class OrderToQueue
 	public void sendMessage(final Order order)
 	{
 		LOG.debug("**Sending Order***" + order.getOrderId());
+
+		//		jmsTemplate.send(new MessageCreator()
+		//		{
+		//			@Override
+		//			public Message createMessage(final Session session) throws JMSException
+		//			{
+		//				final ObjectMessage message = session.createObjectMessage();
+		//				message.setObject(order);
+		//				return message;
+		//			}
+		//		});
+
 		jmsTemplate.send(new MessageCreator()
 		{
 			@Override
 			public Message createMessage(final Session session) throws JMSException
 			{
-				final ObjectMessage message = session.createObjectMessage();
-				message.setObject(order);
+				final TextMessage message = session.createTextMessage();
+				final String orderXml = getOmsOrderService().getOrderAuditXml(order);
+				LOG.debug("OrderToQueue : orderXml" + orderXml);
+				message.setText(orderXml);
 				return message;
 			}
 		});
-
 	}
+
+
+	/**
+	 * @return the omsOrderService
+	 */
+	public CustomOmsOrderService getOmsOrderService()
+	{
+		return omsOrderService;
+	}
+
+	/**
+	 * @param omsOrderService
+	 *           the omsOrderService to set
+	 */
+	public void setOmsOrderService(final CustomOmsOrderService omsOrderService)
+	{
+		this.omsOrderService = omsOrderService;
+	}
+
 }
