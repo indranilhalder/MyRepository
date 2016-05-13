44acd6d2326a8f4082f000116441f87f67b74fae
SomdipSanyal <somdip.sanyal@tcs.com>
2016-05-13 20:24:59 +0530

    472329: Canonical tags final change

--- ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/tags/RegExFunction.java
+++ ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/tags/RegExFunction.java
@@ -35,4 +35,30 @@ public class RegExFunction
 		//}
 		return flag;
 	}
+
+	/**
+	 * @desc This function matches agaist a regular expression validationg to true or false
+	 * @param toMatch
+	 * @param expression
+	 * @return boolean
+	 */
+	public static String regExMatchAndRemove(final String toMatch, final String expression)
+	{
+		final String removedString = toMatch;
+		final Pattern p = Pattern.compile(expression);
+		final Matcher m = p.matcher(toMatch);
+		try
+		{
+			if (m.find())
+			{
+				return toMatch.substring(0, (m.end() - 1));
+			}
+		}
+		catch (final IllegalStateException e)
+		{
+			return removedString;
+		}
+
+		return removedString;
+	}
 }
