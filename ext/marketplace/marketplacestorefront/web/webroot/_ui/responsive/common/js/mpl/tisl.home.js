c6461be1e62a6e8f52a91d96f01b94c7bd333427
SumitHaldar <sumit.haldar@tcs.com>
2016-05-18 16:48:31 +0530

    <> : 752131 : url fix for offers page fore /store removal

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/tisl.home.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/tisl.home.js
@@ -532,7 +532,7 @@ function getBestPicksAjaxCall(){
 						
 				});
 			//renderHtml += "</div> <a href='/store/o/viewAllOffers' class='view-cliq-offers'> View Cliq Offers </a>";
-			renderHtml += "</div> <a href='/store/view-all-offers' class='view-cliq-offers'> View Cliq Offers </a>";
+			renderHtml += "</div> <a href='/view-all-offers' class='view-cliq-offers'> View Cliq Offers </a>";
 			$("#bestPicks").html(renderHtml);
 			// console.log()
 		},
