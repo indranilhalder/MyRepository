6eb551505b2bfc265acb33118eb06484ed578d5a
472329 <472329@01HW299254.India.TCS.com>
2016-06-13 21:44:48 +0530

    Sonar Fix for cron sync job

--- ext/marketplace/marketplacecore/src/com/tisl/mpl/core/aspect/cronjob/DefaultCronjobStatusLogger.java
+++ ext/marketplace/marketplacecore/src/com/tisl/mpl/core/aspect/cronjob/DefaultCronjobStatusLogger.java
@@ -2,68 +2,75 @@ package com.tisl.mpl.core.aspect.cronjob;
 
 import de.hybris.platform.cronjob.model.CronJobModel;
 import de.hybris.platform.servicelayer.config.ConfigurationService;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import com.tisl.mpl.core.constants.MarketplaceCoreConstants;
+
 
 /**
  * Created by 585070 on 6/11/2016.
  */
 
 @Aspect
-public class DefaultCronjobStatusLogger {
+public class DefaultCronjobStatusLogger
+{
 
-    private static final Logger LOG = Logger.getLogger(DefaultCronjobStatusLogger.class);
+	private static final Logger LOG = Logger.getLogger(DefaultCronjobStatusLogger.class);
+	private ConfigurationService configurationService;
 
-    private ConfigurationService configurationService;
+	public ConfigurationService getConfigurationService()
+	{
+		return configurationService;
+	}
 
-    public ConfigurationService getConfigurationService() {
-        return configurationService;
-    }
+	public void setConfigurationService(final ConfigurationService configurationService)
+	{
+		this.configurationService = configurationService;
+	}
 
-    public void setConfigurationService(ConfigurationService configurationService) {
-        this.configurationService = configurationService;
-    }
+	@After("target(de.hybris.platform.servicelayer.cronjob.CronJobService) && execution(* performCronJob(..)) && args(cronjob,..)")
+	public void afterPerform(final CronJobModel cronjob)
+	{
 
-    @After("target(de.hybris.platform.servicelayer.cronjob.CronJobService) && execution(* performCronJob(..)) && args(cronjob,..)")
-    public void afterPerform(final CronJobModel cronjob) {
+		if (getConfigurationService().getConfiguration().getBoolean("mpl.log.cronjob.enabled", false))
+		{
+			try
+			{
+				LOG.error(MarketplaceCoreConstants.SeperaterHashHead + "\nCode : " + cronjob.getCode() + "\nJob : "
+						+ cronjob.getJob().getCode() + "\nNode ID : " + cronjob.getNodeID() + "\nStart time : " + cronjob.getStartTime()
+						+ "\nEnd time : " + cronjob.getEndTime() + "\nResult : " + cronjob.getResult().name() + "\nStatus : "
+						+ cronjob.getStatus().name() + "\nServer IP : " + InetAddress.getLocalHost().toString()
+						+ MarketplaceCoreConstants.SeperaterHash);
+			}
+			catch (final UnknownHostException e)
+			{
+				//Never mind
+				LOG.error(MarketplaceCoreConstants.SeperaterHashHead + "\nCode : " + cronjob.getCode() + "\nJob : "
+						+ cronjob.getJob().getCode() + "\nNode ID : " + cronjob.getNodeID() + "\nStart time : " + cronjob.getStartTime()
+						+ "\nEnd time : " + cronjob.getEndTime() + "\nResult : " + cronjob.getResult().name() + "\nStatus : "
+						+ cronjob.getStatus().name() + MarketplaceCoreConstants.SeperaterHash);
+			}
+		}
 
-        if(getConfigurationService().getConfiguration().getBoolean("mpl.log.cronjob.enabled", false))
-        {
-            try {
-                LOG.error("\n################################### Cronjob Stats ##################################" +
-                        "\nCode : " + cronjob.getCode() + "\nJob : " + cronjob.getJob().getCode() +
-                        "\nNode ID : " + cronjob.getNodeID() + "\nStart time : " + cronjob.getStartTime() +
-                        "\nEnd time : " + cronjob.getEndTime() + "\nResult : " + cronjob.getResult().name() +
-                        "\nStatus : " + cronjob.getStatus().name() + "\nServer IP : " + InetAddress.getLocalHost().toString() +
-                        "\n###################################################################################");
-            } catch (UnknownHostException e) {
-                //Never mind
-                LOG.error("\n################################### Cronjob Stats ##################################" +
-                        "\nCode : " + cronjob.getCode() + "\nJob : " + cronjob.getJob().getCode() +
-                        "\nNode ID : " + cronjob.getNodeID() + "\nStart time : " + cronjob.getStartTime() +
-                        "\nEnd time : " + cronjob.getEndTime() + "\nResult : " + cronjob.getResult().name() +
-                        "\nStatus : " + cronjob.getStatus().name() +
-                        "\n###################################################################################");
-            }
-        }
+	}
 
-    }
+	@Before("target(de.hybris.platform.servicelayer.cronjob.CronJobService) && execution(* performCronJob(..)) && args(cronjob,..)")
+	public void beforePerform(final CronJobModel cronjob)
+	{
 
-    @Before("target(de.hybris.platform.servicelayer.cronjob.CronJobService) && execution(* performCronJob(..)) && args(cronjob,..)")
-    public void beforePerform(final CronJobModel cronjob) {
+		if (getConfigurationService().getConfiguration().getBoolean("mpl.log.cronjob.enabled", false))
+		{
+			LOG.error(MarketplaceCoreConstants.SeperaterHash + "\nStarting cronjob with code " + cronjob.getCode()
+					+ " running the job " + cronjob.getJob().getCode() + " at cluster node " + cronjob.getNodeID()
+					+ MarketplaceCoreConstants.SeperaterHash);
+		}
 
-        if(getConfigurationService().getConfiguration().getBoolean("mpl.log.cronjob.enabled", false))
-        {
-            LOG.error("\n###################################################################################" +
-                    "\nStarting cronjob with code " + cronjob.getCode() + " running the job "
-                    + cronjob.getJob().getCode() + " at cluster node " + cronjob.getNodeID() +
-                    "\n###################################################################################");
-        }
-
-    }
+	}
 }
