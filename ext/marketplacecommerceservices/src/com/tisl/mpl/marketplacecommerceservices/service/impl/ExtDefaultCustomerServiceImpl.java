9e18aa83445c1e8a03a64d082d055dc3b0135734
1022570 <1022570@01HW851359.India.TCS.com>
2016-06-17 20:08:11 +0530

    1022570:TISPRO-610:issue fixed

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/ExtDefaultCustomerServiceImpl.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/ExtDefaultCustomerServiceImpl.java
@@ -416,6 +416,8 @@ public class ExtDefaultCustomerServiceImpl extends DefaultCustomerAccountService
 		try
 		{
 			getModelService().save(customerModel);
+			//TISPRO-181-capturing mpl preference data to customer model during registration
+			mplPreferenceService.saveUserSpecificMplPrefDataInitially(customerModel);
 		}
 		catch (final PasswordEncoderNotFoundException ex)
 		{
