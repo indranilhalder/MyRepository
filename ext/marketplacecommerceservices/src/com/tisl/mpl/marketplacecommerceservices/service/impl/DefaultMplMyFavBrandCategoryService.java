e49e56a3411e6982ae8a66f210188b43b238370f
Masani93 <masani.soumya@tcs.com>
2016-06-28 20:42:53 +0530

    unused import removed

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/DefaultMplMyFavBrandCategoryService.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/DefaultMplMyFavBrandCategoryService.java
@@ -21,7 +21,6 @@ import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tisl.mpl.constants.MarketplacecommerceservicesConstants;
