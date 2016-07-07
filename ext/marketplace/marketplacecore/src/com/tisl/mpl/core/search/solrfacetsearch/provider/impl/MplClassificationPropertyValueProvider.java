167fce82e87893f180a85d0049c28b8606b34103
Deng, Mingming <mingming.deng@sap.com>
2016-07-07 10:11:11 +0800

    Tata-24: Dynamic display in PLP facet for the classificaation property

--- ext/marketplace/marketplacecore/src/com/tisl/mpl/core/search/solrfacetsearch/provider/impl/MplClassificationPropertyValueProvider.java
+++ ext/marketplace/marketplacecore/src/com/tisl/mpl/core/search/solrfacetsearch/provider/impl/MplClassificationPropertyValueProvider.java
@@ -4,12 +4,15 @@
 package com.tisl.mpl.core.search.solrfacetsearch.provider.impl;
 
 import de.hybris.platform.catalog.jalo.classification.ClassAttributeAssignment;
+import de.hybris.platform.catalog.jalo.classification.ClassificationAttributeValue;
 import de.hybris.platform.catalog.jalo.classification.util.Feature;
 import de.hybris.platform.catalog.jalo.classification.util.FeatureContainer;
+import de.hybris.platform.catalog.jalo.classification.util.FeatureValue;
 import de.hybris.platform.catalog.model.classification.ClassAttributeAssignmentModel;
 import de.hybris.platform.core.model.c2l.LanguageModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.jalo.product.Product;
+import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.solrfacetsearch.config.IndexConfig;
 import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
 import de.hybris.platform.solrfacetsearch.config.exceptions.FieldValueProviderException;
@@ -24,10 +27,13 @@ import java.util.Locale;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class MplClassificationPropertyValueProvider extends ClassificationPropertyValueProvider
 {
+	@Autowired
+	private ConfigurationService configurationService;
 
 	//private static final Logger LOG = Logger.getLogger(MplClassificationPropertyValueProvider.class);
 
@@ -37,6 +43,8 @@ public class MplClassificationPropertyValueProvider extends ClassificationProper
 	{
 		if (model instanceof ProductModel)
 		{
+			//Added for Tata-24 Start :::
+			final ProductModel productModel = (ProductModel) model;
 			/********** TISPRO-326 changes **********/
 			if (!"Electronics".equalsIgnoreCase(((ProductModel) model).getProductCategoryType())
 					&& StringUtils.isEmpty(indexedProperty.getClassificationProductType())
@@ -94,6 +102,24 @@ public class MplClassificationPropertyValueProvider extends ClassificationProper
 							final Feature feature = cont.getFeature(classAttributeAssignment);
 							if ((feature != null) && (!feature.isEmpty()))
 							{
+								//Added for Tata-24 Start :::
+								final String dynCategory = configurationService.getConfiguration().getString(
+										"classification.attirbutes.dynamic." + productModel.getProductCategoryType());
+
+								if (StringUtils.isNotEmpty(dynCategory))
+								{
+									final String[] dynProperties = dynCategory.split(",");
+									for (final String dynproperty : dynProperties)
+									{
+										final String property = dynproperty.replaceAll(" ", "").replaceAll("-", "").toLowerCase();
+										if (property.equals(indexedProperty.getExportId().toLowerCase()))
+										{
+											dynGroupFeaturesValues(property, feature);
+											break;
+										}
+									}
+								}
+								//Added for Tata-24 END :::
 								final List<FieldValue> temp = getFeaturesValues(indexConfig, feature, indexedProperty);
 								fieldValues.addAll(temp);
 							}
@@ -148,5 +174,77 @@ public class MplClassificationPropertyValueProvider extends ClassificationProper
 			result.addAll(extractFieldValues(indexedProperty, null, feature.getValues()));
 		}
 		return result;
+	}
+
+	/**
+	 * Method used to get local.properties configuration display for the indexed property
+	 */
+	public void dynGroupFeaturesValues(final String property, final Feature feature)
+	{
+		final List<FeatureValue> featureValues = feature.getValues();
+		final List<FeatureValue> newFeatures = new ArrayList<FeatureValue>();
+
+		//classification.attirbutes.dynamic.materialtype=Canvas,Cotton,Leather,Others,PU,Suede,Fabric,Metal,Plastic
+		final String dynGroup = configurationService.getConfiguration().getString("classification.attirbutes.dynamic." + property);
+		if (StringUtils.isNotEmpty(dynGroup))
+		{
+			boolean flag = false;
+			final String[] dynGroups = dynGroup.split(",");
+			if (dynGroups != null && dynGroups.length > 0)
+			{
+				//groupName=Canvas
+				for (final String groupName : dynGroups)
+				{
+					final String name = groupName.replaceAll(" ", "").replaceAll("-", "").toLowerCase();
+					//classification.attirbutes.dynamic.materialtype.metal=Metal,Alloys,Titanium,Aluminium,Stainless Steel
+					final String dynAttribute = configurationService.getConfiguration().getString(
+							"classification.attirbutes.dynamic." + property + "." + name);
+					if (StringUtils.isNotEmpty(dynAttribute))
+					{
+						//dynAttributes=[Metal,Alloys,Titanium,Aluminium,Stainless Steel]
+						final String[] dynAttributes = dynAttribute.split(",");
+						if (dynAttributes != null && dynAttributes.length > 0)
+						{
+							for (final String attribute : dynAttributes)
+							{
+								//att= metal || alloys || titanium || aluminium || stainlesssteel
+								final String att = attribute.replaceAll(" ", "").replaceAll("-", "").toLowerCase();
+								for (final FeatureValue featureValue : featureValues)
+								{
+									final Object value = featureValue.getValue();
+									if (value instanceof ClassificationAttributeValue)
+									{
+										final String valueName = ((ClassificationAttributeValue) value).getName();
+										if (valueName != null && att.equals(valueName.toLowerCase()))
+										{
+
+											System.out.println("loggggggg========groupName=" + groupName);
+											((ClassificationAttributeValue) value).setName(groupName);
+											featureValue.setValue(value);
+											flag = true;
+											newFeatures.add(featureValue);
+											feature.getValues().remove(featureValue);
+											break;
+										}
+
+									}
+
+								}
+								if (flag)
+								{
+									break;
+								}
+							}
+						}
+					}
+					if (flag)
+					{
+						break;
+					}
+				}
+				feature.getValues().addAll(newFeatures);
+			}
+		}
+
 	}
 }
