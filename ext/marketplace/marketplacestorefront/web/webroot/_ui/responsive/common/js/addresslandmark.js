c7c824944e573d1a6cb7d764eec1230a1e5677e0
partha <partha@techouts.com>
2016-07-07 18:13:39 +0530

    FL04 Java Script Changes

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/addresslandmark.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/addresslandmark.js
@@ -1,18 +1,16 @@
 
 $(".address_postcode").blur(function()
 		{
-			//alert("Hi ! LandMarks Populate");
+			
 			var Pincode=$(".address_postcode").val();
-	        //alert("Pincode is:  "+Pincode);
-			console.log("Ajax Call Started");
+			//console.log("Ajax Call Started");
 	        $.ajax({
 	    		url: ACC.config.encodedContextPath + "/checkout/multi/delivery-method/landmarks",
 	    		data: { 'pincode' : Pincode },
 	    		type: "POST",	
 	    		success : function(response) {
-	    			console.log("Response : "+response);
-	    			console.log(response.cityName);
-	    			alert("Hi !");
+	    			//console.log("Response : "+response);
+	    			//console.log(response.cityName);
 	        		$(".address_townCity").val(response['cityName']);
 	        		$('.address_landmarks').empty();
 	        		 $('.address_landmarks').append($("<option></option>").attr("disabled","disabled").attr("selected","selected").text("select a land mark"));
