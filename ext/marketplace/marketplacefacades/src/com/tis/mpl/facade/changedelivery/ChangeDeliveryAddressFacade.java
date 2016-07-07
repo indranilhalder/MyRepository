07c0120dace26c643485815d2375cf27145846ce
Pankaj <pankaj.s@techouts.com>
2016-07-07 13:15:32 +0530

    Added SaveTmpropry Method

--- ext/marketplace/marketplacefacades/src/com/tis/mpl/facade/changedelivery/ChangeDeliveryAddressFacade.java
+++ ext/marketplace/marketplacefacades/src/com/tis/mpl/facade/changedelivery/ChangeDeliveryAddressFacade.java
@@ -3,6 +3,7 @@
  */
 package com.tis.mpl.facade.changedelivery;
 
+import de.hybris.platform.commercefacades.user.data.AddressData;
 import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.core.model.user.AddressModel;
 
@@ -18,4 +19,21 @@ public interface ChangeDeliveryAddressFacade
 	public void createcrmTicketForChangeDeliveryAddress(OrderModel order, String customerId, String source);
 	
 	public boolean isDeliveryAddressChangable(OrderModel order);
+	
+		/**
+		 *
+		 * @param customerId
+		 * @param orderCode
+		 * @param addressData
+		 * @return String Status Msg Failure, sucess
+		 */
+		public String saveAsTemproryAddressForCustomer(String customerId, String orderCode, AddressData addressData);
+
+		/**
+	S	 *
+		 * @param customerId
+		 * @param enteredOTPNumber
+		 * @return String msg Valid or not
+		 */
+		public String validateOTP(String customerId, String enteredOTPNumber);
 }
