11823933bcede4d7490b015d4cddb1d41b89a1fb
prasad <prasad.v@Techouts>
2016-07-08 14:02:20 +0530

    delivery addres controller

--- ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/cscockpit/widgets/controllers/MarketPlaceChangeDeliveryAddressController.java
+++ ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/cscockpit/widgets/controllers/MarketPlaceChangeDeliveryAddressController.java
@@ -3,9 +3,8 @@ package com.tisl.mpl.cockpits.cscockpit.widgets.controllers;
 import de.hybris.platform.cockpit.model.meta.TypedObject;
 import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.core.model.user.AddressModel;
-import de.hybris.platform.cscockpit.widgets.controllers.OrderManagementActionsWidgetController;
 
-public interface MarketPlaceChangeDeliveryAddressController extends OrderManagementActionsWidgetController{
+public interface MarketPlaceChangeDeliveryAddressController {
 	public abstract boolean isDeliveryAddressChangable(TypedObject orderObject);
 	public abstract void ticketCreateToCrm(OrderModel Order,String customerId,String source);
 	public abstract boolean changeDeliveryAddressCallToOMS(String code,AddressModel newDeliveryAddress);
