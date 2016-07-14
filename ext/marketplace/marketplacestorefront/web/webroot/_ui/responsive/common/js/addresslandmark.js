65b93cb69cfcc4703cde2c31916861ed440beedb
partha <partha@techouts.com>
2016-07-14 19:00:55 +0530

    FL04 Editable Script Changes

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/addresslandmark.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/addresslandmark.js
@@ -1,4 +1,5 @@
-
+var addressLandMark = "";
+$(".address_landmarkOtherDiv").hide();
 $(".address_postcode").blur(function()
 		{
 			
@@ -11,14 +12,20 @@ $(".address_postcode").blur(function()
 	    		success : function(response) {
 	    			//console.log("Response : "+response);
 	    			//console.log(response.cityName);
+	    			$(".optionsLandmark, .optionsLandmark label, .optionsLandmark input,  .optionsLandmark select").show();
+	    			$(".address_landmarkOtherDiv").hide();
+	    			$(".address_landmarkOther").attr("value", "");
+	    			$(".address_landmarkOther").val("");
+	    			$('.otherOption').attr("value", "Other");
 	        		$(".address_townCity").val(response['cityName']);
 	        		$('.address_landmarks').empty();
-	        		 $('.address_landmarks').append($("<option></option>").attr("disabled","disabled").attr("selected","selected").text("select a land mark"));
+	        		 $('.address_landmarks').append($("<option></option>").attr("disabled","disabled").attr("selected","selected").text("Select a Landmark"));
 	        		//then fill it with data from json post
 	        		  $.each(response.landMarks, function(key, value) {
-	        		       $('.address_landmarks').append($("<option></option>").attr("value",value.landmarksCode)
+	        		       $('.address_landmarks').append($("<option></option>").attr("value",value.landmark)
 	        		         .text(value.landmark));
 	        		    });
+	        		  $('.address_landmarks').append($("<option class='otherOption'></option>").attr("value","Other").text("Other"));
 	        		  
 	        		  $(".address_states").val(response.state.name)
 					/*$("#state").val(response['state']);*/
@@ -29,4 +36,18 @@ $(".address_postcode").blur(function()
 		});
 });
 
-        
+//onchange="changeFunction(this.value)"
+	
+$('.address_landmarks').attr("onchange","changeFunction(this.value)");
+$(".address_landmarkOther, .address_landmarkOtherDiv label").hide();
+
+function changeFunction(value) {
+	//alert(value);
+	if(value == "Other") {
+		$(".address_landmarks").hide();
+		$('.otherOption').attr("value", "");
+		$('.otherOption').val("");
+		$(".address_landmarkOtherDiv, .address_landmarkOtherDiv label, .address_landmarkOther").show();
+		$(".optionsLandmark label").hide();
+	}
+}
