f2e189f5feef2a74791e70205043ef00fc7c2a97
Pankaj <pankaj.s@techouts.com>
2016-07-18 15:35:30 +0530

    Updated Code

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/MplChangeDeliveryAddressService.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/MplChangeDeliveryAddressService.java
@@ -3,7 +3,7 @@
  */
 package com.tisl.mpl.marketplacecommerceservices.service;
 
-import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.commercefacades.order.data.OrderData;
 
 import com.tisl.mpl.core.model.TemproryAddressModel;
 
@@ -16,30 +16,32 @@ public interface MplChangeDeliveryAddressService
 {
 
 
-public boolean isDeliveryAddressChangable(OrderModel orderModel);
+	public boolean isDeliveryAddressChangable(OrderData orderData);
 
 
-/**
- * If Old Address and  Changed address both are  different then save TemproryAddressModel
- * @param orderCode
- * @return flag true or false
- */
-public boolean saveAsTemproryAddressForCustomer( final String orderCode, final TemproryAddressModel temproryAddressModel);
+	/**
+	 * If Old Address and Changed address both are different then save TemproryAddressModel
+	 * 
+	 * @param orderCode
+	 * @return flag true or false
+	 */
+	public boolean saveAsTemproryAddressForCustomer(final String orderCode, final TemproryAddressModel temproryAddressModel);
 
-/**
- * Based on orderCode We get TemproryAddressModel And OrderModel
- * @param orderCode
- * @return flag
- */
-public boolean changeDeliveryAddress(String orderCode);
+	/**
+	 * Based on orderCode We get TemproryAddressModel And OrderModel
+	 * 
+	 * @param orderCode
+	 * @return flag
+	 */
+	public boolean changeDeliveryAddress(String orderCode);
 
 
-/**
- * 
- * @param orderCode
- * @return TemproryAddressModel
- */
-public TemproryAddressModel geTemproryAddressModel(String orderCode);
+	/**
+	 * 
+	 * @param orderCode
+	 * @return TemproryAddressModel
+	 */
+	public TemproryAddressModel geTemproryAddressModel(String orderCode);
 
-public void removeTemproryAddress(String orderCode);
+	public void removeTemproryAddress(String orderCode);
 }
