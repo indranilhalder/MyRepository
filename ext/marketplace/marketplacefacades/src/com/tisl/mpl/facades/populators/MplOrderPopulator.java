e1fbc57278d62cc2b743eb992bb146ab1407933e
Pankaj <pankaj.s@techouts.com>
2016-07-19 16:02:26 +0530

    Conevrting ListOfAddressModel To AddressDataList

--- ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/populators/MplOrderPopulator.java
+++ ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/populators/MplOrderPopulator.java
@@ -9,6 +9,7 @@ import de.hybris.platform.commercefacades.order.data.OrderData;
 import de.hybris.platform.commercefacades.order.data.OrderEntryData;
 import de.hybris.platform.commercefacades.product.data.PriceData;
 import de.hybris.platform.commercefacades.product.data.PriceDataType;
+import de.hybris.platform.commercefacades.user.data.AddressData;
 import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
 import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.order.OrderModel;
@@ -18,11 +19,13 @@ import de.hybris.platform.core.model.order.payment.DebitCardPaymentInfoModel;
 import de.hybris.platform.core.model.order.payment.EMIPaymentInfoModel;
 import de.hybris.platform.core.model.order.payment.NetbankingPaymentInfoModel;
 import de.hybris.platform.core.model.order.price.DiscountModel;
+import de.hybris.platform.core.model.user.AddressModel;
 import de.hybris.platform.promotions.model.AbstractPromotionModel;
 import de.hybris.platform.promotions.model.PromotionResultModel;
 import de.hybris.platform.util.DiscountValue;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -66,7 +69,8 @@ public class MplOrderPopulator extends AbstractOrderPopulator<OrderModel, OrderD
 		addPrincipalInformation(source, target);
 		addConvinienceCharges(source, target);
 		addVoucherDiscount(source, target);
-		addPickupPersonDetails(source,target);
+		addPickupPersonDetails(source, target);
+		addDeliverryAddressList(source, target);
 
 		if (CollectionUtils.isNotEmpty(source.getAllPromotionResults()))
 		{
@@ -313,11 +317,31 @@ public class MplOrderPopulator extends AbstractOrderPopulator<OrderModel, OrderD
 
 		target.setCouponDiscount(createPrice(source, Double.valueOf(discounts)));
 	}
-	
+
 	private void addPickupPersonDetails(final OrderModel source, final OrderData target)
 	{
 		target.setPickupName(source.getPickupPersonName());
 		target.setPickupPhoneNumber(source.getPickupPersonMobile());
+	}
+
+	private void addDeliverryAddressList(final OrderModel source, final OrderData target)
+	{
+		Assert.notNull(source, MarketplacecommerceservicesConstants.SOURCENOTNULL);
+		Assert.notNull(target, MarketplacecommerceservicesConstants.TARGETNOTNULL);
+
+		if (CollectionUtils.isNotEmpty(source.getDeliveryAddresses()))
+		{
+			final List<AddressData> addressDataList = new ArrayList<AddressData>();
+
+			for (final AddressModel addressModel : source.getDeliveryAddresses())
+			{
+				if (addressModel != null)
+				{
+					addressDataList.add(getAddressConverter().convert(addressModel));
+				}
+			}
+			target.setDeliveryAddressList(addressDataList);
+		}
 	}
 
 }
