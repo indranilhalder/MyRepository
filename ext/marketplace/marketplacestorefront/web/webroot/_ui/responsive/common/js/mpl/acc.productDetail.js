7445b240d1908e7ea96d7435c05292e597326197
SomdipSanyal <somdip.sanyal@tcs.com>
2016-07-23 19:16:21 +0530

    472329: PDP SIZEGUIDE changes

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.productDetail.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.productDetail.js
@@ -2118,6 +2118,16 @@ function buyboxDetailsForSizeGuide(productCode){
 				$("#nosellerVal").val(nosellerData);
 				dispPriceForSizeGuide(mrpPrice, mopPrice, specialPrice,savingsOnProduct);
 				//if(availableStock==0  && $(".variant-select-sizeGuidePopUp option:selected").val()!="#"){	//changes for TISPRO-338
+				if(null!= availibility){
+				$.each(availibility,function(key,value){
+				$(".variant-select-sizeGuidePopUp option").each(function(){
+					if(typeof($(this).attr("data-producturl"))!= 'undefined' && $(this).attr("data-producturl").indexOf(key)!= -1 && value == 0){
+						$(this).attr("disabled","disabled");
+						}
+				});
+				});	
+		
+				}
 				$(".variant-select-sizeGuidePopUp").trigger('click');
 				if(isOOSSizeGuide()){	//changes for TPR-465	
 				$("#outOfStockText").html("<font color='#ff1c47'>" + $('#outOfStockText').text() + "</font>");
