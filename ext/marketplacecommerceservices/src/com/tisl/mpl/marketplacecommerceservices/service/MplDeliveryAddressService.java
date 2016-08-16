43bd237e796fa525fdcaefe3c7445661ce84face
Pankaj <pankaj.s@techouts.com>
2016-08-16 15:28:57 +0530

    Updated Code

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/MplDeliveryAddressService.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/MplDeliveryAddressService.java
@@ -4,6 +4,14 @@
 package com.tisl.mpl.marketplacecommerceservices.service;
 
 
+import de.hybris.platform.core.model.order.OrderModel;
+
+import java.util.List;
+import java.util.Map;
+
+import bsh.ParseException;
+
+import com.hybris.oms.domain.changedeliveryaddress.TransactionEddDto;
 import com.tisl.mpl.core.model.TemproryAddressModel;
 
 
@@ -24,7 +32,7 @@ public interface MplDeliveryAddressService
 	 * @param orderCode
 	 * @return flag true or false
 	 */
-	public boolean saveTemporaryAddress(String orderCode,TemproryAddressModel temproryAddressModel);
+	public boolean saveTemporaryAddress(OrderModel orderModel,TemproryAddressModel temproryAddressModel);
 
 	/**
 	 * Based on orderCode We get TemproryAddressModel And OrderModel
@@ -40,7 +48,16 @@ public interface MplDeliveryAddressService
 	 * @param orderCode
 	 * @return TemproryAddressModel
 	 */
-	public TemproryAddressModel getTemporaryAddressModel(String orderCode);
+   
+   public TemproryAddressModel getTemporaryAddressModel(String orderCode);
 
-	public void removeTemporaryAddress(String orderCode);
+   public void removeTemporaryAddress(String orderCode);
+	
+   public boolean setStatusForTemporaryAddress(String orderId,boolean flag);
+   
+   public boolean updateContactDetails(TemproryAddressModel temproryAddressModel,OrderModel orderModel);
+   
+   public List<TransactionEddDto>  getScheduledDeliveryDate(OrderModel orderModel, String newPincode);
+   
+   public Map<String,Object> getDeliveryDate(List<TransactionEddDto> transactionEddDtoList);
 }
