a49267431228a7633bd607f369696c7288d5103e
pratha@techouts.com <Tech@uts1>
2016-08-18 18:52:06 +0530

    Delivery address request screen new:prabhakar.s@techouts.com

---
+++ ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/DeliveryAddressRequestWidgetController.java
@@ -0,0 +1,85 @@
+/**
+ *
+ */
+package com.techouts.backoffice.widget.controller;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.zk.ui.Component;
+
+import com.hybris.cockpitng.annotations.SocketEvent;
+import com.hybris.cockpitng.util.DefaultWidgetController;
+
+
+/**
+ * this class is used for Delivery Address request report purposes
+ *
+ * @author prabhakar
+ *
+ */
+public class DeliveryAddressRequestWidgetController extends DefaultWidgetController
+{
+
+	private static final Logger LOG = LoggerFactory.getLogger(DeliveryAddressRequestWidgetController.class);
+	private String startDate;
+	private String endDate;
+	private Date fromDate;
+	private Date toDate;
+
+	@Override
+	public void initialize(final Component comp)
+	{
+		final Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -6);
+		super.initialize(comp);
+		LOG.info("inside initialize method" + "Start Date " + cal.getTime() + "******* End Date " + new Date());
+
+		//	shipTxnInfo.setFromDate(cal.getTime());
+		//shipTxnInfo.setToDate(new Date());
+		//call method to get
+		getDeliveryAddressRequestInfo();
+
+	}
+
+	/**
+	 *
+	 * @param startendDates
+	 *
+	 */
+	@SuppressWarnings("deprecation")
+	@SocketEvent(socketId = "startendDates")
+	public void getTshipVSshipReportBySocketEvent(final String startendDates)
+	{
+		final String[] startEndArray = startendDates.trim().split(",");
+		startDate = startEndArray[0];
+		endDate = startEndArray[1];
+
+		final DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		LOG.info(" inside sockent Start Date " + startDate + "******* End Date " + endDate);
+
+		try
+		{
+			fromDate = dateFormat.parse(startDate);
+			toDate = dateFormat.parse(endDate);
+		}
+		catch (final ParseException e)
+		{
+
+			e.printStackTrace();
+		}
+
+		getDeliveryAddressRequestInfo();
+
+	}
+
+	private void getDeliveryAddressRequestInfo()
+	{
+
+	}
+}
