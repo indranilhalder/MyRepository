43579da6136b269643b8662da5cf1115a349bed4
Sonal Anand <sonal.anand@tcs.com>
2016-08-22 19:30:13 +0530

    TPR-796:Quantity upadated

--- ext/marketplacewebservices/src/com/tisl/mpl/utility/SearchSuggestUtilityMethods.java
+++ ext/marketplacewebservices/src/com/tisl/mpl/utility/SearchSuggestUtilityMethods.java
@@ -1539,7 +1539,7 @@ public class SearchSuggestUtilityMethods
 								{
 									l4Depart.setSelected(Boolean.valueOf(value.isSelected()));
 									//TPR-796
-									l4Depart.setQuauntity((int) (value.getCount()));
+									l4Depart.setQuantity((int) (value.getCount()));
 									if (value.isSelected())
 									{
 										flag = true;
@@ -1572,7 +1572,7 @@ public class SearchSuggestUtilityMethods
 							{
 								l3Depart.setSelected(Boolean.valueOf(value.isSelected()));
 								//TPR-796
-								l3Depart.setQuauntity((int) (value.getCount()));
+								l3Depart.setQuantity((int) (value.getCount()));
 								if (value.isSelected())
 								{
 									flag = true;
@@ -1608,7 +1608,7 @@ public class SearchSuggestUtilityMethods
 						{
 							l2Depart.setSelected(Boolean.valueOf(value.isSelected()));
 							//TPR-796
-							l2Depart.setQuauntity((int) (value.getCount()));
+							l2Depart.setQuantity((int) (value.getCount()));
 							if (value.isSelected())
 							{
 								flag = true;
@@ -1639,7 +1639,7 @@ public class SearchSuggestUtilityMethods
 					{
 						l1Depart.setSelected(Boolean.valueOf(value.isSelected()));
 						//TPR-796
-						l1Depart.setQuauntity((int) (value.getCount()));
+						l1Depart.setQuantity((int) (value.getCount()));
 						if (value.isSelected())
 						{
 							flag = true;
