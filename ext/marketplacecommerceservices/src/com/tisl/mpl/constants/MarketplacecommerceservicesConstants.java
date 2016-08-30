399ba9ea7feb62ffae46235d97b3fc85a0f4b98b
Bala <bala.g@techouts.com>
2016-08-30 15:19:41 +0530

    RL01:bala.g@techouts.com:added return type constant

--- ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
@@ -1604,6 +1604,7 @@ public final class MarketplacecommerceservicesConstants extends GeneratedMarketp
 	public static final String RETURN_TYPE_RSP = "RSP";
 	public static final String TICKETID_PREFIX_E = "E";
 	public static final String RETURN_METHOD_QUICKDROP = "quickdrop";
+	public static final String RETURN_TYPE = "R";
 
 	private MarketplacecommerceservicesConstants()
 	{
