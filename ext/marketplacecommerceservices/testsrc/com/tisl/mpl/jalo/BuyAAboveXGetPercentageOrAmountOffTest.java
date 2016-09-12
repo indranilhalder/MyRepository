1f557557237761992398480195868dd1c24e529d
590728 <590728@01HW600568.India.TCS.com>
2016-09-12 18:00:57 +0530

    TPR-715 : 590728 : Promotions JUnits

--- ext/marketplacecommerceservices/testsrc/com/tisl/mpl/jalo/BuyAAboveXGetPercentageOrAmountOffTest.java
+++ ext/marketplacecommerceservices/testsrc/com/tisl/mpl/jalo/BuyAAboveXGetPercentageOrAmountOffTest.java
@@ -3,147 +3,274 @@
  */
 package com.tisl.mpl.jalo;
 
-import static org.junit.Assert.assertTrue;
-
 import de.hybris.platform.catalog.CatalogVersionService;
 import de.hybris.platform.catalog.model.CatalogVersionModel;
+import de.hybris.platform.commerceservices.enums.SalesApplication;
+import de.hybris.platform.commerceservices.order.CommerceCartService;
+import de.hybris.platform.core.Registry;
 import de.hybris.platform.core.model.c2l.CurrencyModel;
 import de.hybris.platform.core.model.order.CartModel;
 import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.core.model.user.UserModel;
+import de.hybris.platform.jalo.JaloSession;
 import de.hybris.platform.order.CartService;
 import de.hybris.platform.product.ProductService;
 import de.hybris.platform.promotions.PromotionsService;
-import de.hybris.platform.promotions.jalo.PromotionsManager.AutoApplyMode;
 import de.hybris.platform.promotions.model.AbstractPromotionModel;
 import de.hybris.platform.promotions.model.PromotionGroupModel;
 import de.hybris.platform.promotions.model.PromotionPriceRowModel;
-import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
 import de.hybris.platform.servicelayer.i18n.CommonI18NService;
 import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.servicelayer.search.FlexibleSearchService;
 import de.hybris.platform.servicelayer.user.UserService;
+import de.hybris.platform.util.Config;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
-import javax.annotation.Resource;
-
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tisl.mpl.model.BuyAAboveXGetPercentageOrAmountOffModel;
+import com.tisl.mpl.servicelayer.MplServicelayerTest;
 
 
 /**
  * @author TCS
  *
  */
-public class BuyAAboveXGetPercentageOrAmountOffTest extends ServicelayerTransactionalTest
+@SuppressWarnings("deprecation")
+public class BuyAAboveXGetPercentageOrAmountOffTest extends MplServicelayerTest
 {
 
-	@Resource
-	private CatalogVersionService catalogVersionService;
-	@Resource
-	private ProductService productService;
-	@Resource
-	private UserService userService;
-	@Resource
-	private CartService cartService;
-	@Resource
-	private CommonI18NService commonI18NService;
-	@Resource
-	private ModelService modelService;
-	@Resource
-	private PromotionsService promotionsService;
 
-	private ProductModel product;
-	private PromotionGroupModel promotionGroup;
+	private static final Logger LOG = Logger.getLogger(BuyAAboveXGetPercentageOrAmountOffTest.class);
+
+	private ProductModel product1;
+	private UserModel user;
 	private CurrencyModel currency;
+	private CartModel cart;
+	private PromotionGroupModel promotionGroup;
+	private CatalogVersionModel version;
+
+	@Autowired
+	private CommerceCartService commerceCartService;
 
 	@Before
 	public void setUp() throws Exception
 	{
-		final CatalogVersionModel version = catalogVersionService.getCatalogVersion("", "");//TODO : Please enter catalogue name,Please enter version
-		catalogVersionService.addSessionCatalogVersion(version);
-		userService.setCurrentUser(userService.getUserForUID("demo"));//Add User ID
+		createDefaultUsers();
+		LOG.info("Setting Up Data");
+		MockitoAnnotations.initMocks(this);
+		this.user = Mockito.mock(UserModel.class);
+		this.currency = Mockito.mock(CurrencyModel.class);
+		this.version = Mockito.mock(CatalogVersionModel.class);
+		this.product1 = Mockito.mock(ProductModel.class);
+		this.cart = Mockito.mock(CartModel.class);
+		this.promotionGroup = Mockito.mock(PromotionGroupModel.class);
 
 
+		getCatalogData();
+		product1 = getProductData("987654341");
+		setUser("2000000018");
+		setCurrency("INR");
 
-		//TISSEC-50
-		currency = commonI18NService.getCurrency("");//TODO : Please enter currency
-		promotionGroup = createPromotionGroup("");//TODO : Please enter promotion group
-		product = productService.getProductForCode(version, "");//TODO : Please enter product code
+		promotionSetup(product1);
 	}
 
-	private PromotionGroupModel createPromotionGroup(final String name)
+	/**
+	 * @param currencyData
+	 */
+
+
+	private void setCurrency(final String currencyData)
 	{
-		final PromotionGroupModel promotionGroup = new PromotionGroupModel();
-		promotionGroup.setIdentifier(name);
-		modelService.save(promotionGroup);
+		final CommonI18NService currencysvc = (CommonI18NService) Registry.getApplicationContext().getBean("commonI18NService");
+		junit.framework.Assert.assertNotNull(currencysvc);
+		currency = currencysvc.getCurrency(currencyData);
+		junit.framework.Assert.assertNotNull(currency);
+		currencysvc.setCurrentCurrency(currency);
+	}
+
+	/**
+	 * @param userUID
+	 */
+	private void setUser(final String userUID)
+	{
+		final UserService usersvc = (UserService) Registry.getApplicationContext().getBean("userService");
+		junit.framework.Assert.assertNotNull(usersvc);
+		user = usersvc.getUserForUID(userUID);
+		junit.framework.Assert.assertNotNull(user);
+		usersvc.setCurrentUser(user);
+	}
+
+	/**
+	 * @param producCode
+	 * @return ProductModel
+	 */
+	private ProductModel getProductData(final String producCode)
+	{
+		final ProductService product = (ProductService) Registry.getApplicationContext().getBean("productService");
+		junit.framework.Assert.assertNotNull(product);
+
+		return product.getProductForCode(producCode);
+	}
+
+
+	private void getCatalogData()
+	{
+		final CatalogVersionService catalog = (CatalogVersionService) Registry.getApplicationContext().getBean(
+				"catalogVersionService");
+		junit.framework.Assert.assertNotNull(catalog);
+		version = catalog.getCatalogVersion("mplProductCatalog", "Online");
+		junit.framework.Assert.assertNotNull(version);
+		catalog.addSessionCatalogVersion(version);
+
+	}
+
+	/**
+	 * @param product
+	 * @Description : Setting up the Promotion
+	 *
+	 */
+	private void promotionSetup(final ProductModel product)
+	{
+		final List<ProductModel> productList = new ArrayList<ProductModel>();
+		final List<SalesApplication> channelList = new ArrayList<SalesApplication>();
+		final BuyAAboveXGetPercentageOrAmountOffModel dicountModel = new BuyAAboveXGetPercentageOrAmountOffModel();
+		dicountModel.setCode("ProductThreshPromotion");
+		dicountModel.setTitle("Buy above 500  10 Percent off");
+		dicountModel.setChannel(channelList);
+		dicountModel.setPriority(Integer.valueOf(999));
+		dicountModel.setDescription("Buy above 500  10 Percent off");
+		dicountModel.setPromotionGroup(getPromoGroup("mplPromoGrp"));
+		dicountModel.setThresholdTotals(createPriceRow(currency, 500));
+		if (null != product)
+		{
+			productList.add(product);
+			if (CollectionUtils.isNotEmpty(productList))
+			{
+				dicountModel.setProducts(productList);
+			}
+		}
+		dicountModel.setPercentageDiscount(Double.valueOf(10));
+		dicountModel.setPercentageOrAmount(Boolean.TRUE);
+
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
+		junit.framework.Assert.assertNotNull(modelService);
+
+		modelService.save(dicountModel);
+	}
+
+	@Test
+	public void test() throws Exception
+	{
+		LOG.info("Running Test Data ");
+		cart = getCart();
+		calculateCart(cart);
+	}
+
+	/**
+	 * @param promoGrp
+	 */
+	private PromotionGroupModel getPromoGroup(final String promoGrp)
+	{
+		final PromotionsService promoSvc = (PromotionsService) Registry.getApplicationContext().getBean("promotionsService");
+		junit.framework.Assert.assertNotNull(promoSvc.getPromotionGroup(promoGrp));
+		promotionGroup = promoSvc.getPromotionGroup(promoGrp);
+		junit.framework.Assert.assertNotNull(promotionGroup);
+
 		return promotionGroup;
+
 	}
 
-	private BuyAAboveXGetPercentageOrAmountOffModel preparePromotion(final String code, final double thresholdTotals,
-			final Double percentageDiscount, PromotionGroupModel promotionGroup, final double discountPrices)
+	/**
+	 * @return CartModel
+	 */
+	private CartModel getCart()
 	{
-		final BuyAAboveXGetPercentageOrAmountOffModel promo = modelService.create(BuyAAboveXGetPercentageOrAmountOffModel.class);
-		promo.setCode(code);
-		promo.setEnabled(Boolean.TRUE);
-		promo.setPriority(Integer.valueOf(1000));
+		final CartService cartSvc = (CartService) Registry.getApplicationContext().getBean("cartService");
+		junit.framework.Assert.assertNotNull(cartSvc);
 
-		promotionGroup = promotionsService.getPromotionGroup("");//TODO : Please enter promotion grp
-		final Collection<PromotionGroupModel> promotionGroups = new ArrayList<PromotionGroupModel>();
-		promotionGroups.add(promotionGroup);
-		promo.setPercentageOrAmount(Boolean.FALSE);
-		promo.setPercentageDiscount(percentageDiscount);
-		promo.setDiscountPrices(createPriceRow(currency, discountPrices));
-		promo.setThresholdTotals(createPriceRow(currency, thresholdTotals));
+		cart = createCart();
+		cartSvc.addNewEntry(cart, product1, 1, product1.getUnit());
 
-		modelService.save(promo);
-		return promo;
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
+		junit.framework.Assert.assertNotNull(modelService);
+		modelService.save(cart);
+
+		return cart;
 	}
 
-	private Collection<PromotionPriceRowModel> createPriceRow(final CurrencyModel currency, final double discount)
+	/**
+	 * @return CartModel
+	 */
+	private CartModel createCart()
+	{
+		final String cartModelTypeCode = Config.getString(JaloSession.CART_TYPE, "Cart");
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
+		final CartModel cartModel = modelService.create(cartModelTypeCode);
+		cartModel.setUser(user);
+		cartModel.setCurrency(currency);
+		cartModel.setCode(UUID.randomUUID().toString());
+		cartModel.setDate(new Date());
+		cartModel.setNet(Boolean.TRUE);
+
+		modelService.save(cartModel);
+		return cartModel;
+	}
+
+	private void calculateCart(final CartModel cartModel)
+	{
+		//TODO : Presently Block as Price Factory needs to be overwritten by Cart Team
+		//		final CommerceCartService comCartSvc = (CommerceCartService) Registry.getApplicationContext()
+		//				.getBean("commerceCartService");
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
+		//		junit.framework.Assert.assertNotNull(comCartSvc);
+		//		comCartSvc.recalculateCart(cartModel);
+		modelService.refresh(cartModel);
+		modelService.save(cartModel);
+		//System.out.println("Cart Total Price:" + cartModel.getTotalPrice());
+	}
+
+	private List<PromotionPriceRowModel> createPriceRow(final CurrencyModel currency, final double discount)
 	{
 		final PromotionPriceRowModel priceRow = new PromotionPriceRowModel();
 		priceRow.setCurrency(currency);
 		priceRow.setPrice(Double.valueOf(discount));
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
 		modelService.save(priceRow);
 		final List<PromotionPriceRowModel> rows = new ArrayList<PromotionPriceRowModel>();
 		rows.add(priceRow);
 		return rows;
 	}
 
-	@Test
-	public void testHighDiscountPromotion()
+	@After
+	public void tearDown()
 	{
-		final CartModel cart = cartService.getSessionCart();
-		final List<AbstractPromotionModel> promotions = new ArrayList<AbstractPromotionModel>();
-		promotions.add(preparePromotion("ProductThreshPromo", 100.00, Double.valueOf(10.00), promotionGroup, 50));
-		promotionGroup.setPromotions(promotions);
+		System.out.println("Tear Down Method");
+		final FlexibleSearchService flexibleSearchService = (FlexibleSearchService) Registry.getApplicationContext().getBean(
+				"flexibleSearchService");
+		final ModelService modelService = (ModelService) Registry.getApplicationContext().getBean("modelService");
+		junit.framework.Assert.assertNotNull(modelService);
 
-		//Adding a new Entry
-		cartService.addNewEntry(cart, product, 1, product.getUnit());
-		modelService.save(cart); //Saving the Cart
-		promotionsService.updatePromotions(Collections.singletonList(promotionGroup), cart, false, AutoApplyMode.APPLY_ALL,
-				AutoApplyMode.APPLY_ALL, new java.util.Date());
-		modelService.refresh(cart);
-		assertTrue("Price must be positive: " + cart.getTotalPrice(), cart.getTotalPrice().doubleValue() >= 0);
+		final AbstractPromotionModel promotion = (AbstractPromotionModel) flexibleSearchService
+				.search("SELECT {PK} FROM {AbstractPromotion} WHERE {code}='ProductThreshPromotion'").getResult().get(0);
+		junit.framework.Assert.assertNotNull(promotion);
+
+		//System.out.println("Promotion Code" + promotion.getCode());
+		promotion.setEnabled(Boolean.FALSE);
+		modelService.save(promotion);
 	}
 
-	@Test
-	public void testLowDiscountPromotion()
-	{
-		final CartModel cart = cartService.getSessionCart();
-		final List<AbstractPromotionModel> promotions = new ArrayList<AbstractPromotionModel>();
-		promotions.add(preparePromotion("ProductThreshPromo", 100.00, Double.valueOf(10.00), promotionGroup, 50));
-		promotionGroup.setPromotions(promotions);
-		//Adding a new Entry
-		cartService.addNewEntry(cart, product, 1, product.getUnit());
-		modelService.save(cart); //Saving the Cart
-		promotionsService.updatePromotions(Collections.singletonList(promotionGroup), cart, false, AutoApplyMode.APPLY_ALL,
-				AutoApplyMode.APPLY_ALL, new java.util.Date());
-		modelService.refresh(cart);
-	}
+
+
 }
