65d631d7d4eeabf2055bef88f0960ffff8a41d7a
592991 <592991@01HW851375.India.TCS.com>
2016-09-14 10:07:18 +0530

    592991 : TPR-1295 : issue fixed

--- ext/marketplacecoupon/src/com/tisl/mpl/interceptor/CouponSellerRestSaveInterceptor.java
+++ ext/marketplacecoupon/src/com/tisl/mpl/interceptor/CouponSellerRestSaveInterceptor.java
@@ -17,6 +17,7 @@ import javax.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.PredicateUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.google.common.collect.Lists;
 import com.tisl.mpl.constants.MarketplacecommerceservicesConstants;
@@ -31,7 +32,7 @@ import com.tisl.mpl.util.ExceptionUtil;
  * @author TCS
  *
  */
-public class CouponSellerRestSaveInterceptor implements PrepareInterceptor<SellerRestrictionModel>
+public class CouponSellerRestSaveInterceptor implements PrepareInterceptor
 {
 	/**
 	 *
@@ -47,6 +48,7 @@ public class CouponSellerRestSaveInterceptor implements PrepareInterceptor<Selle
 
 	@Resource(name = "mplSellerMaster")
 	private MplSellerMasterService mplSellerMasterService;
+	private static final Logger LOG = Logger.getLogger(CouponSellerRestSaveInterceptor.class);
 
 	/**
 	 * @Description : This Method is evaluated when seller restriction is created(changes incorporated for TPR-715)
@@ -54,38 +56,42 @@ public class CouponSellerRestSaveInterceptor implements PrepareInterceptor<Selle
 	 * @param: paramInterceptorContext
 	 */
 	@Override
-	public void onPrepare(final SellerRestrictionModel sellerModel, final InterceptorContext arg1) throws InterceptorException
+	public void onPrepare(final Object rModel, final InterceptorContext arg1) throws InterceptorException
 	{
 		try
 		{
-
-			if (null != sellerModel && StringUtils.isNotEmpty(sellerModel.getSellerCodeList()))
+			if (rModel instanceof SellerRestrictionModel)
 			{
-				final String sellerString = sellerModel.getSellerCodeList(); //List of sellers codes separated by commas
-				final Set<SellerMasterModel> newsellerList = new HashSet<SellerMasterModel>();
+				LOG.debug("seller restriction interceptor is getting invoked.........");
+				final SellerRestrictionModel sellerModel = (SellerRestrictionModel) rModel;
+				if (StringUtils.isNotEmpty(sellerModel.getSellerCodeList()))
+				{
+					final String sellerString = sellerModel.getSellerCodeList(); //List of sellers codes separated by commas
+					final Set<SellerMasterModel> newsellerList = new HashSet<SellerMasterModel>();
 
-				final StringTokenizer newSellerTokens = new StringTokenizer(sellerString,
-						MarketplacecommerceservicesConstants.CAMPAIGN_FILE_DELIMITTER);
-				//getting tokens for seller ids and checking its presence in seller master
-				while (newSellerTokens.hasMoreTokens())
-				{
-					newsellerList.add(mplSellerMasterService.getSellerMaster(newSellerTokens.nextToken().trim()));
+					final StringTokenizer newSellerTokens = new StringTokenizer(sellerString,
+							MarketplacecommerceservicesConstants.CAMPAIGN_FILE_DELIMITTER);
+					//getting tokens for seller ids and checking its presence in seller master
+					while (newSellerTokens.hasMoreTokens())
+					{
+						newsellerList.add(mplSellerMasterService.getSellerMaster(newSellerTokens.nextToken().trim()));
+					}
+					final List<SellerMasterModel> existingSellerList = Lists.newArrayList(sellerModel.getSeller());
+					if (CollectionUtils.isNotEmpty(existingSellerList))
+					{
+						newsellerList.addAll(existingSellerList);
+						//removing if any null values exist in the final seller list
+						CollectionUtils.filter(newsellerList, PredicateUtils.notNullPredicate());
+					}
+					if (CollectionUtils.isNotEmpty(sellerModel.getSeller()))
+					{
+						existingSellerList.clear();
+					}
+					sellerModel.setSeller(newsellerList);
 				}
-				final List<SellerMasterModel> existingSellerList = Lists.newArrayList(sellerModel.getSeller());
-				if (CollectionUtils.isNotEmpty(existingSellerList))
-				{
-					newsellerList.addAll(existingSellerList);
-					//removing if any null values exist in the final seller list
-					CollectionUtils.filter(newsellerList, PredicateUtils.notNullPredicate());
-				}
-				if (CollectionUtils.isNotEmpty(sellerModel.getSeller()))
-				{
-					existingSellerList.clear();
-				}
-				sellerModel.setSeller(newsellerList);
+
+				sellerModel.setSellerCodeList(BLANK);
 			}
-
-			sellerModel.setSellerCodeList(BLANK);
 		}
 		catch (final EtailNonBusinessExceptions e)
 		{
