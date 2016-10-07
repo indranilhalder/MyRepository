d8eeaa3bbb9721fadab68ae6844d0f349206e15d
pratha@techouts.com <Tech@uts1>
2016-10-07 10:10:55 +0530

    shorturlitemRenderer data class package changed

--- ext/tataomsbackoffice/backoffice/src/com/hybris/oms/tata/renderer/ShortUrlReportItemRenderer.java
+++ ext/tataomsbackoffice/backoffice/src/com/hybris/oms/tata/renderer/ShortUrlReportItemRenderer.java
@@ -8,7 +8,7 @@ import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.ListitemRenderer;
 
-import com.techouts.backoffice.ShortUrlReportData;
+import com.tis.mpl.facade.shorturl.ShortUrlReportData;
 
 
 /**
