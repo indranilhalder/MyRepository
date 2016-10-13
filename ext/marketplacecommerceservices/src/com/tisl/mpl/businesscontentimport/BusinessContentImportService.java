77e43185a23a3d9bb730f9ffea80afb4c7e2311f
Somdip Sanyal <472329@01HW851273.India.TCS.com>
2016-10-13 17:16:12 +0530

    472329:TPR-978

---
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/businesscontentimport/BusinessContentImportService.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.tisl.mpl.businesscontentimport;
+
+import de.hybris.platform.util.CSVReader;
+import de.hybris.platform.util.CSVWriter;
+
+import java.util.Map;
+
+
+/**
+ * @author TCS
+ *
+ */
+public interface BusinessContentImportService
+{
+
+	void processUpdateForContentImport(CSVReader reader, CSVWriter writer, Map<Integer, String> map, Integer errorPosition,
+			boolean headerRowIncluded);
+
+
+}
