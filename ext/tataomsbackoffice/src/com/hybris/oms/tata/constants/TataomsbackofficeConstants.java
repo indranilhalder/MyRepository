9e656715d12a77964284324e06df9eb1fe038191
pratha@techouts.com <Tech@uts1>
2016-10-27 17:10:17 +0530

    constants changed

--- ext/tataomsbackoffice/src/com/hybris/oms/tata/constants/TataomsbackofficeConstants.java
+++ ext/tataomsbackoffice/src/com/hybris/oms/tata/constants/TataomsbackofficeConstants.java
@@ -28,20 +28,18 @@ public final class TataomsbackofficeConstants extends GeneratedTataomsbackoffice
 	public static final String SCHEDULEDDELIVERY_POPUP_ITEMADD = "sdPopupSave";
 	public static final String EXPRESSDELIVERY_POPUP_ITEMADD = "edPopupSave";
 	public static final String RETURNDELIVERY_POPUP_ITEMADD = "rdPopupSave";
-
 	public static final String SCHEDULEDDELIVERY_ITEMSAVE = "saveSdTimeSlots";
 	public static final String EXPRESSDELIVERY_ITEMSAVE = "saveEdTimeSlots";
 	public static final String RETURNDELIVERY_ITEMSAVE = "saveRdTimeSlots";
 	public static final String ORDERSTATUS_NONE = "NONE";
 	public static final String ORDERSTATUS_HOTCOURI = "HOTCOURI";
 	public static final String ORDERSTATUS_ORDALLOC = "ORDALLOC";
-	public static final String ORDERSTATUS_ORDREJEC = "ORDREJEC";
 	public static final String ORDERSTATUS_ODREALOC = "ODREALOC";
 	public static final String ORDERSTATUS_PILIGENE = "PILIGENE";
 	public static final String ORDERSTATUS_PICKCONF = "PICKCONF";
 	public static final String ORDERSTATUS_SCANNED = "SCANNED";
-	public static final String REVERSE_ORDERSTATUS_REVERSEAWB = "REVERSEAWB";
-	public static final String REVERSE_ORDERSTATUS_RETURINIT = "RETURINIT";
+	public static final String REVERSE_ORDERSTATUS_REVERSEAWB = "REVRSAWB";
+	public static final String REVERSE_ORDERSTATUS_RETURINIT = "RETUINIT";
 	public static final String LPNAME_NONE = "NONE";
 
 	private TataomsbackofficeConstants()
