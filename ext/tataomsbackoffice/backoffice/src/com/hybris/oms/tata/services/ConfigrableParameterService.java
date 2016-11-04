1a9a2ec05e515818815cff8d2b5da6985d96b89e
partha@techouts.com <partha@techouts.com>
2016-11-04 21:18:54 +0530

    R2.3 new file  : partha@techouts.com <partha@techouts.com>

---
+++ ext/tataomsbackoffice/backoffice/src/com/hybris/oms/tata/services/ConfigrableParameterService.java
@@ -0,0 +1,20 @@
+package com.hybris.oms.tata.services;
+
+import java.util.List;
+
+import com.hybris.oms.tata.model.MplBUCConfigurationsModel;
+import com.hybris.oms.tata.model.MplTimeSlotsModel;
+
+
+/**
+ * @author prabhakar this is service class used for configarable Search parms
+ */
+public interface ConfigrableParameterService
+{
+
+	public List<MplTimeSlotsModel> onLoadMplTimeSlots();
+
+	public void saveMplTimeSlots(List<MplTimeSlotsModel> mplTimeSlots, String timeSlotType);
+
+	public void saveMplBUCConfigurations(MplBUCConfigurationsModel mplBucConfigurations);
+}
