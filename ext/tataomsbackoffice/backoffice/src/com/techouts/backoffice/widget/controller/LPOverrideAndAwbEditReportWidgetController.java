40f30d3874f5f65d285e596f8377654889fd6173
prabhakar <prabhakar.s@techouts.com>
2016-11-21 18:39:42 +0530

    R2.3:Download Option For Lp and Awb Edit Report Added

--- ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/LPOverrideAndAwbEditReportWidgetController.java
+++ ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/LPOverrideAndAwbEditReportWidgetController.java
@@ -6,6 +6,7 @@ package com.techouts.backoffice.widget.controller;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -14,8 +15,12 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listhead;
+import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 
 import com.hybris.cockpitng.annotations.SocketEvent;
@@ -48,7 +53,7 @@ public class LPOverrideAndAwbEditReportWidgetController extends DefaultWidgetCon
 	private Textbox txtTransactionId;
 	private Listbox typeListbox;
 	private Listbox flowTypeListbox;
-
+	private List<LPAWBEditAuditDto> lpawbAuditReport;
 
 	@Override
 	public void initialize(final Component comp)
@@ -65,13 +70,10 @@ public class LPOverrideAndAwbEditReportWidgetController extends DefaultWidgetCon
 
 	private void getLpOverrideAndAwbEditReport(final String fromDate, final String toDate)
 	{
-
 		final LPAWBUpdateTrackingInfo lpAndAwbUpdateInfo = orderLogisticsFacade.getLPAWBUpdateTrackingInfoBetweenTwoDates(fromDate,
 				toDate);
-		final List<LPAWBEditAuditDto> lpawbauditdto = lpAndAwbUpdateInfo.getLPAWBEditAudit();
-		LOG.info("lp audit" + lpawbauditdto.toString());
-
-		listBoxData.setModel(new ListModelList<LPAWBEditAuditDto>(lpawbauditdto));
+		lpawbAuditReport = lpAndAwbUpdateInfo.getLPAWBEditAudit();
+		listBoxData.setModel(new ListModelList<LPAWBEditAuditDto>(lpawbAuditReport));
 	}
 
 	/**
@@ -92,7 +94,108 @@ public class LPOverrideAndAwbEditReportWidgetController extends DefaultWidgetCon
 	@ViewEvent(componentID = "LpAndAwbReportsearch", eventName = Events.ON_CLICK)
 	public void sshipOrderSearch()
 	{
+		/*
+		final LPAWBUpdateTrackingInfo lpAndAwbUpdateInfo = orderLogisticsFacade.getLpAwbReportsData(txtOrderId.getValue(),
+				txtTransactionId.getValue(), typeListbox.getSelectedItem().getLabel(), flowTypeListbox.getSelectedItem().getLabel());
+		lpawbAuditReport = lpAndAwbUpdateInfo.getLPAWBEditAudit();
+		listBoxData.setModel(new ListModelList<LPAWBEditAuditDto>(lpawbAuditReport));*/
+	}
 
+	/**
+	 * export csv file from listview
+	 *
+	 * @throws InterruptedException
+	 */
+	@ViewEvent(componentID = "savecsv", eventName = Events.ON_CLICK)
+	public void getCsv() throws InterruptedException
+	{
+
+		exportToCsv(listBoxData, lpawbAuditReport,
+				"SnL_Perf_Report_" + startDate.replace("-", "") + "_" + endDate.replace("-", ""));
+
+	}
+
+	public static void exportToCsv(final Listbox listbox, final List<LPAWBEditAuditDto> lpawbAuditReport, final String fileName)
+			throws InterruptedException
+	{
+		final String saperator = ",";
+		StringBuffer stringBuff = null;
+		if (lpawbAuditReport == null || listbox.getItems() == null || lpawbAuditReport.isEmpty())
+		{
+			LOG.info("**************************** Lp awb tracking List Is Empty***************************");
+			Messagebox.show("List is empty", "Error", Messagebox.OK, Messagebox.ERROR);
+			return;
+		}
+		stringBuff = new StringBuffer("");
+		for (final Object head : listbox.getHeads())
+		{
+			final StringBuffer headLabel = new StringBuffer();
+			for (final Object header : ((Listhead) head).getChildren())
+			{
+				headLabel.append(((Listheader) header).getLabel().concat(saperator));
+			}
+			stringBuff.append(headLabel.append("\n").toString());
+		}
+		final Iterator<LPAWBEditAuditDto> itr = lpawbAuditReport.iterator();
+		StringBuffer cellLabel = null;
+		while (itr.hasNext())
+		{
+			cellLabel = new StringBuffer("");
+			final LPAWBEditAuditDto item = itr.next();
+			cellLabel.append(item.getOrderId().concat(saperator));
+			cellLabel.append(item.getTransactionId().concat(saperator));
+			cellLabel.append(item.getTransactionType().concat(saperator));
+			if (item.getIsSuccess())
+			{
+				cellLabel.append("true".concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("false".concat(saperator));
+			}
+			if (item.getIsReturn())
+			{
+				cellLabel.append("true".concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("false".concat(saperator));
+			}
+			if (item.getOldLogisticsID() != null)
+			{
+				cellLabel.append(item.getOldLogisticsID().concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("".concat(saperator));
+			}
+			if (item.getNewLogisticsID() != null)
+			{
+				cellLabel.append(item.getNewLogisticsID().concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("".concat(saperator));
+			}
+			if (item.getOldAWBNumber() != null)
+			{
+				cellLabel.append(item.getOldAWBNumber().concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("".concat(saperator));
+			}
+			if (item.getNewAWBNumber() != null)
+			{
+				cellLabel.append(item.getNewAWBNumber().concat(saperator));
+			}
+			else
+			{
+				cellLabel.append("".concat(saperator));
+			}
+			stringBuff.append(cellLabel.toString().concat("\n"));
+		}
+		Filedownload.save(stringBuff.toString().getBytes(), "text/plain", fileName.concat(".csv"));
 	}
 
 
