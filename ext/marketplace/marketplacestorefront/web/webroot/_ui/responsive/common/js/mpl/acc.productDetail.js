2c16aeedc85b88d72ab1eb2f2dd3dc13a66f61c9
668514 <668514@01HW363100.India.TCS.com>
2016-12-09 21:27:27 +0530

    668514:Travel&luggage Buy Now button enable and webservices change

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.productDetail.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.productDetail.js
@@ -2724,7 +2724,7 @@ function loadDefaultWishListName_SizeGuide() {
 	$(document).on('click','#buyNow .js-add-to-cart',function(event){
 		//var cartReturn = ACC.product.sendAddToBag("addToCartForm");
 		var isShowSize= $("#showSize").val();
-		 if(!$("#variant li ").hasClass("selected") && typeof($(".variantFormLabel").html())== 'undefined' && $("#ia_product_rootCategory_type").val()!='Electronics'&& $("#ia_product_rootCategory_type").val()!='Watches' && isShowSize=='true'){
+		 if(!$("#variant li ").hasClass("selected") && typeof($(".variantFormLabel").html())== 'undefined' && $("#ia_product_rootCategory_type").val()!='Electronics'&& $("#ia_product_rootCategory_type").val()!='Watches'&& $("#ia_product_rootCategory_type").val()!='TravelAndLuggage' && isShowSize=='true'){
 			$("#addToCartFormTitle").html("<font color='#ff1c47'>" + $('#selectSizeId').text() + "</font>");
 			$("#addToCartFormTitle").show();
 	 	    return false;
