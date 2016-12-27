1a1a3c676b11f51b65942cde6a7eb4fc76801496
Shankha <shankha.pramanik@tcs.com>
2016-12-27 19:18:42 +0530

    397968:TPR-3762:Pincode Service correction

---
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/MplJewelleryServiceImpl.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package com.tisl.mpl.marketplacecommerceservices.service.impl;
+
+import de.hybris.platform.core.model.JewelleryInformationModel;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.tisl.mpl.marketplacecommerceservices.daos.MplJewelleryDao;
+import com.tisl.mpl.marketplacecommerceservices.service.MplJewelleryService;
+
+
+/**
+ * @author TCS
+ *
+ */
+public class MplJewelleryServiceImpl implements MplJewelleryService
+{
+
+	@Resource(name = "mplJewelleryDao")
+	private MplJewelleryDao mplJewelleryDao;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.tisl.mpl.marketplacecommerceservices.service.MplJewelleryService#getJewelleryUssid(java.lang.String)
+	 */
+	@Override
+	public List<JewelleryInformationModel> getJewelleryUssid(final String productCode)
+	{
+
+		return mplJewelleryDao.getJewelleryUssid(productCode);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.tisl.mpl.marketplacecommerceservices.service.MplJewelleryService#getJewelleryInfoByUssid(java.lang.String)
+	 */
+	@Override
+	public List<JewelleryInformationModel> getJewelleryInfoByUssid(final String ussid)
+	{
+		// YTODO Auto-generated method stub
+		return mplJewelleryDao.getJewelleryInfoByUssid(ussid);
+	}
+
+}
