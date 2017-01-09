fee3f0fbbcd2bdb12825b9ea0464440a33258023
prabhakar <prabhakar.s@techouts.com>
2017-01-09 17:01:44 +0530

    TATA:837 Cancellation From Back office

--- ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/SShipDeliveryBreachWidgetController.java
+++ ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/SShipDeliveryBreachWidgetController.java
@@ -6,43 +6,32 @@ package com.techouts.backoffice.widget.controller;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 
 import com.hybris.cockpitng.annotations.SocketEvent;
 import com.hybris.cockpitng.annotations.ViewEvent;
 import com.hybris.cockpitng.util.DefaultWidgetController;
-import com.hybris.oms.api.ordercancel.CancelOrderFacade;
 import com.hybris.oms.api.sshiptxn.SShipTxnFacade;
-import com.hybris.oms.domain.order.cancel.CancelOrderLine;
-import com.hybris.oms.domain.order.cancel.OrderCancelRequest;
-import com.hybris.oms.domain.order.cancel.OrderCancellableCheckRequest;
-import com.hybris.oms.domain.order.cancel.OrderCancellableCheckRequests;
-import com.hybris.oms.domain.order.cancel.OrderCancellableCheckResponse;
-import com.hybris.oms.domain.order.cancel.OrderCancellableCheckResponses;
 import com.hybris.oms.domain.sshiptxninfo.dto.SShipTxnInfo;
 import com.hybris.oms.domain.sshiptxnresponseinfo.dto.SShipTxnResponseInfo;
 import com.hybris.oms.domain.sshiptxnresponseinfo.dto.SShipTxnResponseInfos;
 import com.hybris.oms.tata.renderer.SShipTransactionInfoItemRenderer;
-import com.techouts.backoffice.exception.InvalideOrderCancelException;
+import com.tisl.mpl.facades.account.cancelreturn.CancelReturnFacade;
 
 
 /**
@@ -59,8 +48,7 @@ public class SShipDeliveryBreachWidgetController extends DefaultWidgetController
 	 */
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = LoggerFactory.getLogger(SShipDeliveryBreachWidgetController.class);
-	private static final String ORDERSTATUSONE = "ORDCANCL";
-	private static final String ORDERSTATUSTWO = "CLONCANC";
+
 	@Wire
 	private Datebox startdpic;
 	@Wire
@@ -76,8 +64,8 @@ public class SShipDeliveryBreachWidgetController extends DefaultWidgetController
 	private Textbox txtSlaveId;
 	@WireVariable("sshipTxnRestClient")
 	private SShipTxnFacade sShipTxnFacade;
-	@WireVariable("orderCancellationRestClient")
-	private CancelOrderFacade cancelOrderFacade;
+	@WireVariable("cancelReturnFacade")
+	private CancelReturnFacade cancelReturnFacade;
 	private String startDate;
 	private String endDate;
 
@@ -178,109 +166,41 @@ public class SShipDeliveryBreachWidgetController extends DefaultWidgetController
 	@ViewEvent(componentID = "sshipOrderCancel", eventName = Events.ON_CLICK)
 	public void sshipOrderCancel()
 	{
+		if (listBoxData.getSelectedItem() == null || listBoxData.getSelectedItem().equals(""))
+		{
+			Messagebox.show("Please Select at least One List Item", "OrderCancel Dialog", Messagebox.OK, Messagebox.INFORMATION);
+			return;
+		}
+		final SShipTxnResponseInfo sShipTxnResponseInfo = listBoxData.getSelectedItem().getValue();
 		try
 		{
-
-			final OrderCancelRequest orderCancelRequest = new OrderCancelRequest();
-			if (listBoxData.getSelectedItem() == null || listBoxData.getSelectedItem().equals(""))
+			final boolean orderCancelStatus = cancelReturnFacade.orderCancellationFromBackoffice(sShipTxnResponseInfo.getOrderId(),
+					sShipTxnResponseInfo.getOrderLineId());
+			if (orderCancelStatus)
 			{
-				Messagebox.show("Please Select at least One List Item", "OrderCancel Dialog", Messagebox.OK, Messagebox.INFORMATION);
+				Messagebox.show("Order Cancel Success");
 				return;
 			}
-			final Set<Listitem> setOfListBox = listBoxData.getSelectedItems();
-			//check for Order Cancel ,weather is given order eligible or not
-			final List<OrderCancellableCheckRequest> checkOrdersForCancel = new ArrayList<OrderCancellableCheckRequest>();
-
-			for (final Listitem listItem : setOfListBox) // filter one ignoring for cancellable check the status orders equal to ORDCANCL ,CLONCANC
+			else
 			{
-				final SShipTxnResponseInfo sshipTxnOrder = listItem.getValue();
-				final OrderCancellableCheckRequest orderCancelCheckRequest = new OrderCancellableCheckRequest();
-				if (StringUtils.isNotEmpty(sshipTxnOrder.getOrderLineStatus())
-						&& sshipTxnOrder.getOrderLineStatus().equalsIgnoreCase(ORDERSTATUSONE)
-						|| sshipTxnOrder.getOrderLineStatus().equalsIgnoreCase(ORDERSTATUSTWO))
-				{
-					throw new InvalideOrderCancelException("Some of the Orders that are selected were already cancelled.");
-				}
-				orderCancelCheckRequest.setOrderId(sshipTxnOrder.getOrderId());
-				orderCancelCheckRequest.setTransactionId(sshipTxnOrder.getOrderLineId());
-				checkOrdersForCancel.add(orderCancelCheckRequest);
+				Messagebox.show("Order Cancel Faild");
+				return;
 			}
-			final OrderCancellableCheckRequests orderCancellableCheckRequests = new OrderCancellableCheckRequests(); //dto to send oms to check the eligible orders for cancel
-			orderCancellableCheckRequests.setOrderCancellableCheckRequests(checkOrdersForCancel);
-
-			final OrderCancellableCheckResponses orderCancellableCheckResponse = cancelOrderFacade
-					.checkOrderLineCancellable(orderCancellableCheckRequests);
-			final List<OrderCancellableCheckResponse> orderCancelResponse = orderCancellableCheckResponse
-					.getOrderCancellableCheckResponses();
-			int index = 0;
-			for (final OrderCancellableCheckResponse orderCancelCheckResponse : orderCancelResponse)
-			{
-				if (orderCancelCheckResponse.getIsCancellable() == Boolean.FALSE)
-				{
-					throw new InvalideOrderCancelException(
-							"Not Possible to Cancel This Order" + checkOrdersForCancel.get(index).getOrderId());
-				}
-				++index;
-			}
-			Messagebox.show("Are you sure to cancel the order?", "Order Cancel Dialog", Messagebox.OK | Messagebox.CANCEL,
-					Messagebox.QUESTION, new org.zkoss.zk.ui.event.EventListener()
-					{
-						public void onEvent(final Event evt) throws InterruptedException, InvalideOrderCancelException
-						{
-							if (evt.getName().equals("onOK"))
-							{
-								final List<CancelOrderLine> cancelOrderList = new ArrayList<CancelOrderLine>();
-								for (final Listitem listItem : setOfListBox)
-								{
-									final SShipTxnResponseInfo sshipTxnOrder = listItem.getValue();
-
-									if (StringUtils.isNotEmpty(sshipTxnOrder.getOrderLineStatus())
-											&& sshipTxnOrder.getOrderLineStatus().equalsIgnoreCase(ORDERSTATUSONE)
-											|| sshipTxnOrder.getOrderLineStatus().equalsIgnoreCase(ORDERSTATUSTWO))
-									{
-										throw new InvalideOrderCancelException("This order is already cancelled");
-									}
-
-									final CancelOrderLine cancelOrderLine = new CancelOrderLine();
-									cancelOrderLine.setOrderId(sshipTxnOrder.getOrderId());
-									cancelOrderLine.setTransactionId(sshipTxnOrder.getOrderLineId());
-									cancelOrderLine.setReturnCancelFlag("C");
-									cancelOrderLine.setReasonCode("05");
-									cancelOrderLine.setReturnCancelRemarks("Admin Cancelled due to seller Turn around time breach");
-									cancelOrderLine.setRequestId(
-											sshipTxnOrder.getSellerId() + sshipTxnOrder.getOrderId() + sshipTxnOrder.getOrderLineId());
-									cancelOrderList.add(cancelOrderLine);
-								}
-
-								final OrderCancelRequest orderCancelRequest = new OrderCancelRequest();
-								orderCancelRequest.setCancelOrderLine(cancelOrderList);
-								cancelOrderFacade.cancelOrderLine(orderCancelRequest);
-								Messagebox.show(" Order Cancel Success", "Order Cancellation Dialog", Messagebox.OK,
-										Messagebox.INFORMATION);
-
-							}
-							else
-							{
-								Messagebox.show("Removing Canceled", "Order Cancel Dialog", Messagebox.OK, Messagebox.INFORMATION);
-							}
-						}
-					});
 		}
 		catch (final Exception e)
 		{
-
-			Messagebox.show(e.getMessage());
+			Messagebox.show("Order Cancel Faild");
+			e.printStackTrace();
 		}
 	}
+
 
 	private void displaySshipDeiveryBreachData(final SShipTxnInfo shipTxnInfo)
 	{
 
 		final SShipTxnResponseInfos sshipTxnResponse = sShipTxnFacade.getSShipTxns(shipTxnInfo);
 		final List<SShipTxnResponseInfo> listOfSshipResponse = sshipTxnResponse.getSShipTxnResponseInfo();
-		final ListModelList<SShipTxnResponseInfo> sshipResponce = new ListModelList<SShipTxnResponseInfo>(listOfSshipResponse);
-		sshipResponce.setMultiple(Boolean.TRUE);
-		listBoxData.setModel(sshipResponce);
+		listBoxData.setModel(new ListModelList<SShipTxnResponseInfo>(listOfSshipResponse));
 		listBoxData.setItemRenderer(new SShipTransactionInfoItemRenderer());
 
 	}
