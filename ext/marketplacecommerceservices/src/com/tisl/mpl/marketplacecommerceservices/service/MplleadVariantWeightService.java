810b955e9d35bdec893bf03a34c73f35877359cf
Satya <satya.paul@tcs.com>
2017-01-18 16:33:48 +0530

    344196:TISPRO-885:Changes related to Lead weight variant import into the Jewellery Information and update of HasVariant in Sellerinformation Table.

---
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/MplleadVariantWeightService.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package com.tisl.mpl.marketplacecommerceservices.service;
+
+import java.util.Map;
+
+
+/**
+ * @author 344196
+ *
+ */
+public interface MplleadVariantWeightService
+{
+	public boolean updateSellerinfoHasvariant(final Map<Integer, String> line);
+}
