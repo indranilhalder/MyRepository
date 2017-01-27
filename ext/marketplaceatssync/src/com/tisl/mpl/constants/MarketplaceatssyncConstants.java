123b39c843b7144fb5d698e5630f371a58a56773
Bala <bala.g@techouts.com>
2017-01-27 16:53:25 +0530

    Initial commit ATSSYNC

---
+++ ext/marketplaceatssync/src/com/tisl/mpl/constants/MarketplaceatssyncConstants.java
@@ -0,0 +1,34 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *
+ */
+package com.tisl.mpl.constants;
+
+/**
+ * Global class for all Marketplaceatssync constants. You can add global constants for your extension into this class.
+ */
+public final class MarketplaceatssyncConstants extends GeneratedMarketplaceatssyncConstants
+{
+	public static final String EXTENSIONNAME = "marketplaceatssync";
+
+	public static final String ATS_KAFKA_BROKERS = "ats.kafka.brokers";
+	public static final String ATS_KAFKA_TOPIC = "ats.kafka.topic";
+	public static final String ATS_KAFKA_GROUP_ID = "ats.kafka.group.id";
+	public static final String ATS_KAFKA_ENABLE_AUTO_COMMIT = "ats.kafka.enable.auto.commit";
+	public static final String ATS_KAFKA_AUTO_COMMIT_INTERVAL_MS = "ats.kafka.auto.commit.interval.ms";
+	public static final String ATS_KAFKA_SESSION_TIMEOUT_MS = "ats.kafka.session.timeout.ms";
+	public static final String ATS_KAFKA_AUTO_OFFSET_RESTORE = "ats.kafka.auto.offset.reset";
+	public static final String ATS_KAFKA_KEY_DESERIALIZER = "ats.kafka.key.deserializer";
+	public static final String ATS_KAFKA_VALUE_DESERIALIZER = "ats.kafka.value.deserializer";
+	public static final String ATS_KAFKA_POLLING_COUNT = "ats.polling.count";
+
+}
