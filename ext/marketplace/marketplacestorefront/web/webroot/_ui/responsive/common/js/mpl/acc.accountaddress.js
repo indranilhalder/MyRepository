b2618099e5c6089a868b8cda603f626d4f31b939
1240990 <1240990@01HW733231.India.TCS.com>
2017-02-16 20:38:58 +0530

    TPR-4815:1240990: Address Issue fix

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.accountaddress.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/acc.accountaddress.js
@@ -115,7 +115,23 @@ function editAddress(addressId) {
     	   contentType : "application/json; charset=utf-8",
     	   success : function(data) {
     		   //TPR-4795 changes
-    		  var fullAddress=data.line1 +" "+ data.line2 +" "+ data.line3;
+    		  var fullAddress="";
+    		  if ((data.line2.value == null || data.line2.value == "") && (data.line3.value == null || data.line3.value == ""))
+			  {
+			   fullAddress = data.line1;
+			  }
+    		  if ((data.line2.value != null || data.line2.value != "") && (data.line3.value != null || data.line3.value != ""))
+			  {
+			   fullAddress=data.line1 +" "+ data.line2 +" "+ data.line3;
+			  }
+    		  if ((data.line2.value == null || data.line2.value == "") && (data.line3.value != null || data.line3.value != ""))
+			  {
+			  fullAddress=data.line1 +" "+ data.line3;
+			  }
+    		  if ((data.line2.value != null || data.line2.value != "") && (data.line3.value == null || data.line3.value == ""))
+			  {
+			  fullAddress=data.line1 +" "+ data.line2;
+			  }
     		   $('#addressId').val(addressId);
    				$('#firstName').val(data.firstName);
    				$('#lastName').val(data.lastName);
