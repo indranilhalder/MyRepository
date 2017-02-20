9a6bac0df2e9601a14dc4d492d7c6e9294d876c2
prabhakar <prabhakar.s@techouts.com>
2017-02-20 14:04:41 +0530

    TISRLUAT-1046:Download buttons not available for downloading reports

--- ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/ShortUrlMappingWidgetController.java
+++ ext/tataomsbackoffice/backoffice/src/com/techouts/backoffice/widget/controller/ShortUrlMappingWidgetController.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +16,11 @@ import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listhead;
+import org.zkoss.zul.Listheader;
 import org.zkoss.zul.Messagebox;
 
 import com.hybris.cockpitng.annotations.ViewEvent;
@@ -50,6 +54,7 @@ public class ShortUrlMappingWidgetController extends DefaultWidgetController
 
 	@WireVariable("shortUrlFacade")
 	private ShortUrlTrackFacade shortUrlFacade;
+	private List<ShortUrlReportData> shortUrlData;
 	final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
 	@Override
@@ -106,9 +111,59 @@ public class ShortUrlMappingWidgetController extends DefaultWidgetController
 
 	private void getShortUrlMappingInfo(final Date fromDate, final Date toDate)
 	{
-		final List<ShortUrlReportData> shortUrlData = shortUrlFacade.getShortUrlReportModels(fromDate, toDate);
+		shortUrlData = shortUrlFacade.getShortUrlReportModels(fromDate, toDate);
 		listBoxData.setModel(new ListModelList<ShortUrlReportData>(shortUrlData));
 		listBoxData.setItemRenderer(new ShortUrlReportItemRenderer());
+	}
+
+	/**
+	 * export csv file from listview
+	 *
+	 * @throws InterruptedException
+	 */
+	@ViewEvent(componentID = "savecsv", eventName = Events.ON_CLICK)
+	public void getCsv() throws InterruptedException
+	{
+
+		exportToCsv(listBoxData, shortUrlData,
+				"ShortUrlTrackingReport_" + startDateValue.getValue() + "_" + endDateValue.getValue());
+
+	}
+
+	public static void exportToCsv(final Listbox listbox, final List<ShortUrlReportData> shortUrlDataList, final String fileName)
+			throws InterruptedException
+	{
+		final String saperator = ",";
+		final StringBuffer stringBuff = new StringBuffer("");
+
+		if (shortUrlDataList == null || listbox.getItems() == null || shortUrlDataList.isEmpty())
+		{
+			LOG.info("****************************Short ur Tracking report is empty***************************");
+			Messagebox.show("List is empty", "Empty Data", Messagebox.OK, Messagebox.ERROR);
+			return;
+		}
+		for (final Object head : listbox.getHeads())
+		{
+			for (final Object header : ((Listhead) head).getChildren())
+			{
+				stringBuff.append(((Listheader) header).getLabel().concat(saperator));
+			}
+			stringBuff.append("\n");
+		}
+		shortUrlDataList.forEach(new Consumer<ShortUrlReportData>()
+		{
+			@Override
+			public void accept(final ShortUrlReportData shortUrlReportData)
+			{
+
+				stringBuff.append(shortUrlReportData.getOrderId().concat(saperator));
+				stringBuff.append(shortUrlReportData.getShortUrl().concat(saperator));
+				stringBuff.append(shortUrlReportData.getCliks() + "".concat(saperator));
+				stringBuff.append(shortUrlReportData.getLogins() + "".concat(saperator));
+				stringBuff.append("\n");
+			}
+		});
+		Filedownload.save(stringBuff.toString().getBytes(), "text/plain", fileName.concat(".csv"));
 	}
 
 }
