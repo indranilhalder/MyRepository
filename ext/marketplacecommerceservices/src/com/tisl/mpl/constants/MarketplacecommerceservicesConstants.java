dcf68f331c445c9816881bf5eb8d1459af94f765
Anindita Sarkar <anindita3.s@tcs.com>
2017-02-20 13:30:18 +0530

    Update MarketplacecommerceservicesConstants.java

--- ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
@@ -533,7 +533,7 @@ public final class MarketplacecommerceservicesConstants extends GeneratedMarketp
 	public static final String SMS_MESSAGE = "Test from TatauniStore";
 	public static final String SMS_MESSAGE_COD_OTP = "Peek-a-boo {0}! One-time password for your COD order is {1}. Please feel free to call us at {2} in case of any queries.";
 	public static final String SMS_MESSAGE_C2C_OTP = "Hi, one time password for your request is {0}. Please enter this to submit the request. Thanks!";
-	public static final String SMS_MESSAGE_PAYMENT_PENDING = "Hmmm… There seems to be a spot of bother. Please hold on.";
+	public static final String SMS_MESSAGE_PAYMENT_PENDING = "HmmmÂ… There seems to be a spot of bother. Please hold on.";
 	public static final String SMS_MESSAGE_PAYMENT_FAILED = "Uh oh. Looks like your order was declined for some reason. Please try again.";
 	public static final String SMS_MESSAGE_PAYMENT_TIMEOUT = "Oh no! Your order couldn't go through due to techincal issues. Please try again.";
 	public static final String SMS_MESSAGE_INVENTORY_RESERVATION_FAILED = "Uh oh! Looks like what you wanted isn't available right now, but it could come back soon. Please try again later";
@@ -582,7 +582,7 @@ public final class MarketplacecommerceservicesConstants extends GeneratedMarketp
 	public final static char FIELD_SEPARATOR = ',';
 
 	public static final String BUYAALONGBGETSHIPPINGFREE = "BuyAalongBgetShippingFree".intern();
-	public static final String BUYAANDBPERCENTAGEDISCOUNT = "BuyAandBPercentageDiscount".intern();
+	public static final String BUYAANDBPERCENTAGEDISCOUNT = "BuyAandBPrecentageDiscount".intern();
 	public static final String BUYAANDBGETC = "BuyAandBgetC".intern();
 	public static final String BUYABFREEPERCENTAGEDISCOUNT = "BuyABFreePrecentageDiscount".intern();
 	public static final String BOGO = "CustomProductBOGOFPromotion".intern();
