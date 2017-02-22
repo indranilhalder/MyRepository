d2a2ce512be5229660686502d022d1acabef2e74
TO-OW101 <partha@techouts.com>
2017-02-22 21:36:30 +0530

    TISRLEE-3196 UI issue in payment page

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/marketplacecheckoutaddon.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/marketplacecheckoutaddon.js
@@ -2063,6 +2063,7 @@ $("#otpMobileNUMField").focus(function(){
   
 
  $("#make_saved_cc_payment,#make_saved_cc_payment_up").click(function(){
+	 $("#cvvError").css("display", "none");
 	var password = $(".card_token").parent().parent().parent().find(".cvv").find(".cvvValdiation").val();
 	var ebsDownCheck=$("#ebsDownCheck").val();
 	var isDomestic=$(".card_token").parent().parent().parent().find('.card').find('.radio').find('.card_is_domestic').val();
