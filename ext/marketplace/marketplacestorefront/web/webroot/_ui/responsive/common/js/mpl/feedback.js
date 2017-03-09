fd5225eb848d97640bd6c811eb5009f54715e868
756699 <756699@01HW851381.India.TCS.com>
2017-03-09 12:41:20 +0530

    UI:756699:UF-69 removing click using jQuery

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/feedback.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/feedback.js
@@ -2979,6 +2979,7 @@ $(document).on("click","button[name='pinCodeButtonId']",function(){
 $(document).ajaxComplete(function(){
 	$("body.page-cartPage .cartBottomCheck button#pinCodeButtonIdsBtm").addClass("CheckAvailability");
 	$("body.page-cartPage .cart-total-block ul.checkOutBtnBtm li.checkout-button a#checkout-down-enabled.checkout-disabled").css("pointer-events","");
+	$("body.page-cartPage .cart-total-block ul.checkOutBtnBtm li.checkout-button a#checkout-down-enabled.checkout-disabled").removeAttr("onclick");
 });
 
 /* UF-68 UF-69 */
