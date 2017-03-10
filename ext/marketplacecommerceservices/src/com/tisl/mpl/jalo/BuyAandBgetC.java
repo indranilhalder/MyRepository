302f4d3da97a729529eab79c389ffd286fdfe3df
590728 <590728@01HW600568.India.TCS.com>
2017-03-10 19:21:35 +0530

    590728 : CAR-262 : Get Product OOB Call Override

--- ext/marketplacecommerceservices/src/com/tisl/mpl/jalo/BuyAandBgetC.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/jalo/BuyAandBgetC.java
@@ -18,6 +18,7 @@ import de.hybris.platform.promotions.jalo.PromotionsManager;
 import de.hybris.platform.promotions.result.PromotionEvaluationContext;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -819,6 +820,26 @@ public class BuyAandBgetC extends GeneratedBuyAandBgetC
 	protected MplPromotionHelper getMplPromotionHelper()
 	{
 		return Registry.getApplicationContext().getBean("mplPromotionHelper", MplPromotionHelper.class);
+	}
+
+	/**
+	 * Building the Hash Key for Promotion
+	 *
+	 * @param builder
+	 * @param ctx
+	 */
+	@Override
+	protected void buildDataUniqueKey(final SessionContext ctx, final StringBuilder builder)
+	{
+		builder.append(super.getClass().getSimpleName()).append('|').append(getPromotionGroup(ctx).getIdentifier(ctx)).append('|')
+				.append(getCode(ctx)).append('|').append(getPriority(ctx)).append('|').append(ctx.getLanguage().getIsocode())
+				.append('|');
+
+
+		//final Date modifyDate = ((Date) getProperty(ctx, "modifiedtime"));
+
+		final Date modifyDate = getModificationTime();
+		builder.append(modifyDate);
 	}
 
 }
