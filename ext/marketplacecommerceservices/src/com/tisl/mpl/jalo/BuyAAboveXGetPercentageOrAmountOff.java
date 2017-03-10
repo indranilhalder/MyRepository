cfbdf10ea6b59a53c1f3e5827a15897bc8eb814d
590728 <590728@01HW600568.India.TCS.com>
2017-03-10 19:17:07 +0530

    590728 : CAR-262 : OOB Get Products Call Override

--- ext/marketplacecommerceservices/src/com/tisl/mpl/jalo/BuyAAboveXGetPercentageOrAmountOff.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/jalo/BuyAAboveXGetPercentageOrAmountOff.java
@@ -24,6 +24,7 @@ import de.hybris.platform.promotions.util.Helper;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -661,5 +662,26 @@ public class BuyAAboveXGetPercentageOrAmountOff extends GeneratedBuyAAboveXGetPe
 	//	{
 	//		return excludedProductList;
 	//	}
+
+
+	/**
+	 * Building the Hash Key for Promotion
+	 *
+	 * @param builder
+	 * @param ctx
+	 */
+	@Override
+	protected void buildDataUniqueKey(final SessionContext ctx, final StringBuilder builder)
+	{
+		builder.append(super.getClass().getSimpleName()).append('|').append(getPromotionGroup(ctx).getIdentifier(ctx)).append('|')
+				.append(getCode(ctx)).append('|').append(getPriority(ctx)).append('|').append(ctx.getLanguage().getIsocode())
+				.append('|');
+
+
+		//final Date modifyDate = ((Date) getProperty(ctx, "modifiedtime"));
+
+		final Date modifyDate = getModificationTime();
+		builder.append(modifyDate);
+	}
 
 }
