9a60f437a22a49ad0d06fdbd9249208735b67be7
Soumya <masani.soumya@tcs.com>
2017-04-18 02:52:47 +0530

    Seach across sellers implemented

--- ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/services/search/DefaultMplProductSearchQueryBuilder.java
+++ ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/services/search/DefaultMplProductSearchQueryBuilder.java
@@ -14,126 +14,50 @@ AbstractCsFlexibleSearchQueryBuilder<DefaultCsTextFacetSearchCommand>
 		DefaultCsTextFacetSearchCommand command) {
 	String productText = command.getText();
 	boolean searchProductText = StringUtils.isNotEmpty(productText);
-	String brand ="'MBH11A00184','MBH13F00003'";
+	//String brand ="'MBH11A00184','MBH13F00003'";
+	String sellerID = "'500112'";
 	StringBuilder query = new StringBuilder(500);
 	
-	query.append("SELECT DISTINCT {pk}, {name}, {code} ");
-	
-	query.append("FROM {Product AS p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} ");
-	query.append(" WHERE ");
-	
-	query.append("{p.code} in");
-	query.append(" (");
-	
-		query.append("{{");
-		query.append(" select {p.code} "
-				+ "from {PcmProductVariant as p} "
-				+ "where {p.baseproduct} in ({{"
-					+ "SELECT {p.baseproduct} "
-					+ "from {PcmProductVariant as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} "
-					+ "where {p.pk} in ({{"
-						+ "select {target} "
-						+ "from {CategoryProductRelation} "
-						+ "where {source} in ({{"
-							+ "select {c.pk} "
-							+ "from {Category as c JOIN Catalogversion as cv ON {c.catalogversion}={cv.pk}} "
-							+ "where {c.code} in ("
-							+ brand
-							+ ") AND {cv.version} = 'Online'"
-						+ "}})"
-					+ "}}) AND {cv.version} = 'Online' ");
-					if (searchProductText) {
-						query.append(" "+"AND"+" ");
-						query.append("({p.name} LIKE ?productNameText OR {p.code} = ?productCodeText)");
-					}
-				query.append("}}) ");
-		
-		query.append("}}");
-	
-		query.append(")");
-		
-		query.append(" "+"AND"+" ");
-		
-		query.append("{cv.version} = 'Online'");
-		
-		query.append(" "+"OR"+" ");
-		
-		query.append("{p.code} in");
-		
-		query.append(" (");
-		
-			query.append("{{");
-			
-			query.append("SELECT {p.code} "
-					+ "from {product as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} "
-					+ "where pk in "
-						+ "({{select {target} "
-						+ "from {CategoryProductRelation} "
-						+ "where {source} in "
-							+ "({{select {c.pk} "
-							+ "from {Category as c JOIN Catalogversion as cv ON {c.catalogversion}={cv.pk}} "
-							+ "where {c.code} in ("
-							+ brand
-							+ ") AND {cv.version} = 'Online'}})"
-						+ "}}) "
-					+ "AND {cv.version} = 'Online' ");
+	query.append("SELECT {p.pk}, {p.name}, {p.code} , {si.ussid}"
+	+"from {product as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk} join SellerInformation as si on {si.productsource}={p.pk}}" 
+	+"where {si.SellerID} = "+sellerID+" AND {p.pk} in ({{"
+		+"select {p.pk}" 
+		+"from {PcmProductVariant as p} "
+		+"where {p.baseproduct} in ({{"
+			+"SELECT {p.baseproduct}" 
+			+"from {PcmProductVariant as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} "
+			+"where {cv.version} = 'Online'");
 			if (searchProductText) {
-				query.append(" "+"AND"+" ");
-				query.append("({p.name} LIKE ?productNameText OR {p.code} = ?productCodeText)");
+				query.append(" AND {p.code} =?productCodeText");
 			}
-			
-			query.append("}}");
-			
-			query.append(")");
-			
-			query.append(" "+"AND"+" ");
-			
-			query.append("{cv.version} = 'Online'");
-			
-			query.append(" "+"OR"+" ");
-			
-			query.append("{p.code} in");
-			
-			query.append(" (");
-			
-				query.append("{{");
-				
-				query.append("SELECT {p.code} "
-						+ "from {product as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} "
-						+ "where pk in ({{"
-							+ "select {si.productsource} "
-							+ "from {SellerInformation as si JOIN Catalogversion as cv ON {si.catalogversion}={cv.pk}} "
-							+ "where {cv.version} = 'Online' ");
-							if (searchProductText) {
-								query.append("AND {SellerSKU} = ?SKUText ");
-							}
-					query.append("AND {si.productsource} in ({{"
-								+ "select {target} "
-								+ "from {CategoryProductRelation} where {source} in ({{"
-									+ "select {c.pk} "
-									+ "from {Category as c JOIN Catalogversion as cv ON {c.catalogversion}={cv.pk}} "
-									+ "where {c.code} in ("
-									+ brand
-									+ ") AND {cv.version} = 'Online'"
-								+ "}})"
-							+ "}})"
-						+ "}}) AND {cv.version} = 'Online'");
-				query.append("}}");
-				
-				query.append(")");
-				
-				query.append(" "+"AND"+" ");
-				
-				query.append("{cv.version} = 'Online'");
-			
-	query.append(" ORDER BY {name}, {code} ASC ");
+		query.append("}})"
+	+"}})"
+	+"OR {si.SellerID} = "+sellerID+" AND {p.pk} in ({{"
+		+"SELECT {p.pk}" 
+		+"from {product as p JOIN Catalogversion as cv ON {p.catalogversion}={cv.pk}} "
+		+"where {cv.version} = 'Online'");
+		if (searchProductText) {
+			query.append(" AND {p.code} =?productCodeText");
+		}
+	query.append("}})"
+	+"OR {p.pk} in ({{"
+		+"select {si.productsource}" 
+		+"from {SellerInformation as si JOIN Catalogversion as cv ON {si.catalogversion}={cv.pk}}"
+		+"where {cv.version} = 'Online'");
+		if (searchProductText) {
+			query.append(" AND {SellerSKU} = ?SKUText");
+		}
+		query.append( " AND {SellerID} = "+sellerID
+	+"}})");
+	
+	query.append(" ORDER BY {p.code}, {p.name}  ASC ");
 	
 	FlexibleSearchQuery searchQuery = new FlexibleSearchQuery(
 			query.toString());
 	
 	if (searchProductText) {
-		searchQuery.addQueryParameter("productNameText",
-				"%" + productText.trim() + "%");
+		/*searchQuery.addQueryParameter("productNameText",
+				"%" + productText.trim() + "%");*/
 		searchQuery.addQueryParameter("productCodeText",
 				productText.trim());
 		searchQuery.addQueryParameter("SKUText",
