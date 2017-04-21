a15bd99191fa80be7314eadf9485a06df385d0ee
Tarshit Roy Chowdhury <tarshit.rchowdhury@tcs.com>
2017-04-21 18:50:42 +0530

    1121427:TPR-4461:Payment Method and Bank Specific Restriction for Voucher

--- ext/marketplacecheckoutaddon/acceleratoraddon/web/src/com/tisl/mpl/controllers/pages/PaymentMethodCheckoutStepController.java
+++ ext/marketplacecheckoutaddon/acceleratoraddon/web/src/com/tisl/mpl/controllers/pages/PaymentMethodCheckoutStepController.java
@@ -3330,6 +3330,9 @@ public class PaymentMethodCheckoutStepController extends AbstractCheckoutStepCon
 				//binData = getBinFacade().binCheck(binNumber);
 				//Added For TPR-1035
 				binData = getBinFacade().binCheck(binNumber, cardType, mplCustomerID, true);
+				//Added for TPR-4461 starts here
+				setBankNameUserPaymentMode(binData.getBankName());
+				//TPR-4461 ends here
 			}
 
 		}
@@ -4633,7 +4636,7 @@ public class PaymentMethodCheckoutStepController extends AbstractCheckoutStepCon
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see com.tisl.mpl.controllers.pages.CheckoutStepController#enterStep(org.springframework.ui.Model,
 	 * org.springframework.web.servlet.mvc.support.RedirectAttributes)
 	 */
