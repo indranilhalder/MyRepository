53add176a68dd53df23f0566275406d99d69df86
1209718 <1209718@01HW536773.India.TCS.com>
2017-04-25 17:10:07 +0530

    TISSQAEE-302:682160:Facing error in running "mplThirdPartyWalletCronjob" in COM-HMC

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/MplThirdPartyWalletServiceImpl.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/MplThirdPartyWalletServiceImpl.java
@@ -262,16 +262,21 @@ public class MplThirdPartyWalletServiceImpl implements MplThirdPartyWalletServic
 
 					LOG.debug("###################auditModelData  ############" + auditModelData);
 
-					//For CAR:127
 
-					LOG.debug("###################For CAR:127 Start of Using order Converter############");
-					final OrderData orderData = getOrderConverter().convert(order);
-					LOG.debug("###################For CAR:127 Using order Converter finished############" + "OrderData is"
-							+ orderData.getCode());
-                                         LOG.debug("###################auditModelData  ############" + auditModelData);
 
 					if (auditModelData != null)
 					{
+
+						//For CAR:127
+
+						LOG.debug("###################For CAR:127 Start of Using order Converter############");
+						final OrderData orderData = getOrderConverter().convert(order);
+						if (null != orderData && null != orderData.getCode())
+						{
+							LOG.debug("###################For CAR:127 Using order Converter finished############" + "OrderData is"
+									+ orderData.getCode());
+						}
+
 						final List<MplPaymentAuditEntryModel> entryList = Lists.newArrayList(auditModelData.getAuditEntries());
 						String status = "";
 						if (CollectionUtils.isNotEmpty(entryList) && OrderStatus.PAYMENT_PENDING.equals(order.getStatus())
