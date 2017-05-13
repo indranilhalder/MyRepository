aa0c8aaca0cf1f490fc4d0294d6f5a691b91fa91
756699 <756699@01HW851381.India.TCS.com>
2017-05-13 16:05:35 +0530

    756699:TISPRDT-1179:Applied filter black box gets hiding in SERP/PLP for tablet view issue fixed

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/feedback.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/feedback.js
@@ -1925,12 +1925,19 @@ $(document).ready(function(){
 					else{
 						var pagination_top= sort_top - 16;
 					}
+					
+					/*TISPRDT-1179*/
+					if($("body").hasClass("page-search")){
 					if($(".listing.wrapper .right-block .listing-menu > div.list_title_sort").css("display") == "block"){
 					$(".listing.wrapper .right-block .sort_by_wrapper.listing-menu").css("top",sort_top+"px");
 					}
 					else{
 						$(".listing.wrapper .right-block .sort_by_wrapper.listing-menu").css("top","auto");
 					}
+					}
+					$("body.page-search .listing.wrapper .right-block .listing-menu").css("display","block");
+					/*TISPRDT-1179*/
+					
 					$(".listing.wrapper .right-block .listing-menu > div .pagination.mobile.tablet-pagination").css("top",pagination_top+"px");
 				}
 			}
