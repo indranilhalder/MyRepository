17165d276f0511016c670d4f1eee1cfc6f61b012
Vishal <vishal.parmar@borngroup.com>
2017-05-17 14:47:49 +0530

    Added luxurystoreaddon

---
+++ ext/luxury/luxurystoreaddon/gensrc/com/tisl/lux/constants/GeneratedLuxurystoreaddonConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 15, 2017 3:10:45 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.tisl.lux.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedLuxurystoreaddonConstants
+{
+	public static final String EXTENSIONNAME = "luxurystoreaddon";
+	
+	protected GeneratedLuxurystoreaddonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
