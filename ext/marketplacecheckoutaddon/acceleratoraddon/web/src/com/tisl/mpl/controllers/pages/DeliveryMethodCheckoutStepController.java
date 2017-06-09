3d97d706d40816b888ed04ebdd42b6ce9dba4555
1022570 <1022570@01HW587462.India.TCS.com>
2017-06-09 19:31:34 +0530

    1022570:TISSQAEE-1121: code fix

--- ext/marketplacecheckoutaddon/acceleratoraddon/web/src/com/tisl/mpl/controllers/pages/DeliveryMethodCheckoutStepController.java
+++ ext/marketplacecheckoutaddon/acceleratoraddon/web/src/com/tisl/mpl/controllers/pages/DeliveryMethodCheckoutStepController.java
@@ -432,8 +432,12 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 
 			//prdi-36/INC144315559
 			String deliveryCode = null;
+			final int deliveryModeCount = deliveryMethodForm.getDeliveryMethodEntry().size();
+			int deliveryModeMatch = 0;
 			if (deliveryMethodForm.getDeliveryMethodEntry() != null && !deliveryMethodForm.getDeliveryMethodEntry().isEmpty())
 			{
+
+
 				for (final DeliveryMethodEntry deliveryEntry : deliveryMethodForm.getDeliveryMethodEntry())
 				{
 					deliveryCode = deliveryEntry.getDeliveryCode();
@@ -441,12 +445,14 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 					if (StringUtils.isNotEmpty(deliveryCode)
 							&& deliveryCode.equalsIgnoreCase(MarketplacecheckoutaddonConstants.CLICK_N_COLLECT)) // Code optimized as part of performance fix TISPT-104
 					{
-
-						return MarketplacecommerceservicesConstants.REDIRECT + "/checkout/multi/delivery-method"
-								+ MarketplacecheckoutaddonConstants.MPLDELIVERYCHOOSEURL;
+						deliveryModeMatch++;
 					}
-
-
+				}
+				//TISSQAEE-1121
+				if (deliveryModeMatch == deliveryModeCount)
+				{
+					return MarketplacecommerceservicesConstants.REDIRECT + "/checkout/multi/delivery-method"
+							+ MarketplacecheckoutaddonConstants.MPLDELIVERYCHOOSEURL;
 				}
 			}
 			// end prdi-36/INC144315559
@@ -1842,7 +1848,8 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 					if (productModel != null && productModel.getRichAttribute() != null)
 					{
 						productRichAttributeModel = (List<RichAttributeModel>) productModel.getRichAttribute();
-						if (productRichAttributeModel != null && !productRichAttributeModel.isEmpty() && productRichAttributeModel.get(0).getScheduledDelivery() != null)
+						if (productRichAttributeModel != null && !productRichAttributeModel.isEmpty()
+								&& productRichAttributeModel.get(0).getScheduledDelivery() != null)
 						{
 							productRichAttr = productRichAttributeModel.get(0).getScheduledDelivery().toString();
 						}
@@ -3048,19 +3055,19 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 	/*
 	 * private List<PincodeServiceData> populatePinCodeServiceData(final String productCode, final GPS gps, final Double
 	 * configurableRadius) {
-	 *
+	 * 
 	 * final List<PincodeServiceData> requestData = new ArrayList<PincodeServiceData>(); PincodeServiceData data = null;
 	 * MarketplaceDeliveryModeData deliveryModeData = null; try { final ProductModel productModel =
 	 * productService.getProductForCode(productCode); final ProductData productData =
 	 * productFacade.getProductForOptions(productModel, Arrays.asList(ProductOption.BASIC, ProductOption.SELLER,
 	 * ProductOption.PRICE));
-	 *
+	 * 
 	 * if (productData != null)
-	 *
+	 * 
 	 * { for (final SellerInformationData seller : productData.getSeller())
-	 *
-	 *
-	 *
+	 * 
+	 * 
+	 * 
 	 * { final List<MarketplaceDeliveryModeData> deliveryModeList = new ArrayList<MarketplaceDeliveryModeData>(); data =
 	 * new PincodeServiceData(); if ((null != seller.getDeliveryModes()) && !(seller.getDeliveryModes().isEmpty())) { for
 	 * (final MarketplaceDeliveryModeData deliveryMode : seller.getDeliveryModes()) { deliveryModeData =
@@ -3078,10 +3085,10 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 	 * Double(seller.getMrpPrice().getValue().doubleValue())); } else { LOG.debug("No price avaiable for seller :" +
 	 * seller.getSellerID()); continue; } if (null != seller.getIsCod() && StringUtils.isNotEmpty(seller.getIsCod())) {
 	 * data.setIsCOD(seller.getIsCod()); }
-	 *
-	 *
+	 * 
+	 * 
 	 * LOG.debug("Current locations for Seller Id**********" + seller.getSellerID());
-	 *
+	 * 
 	 * @SuppressWarnings("boxing") final List<Location> storeList = pincodeServiceFacade.getSortedLocationsNearby(gps,
 	 * configurableRadius.doubleValue(), seller.getSellerID()); // Code optimized as part of performance fix TISPT-104
 	 * LOG.debug("StoreList size is :" + storeList.size()); if (CollectionUtils.isNotEmpty(storeList)) { final
@@ -3091,20 +3098,20 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 	 * seller.getSellerID()); LOG.debug("seller.getUssid():" + seller.getUssid()); LOG.debug("seller.getUssid():" +
 	 * seller.getUssid()); data.setUssid(seller.getUssid());
 	 * data.setIsDeliveryDateRequired(ControllerConstants.Views.Fragments.Product.N); requestData.add(data);
-	 *
-	 *
+	 * 
+	 * 
 	 * }
-	 *
-	 *
-	 *
-	 *
-	 *
-	 *
-	 *
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
 	 * } } catch (final EtailBusinessExceptions e) { ExceptionUtil.etailBusinessExceptionHandler(e, null); }
-	 *
+	 * 
 	 * catch (final Exception e) {
-	 *
+	 * 
 	 * throw new EtailNonBusinessExceptions(e, MarketplacecommerceservicesConstants.E0000); } return requestData; }
 	 */
 
@@ -3118,7 +3125,7 @@ public class DeliveryMethodCheckoutStepController extends AbstractCheckoutStepCo
 	 * final MarketplaceDeliveryModeData deliveryModeData = new MarketplaceDeliveryModeData(); final
 	 * MplZoneDeliveryModeValueModel mplZoneDeliveryModeValueModel = mplCheckoutFacade
 	 * .populateDeliveryCostForUSSIDAndDeliveryMode(deliveryMode, MarketplaceFacadesConstants.INR, ussid);
-	 *
+	 * 
 	 * final PriceData priceData = productDetailsHelper.formPriceData(mplZoneDeliveryModeValueModel.getValue());
 	 * deliveryModeData.setCode(mplZoneDeliveryModeValueModel.getDeliveryMode().getCode());
 	 * deliveryModeData.setDescription(mplZoneDeliveryModeValueModel.getDeliveryMode().getDescription());
