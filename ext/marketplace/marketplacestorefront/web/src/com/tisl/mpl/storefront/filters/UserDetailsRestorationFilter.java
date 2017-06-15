10407694f2609a4bbdcdc9edd5cb32fc2bc1012c
168108 <168108@01HW1057068.India.TCS.com>
2017-06-16 00:45:59 +0530

    168108:UF-93:RememberMe

--- ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/filters/UserDetailsRestorationFilter.java
+++ ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/filters/UserDetailsRestorationFilter.java
@@ -262,6 +262,7 @@ public class UserDetailsRestorationFilter extends OncePerRequestFilter
 		final Cookie[] cookies = request.getCookies();
 		if (cookies != null)
 		{
+			String domain = "";
 			for (final Cookie cookie : cookies)
 			{
 				if (cookie.getName().equals("keepAlive"))
@@ -270,7 +271,11 @@ public class UserDetailsRestorationFilter extends OncePerRequestFilter
 					LOG.info("Cookie domain :::" + cookie.getDomain());
 					cookie.setMaxAge(sessionTimeoutvalue);
 					cookie.setPath("/");
-					//cookie.setSecure(true);
+					domain = getConfigurationService().getConfiguration().getString("shared.cookies.domain");
+					if (null != domain && !domain.equalsIgnoreCase("localhost"))
+					{
+						cookie.setSecure(true);
+					}
 					cookie.setDomain(getConfigurationService().getConfiguration().getString("shared.cookies.domain"));
 					response.addCookie(cookie);
 					request.getSession().setMaxInactiveInterval(sessionTimeoutvalue); // UF-93 Added to set the session to same interval as KeepAlive Cookie
