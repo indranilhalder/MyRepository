18b15437eaa72b0affa00fe1a504246dbfd86406
168108 <168108@01HW1057068.India.TCS.com>
2017-06-18 20:04:25 +0530

    168108:TPR-1348:AutoRefund

---
+++ ext/marketplace/marketplacefulfilmentprocess/src/com/tisl/mpl/fulfilmentprocess/actions/order/AutoRefundInitiateAction.java
@@ -0,0 +1,305 @@
+/**
+ *
+ */
+package com.tisl.mpl.fulfilmentprocess.actions.order;
+
+import de.hybris.platform.basecommerce.enums.ConsignmentStatus;
+import de.hybris.platform.basecommerce.enums.ReturnStatus;
+import de.hybris.platform.core.model.order.OrderEntryModel;
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.orderhistory.model.OrderHistoryEntryModel;
+import de.hybris.platform.orderprocessing.model.OrderProcessModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentModel;
+import de.hybris.platform.payment.model.PaymentTransactionEntryModel;
+import de.hybris.platform.payment.model.PaymentTransactionModel;
+import de.hybris.platform.processengine.action.AbstractProceduralAction;
+import de.hybris.platform.returns.model.RefundEntryModel;
+import de.hybris.platform.returns.model.ReturnEntryModel;
+import de.hybris.platform.returns.model.ReturnRequestModel;
+import de.hybris.platform.servicelayer.model.ModelService;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tisl.mpl.marketplacecommerceservices.service.MplNotificationService;
+import com.tisl.mpl.marketplacecommerceservices.service.MplPaymentService;
+
+
+/**
+ * @author TCS
+ *
+ */
+public class AutoRefundInitiateAction extends AbstractProceduralAction<OrderProcessModel>
+{
+	private static final Logger LOG = Logger.getLogger(AutoRefundInitiateAction.class);
+
+	private List<OrderEntryModel> refundList;
+	private List<ReturnEntryModel> returnList;
+
+	@Autowired
+	private ModelService modelService;
+
+	@Autowired
+	private MplPaymentService mplPaymentService;
+
+	@Autowired
+	private MplNotificationService mplNotificationSaveService;
+
+
+	@Override
+	public void executeAction(final OrderProcessModel process)
+	{
+		LOG.error("Inside AutoRefundInitiateAction");
+
+		refundList = Collections.synchronizedList(new ArrayList<OrderEntryModel>());
+		returnList = Collections.synchronizedList(new ArrayList<ReturnEntryModel>());
+
+		final OrderModel orderModel = process.getOrder();
+		LOG.error("Inside AutoRefundInitiateAction for Order #" + orderModel.getCode());
+		if (orderModel != null && !isOrderCOD(orderModel))
+		{
+			final List<ReturnRequestModel> returnRequestList = orderModel.getReturnRequests();
+			if (CollectionUtils.isNotEmpty(returnRequestList))
+			{
+				for (final ReturnRequestModel returnRequest : returnRequestList)
+				{
+					if (CollectionUtils.isNotEmpty(returnRequest.getReturnEntries()))
+					{
+						for (final ReturnEntryModel returnEntry : returnRequest.getReturnEntries())
+						{
+							if (returnEntry instanceof RefundEntryModel)
+							{
+								if (returnEntry.getOrderEntry() != null
+										&& CollectionUtils.isNotEmpty(returnEntry.getOrderEntry().getConsignmentEntries()))
+								{
+									final ConsignmentStatus status = returnEntry.getOrderEntry().getConsignmentEntries().iterator().next()
+											.getConsignment().getStatus();
+
+									if (status.equals(ConsignmentStatus.RETURN_CLOSED))
+									{
+										populateRefundList(orderModel);
+
+										if (CollectionUtils.isNotEmpty(refundList))
+										{
+											for (final OrderEntryModel orderEntry : refundList)
+											{
+												if (CollectionUtils.isNotEmpty(orderEntry.getConsignmentEntries()))
+												{
+													final ConsignmentModel consignment = orderEntry.getConsignmentEntries().iterator().next()
+															.getConsignment();
+													consignment.setStatus(ConsignmentStatus.REFUND_INITIATED);
+													modelService.save(consignment);
+													final OrderHistoryEntryModel historyEntry = modelService
+															.create(OrderHistoryEntryModel.class);
+													historyEntry.setTimestamp(Calendar.getInstance().getTime());
+													historyEntry.setOrder(orderEntry.getOrder());
+													historyEntry.setLineId(consignment.getCode());
+													historyEntry.setDescription(ConsignmentStatus.REFUND_INITIATED.toString());
+													modelService.save(historyEntry);
+													LOG.error("AutoRefundInitiateAction: historyEntry is set to REFUND_INITIATED for Order #"
+															+ orderModel.getCode());
+												}
+											}
+											LOG.error("AutoRefundInitiateAction: Going to call mplPaymentService.doRefundPayment(refundList); for Order #"
+													+ orderModel.getCode());
+											final String result = mplPaymentService.doRefundPayment(refundList);
+											LOG.error("AutoRefundInitiateAction: After call mplPaymentService.doRefundPayment(refundList); for Order #"
+													+ orderModel.getCode());
+											final String[] resultArray = result.split(",");
+
+											if (ArrayUtils.isNotEmpty(resultArray))
+											{
+												if (!resultArray[0].equalsIgnoreCase("FAILURE"))
+												{
+													for (final ReturnEntryModel returnEntryTmp : returnList)
+													{
+														returnEntryTmp.setStatus(ReturnStatus.RETURN_COMPLETED);
+
+														LOG.error("AutoRefundInitiateAction: returnEntryTmp.setStatus(ReturnStatus.RETURN_COMPLETED) for Order #"
+																+ orderModel.getCode());
+
+														modelService.save(returnEntryTmp);
+														//TODO :Return Initiated Notification
+														try
+														{
+															//send Notification
+															final int noOfItems = returnList.size();
+															mplNotificationSaveService.sendRefundInitiatedNotification(noOfItems,
+																	returnEntryTmp, orderModel);
+															LOG.error("AutoRefundInitiateAction: mplNotificationSaveService.sendRefundInitiatedNotification( for Order #"
+																	+ orderModel.getCode());
+														}
+														catch (final Exception e1)
+														{
+															LOG.error(
+																	"Exception during sending Notification for automatic Refund Initiation>>> ",
+																	e1);
+														}
+
+													}
+												}
+												else
+												{
+													for (final ReturnEntryModel returnEntryTmp : returnList)
+													{
+														returnEntryTmp.setStatus(ReturnStatus.RETURN_INPROGRESS);
+														modelService.save(returnEntryTmp);
+													}
+												}
+											}
+											else
+											{
+												LOG.error("refundRequestedUnresponsive  for automatic Refund");
+											}
+										}
+										else
+										{
+											LOG.error("errorNoRecordSelected for automatic Refund");
+										}
+									}
+								}
+								else
+								{
+									LOG.error("No Order Entry or No Consignment for automatic Refund");
+								}
+							}
+						}
+					}
+					else
+					{
+						LOG.error("No Request Available for automatic Refund");
+					}
+				}
+			}
+		}
+	}
+
+	private boolean isOrderCOD(final OrderModel order)
+	{
+		final List<PaymentTransactionModel> tranactions = new ArrayList<PaymentTransactionModel>(order.getPaymentTransactions());
+		boolean flag = false;
+		if (CollectionUtils.isNotEmpty(tranactions))
+		{
+			for (final PaymentTransactionModel transaction : tranactions)
+			{
+				if (CollectionUtils.isNotEmpty(transaction.getEntries()))
+				{
+					for (final PaymentTransactionEntryModel entry : transaction.getEntries())
+					{
+						if (entry.getPaymentMode() != null && entry.getPaymentMode().getMode() != null
+								&& entry.getPaymentMode().getMode().equalsIgnoreCase("COD"))
+						{
+							flag = true;
+							break;
+						}
+					}
+				}
+				if (flag)
+				{
+					break;
+				}
+			}
+		}
+
+		return flag;
+	}
+
+	protected void populateRefundList(final OrderModel orderModel)
+	{
+		if (orderModel != null)
+		{
+			final List<ReturnRequestModel> returnRequestList = orderModel.getReturnRequests();
+			if (CollectionUtils.isNotEmpty(returnRequestList))
+			{
+				for (final ReturnRequestModel returnRequest : returnRequestList)
+				{
+					if (CollectionUtils.isNotEmpty(returnRequest.getReturnEntries()))
+					{
+						for (final ReturnEntryModel returnEntry : returnRequest.getReturnEntries())
+						{
+							if (returnEntry instanceof RefundEntryModel)
+							{
+								if (returnEntry.getOrderEntry() != null
+										&& CollectionUtils.isNotEmpty(returnEntry.getOrderEntry().getConsignmentEntries()))
+								{
+									final ConsignmentStatus status = returnEntry.getOrderEntry().getConsignmentEntries().iterator().next()
+											.getConsignment().getStatus();
+
+									if (status.equals(ConsignmentStatus.RETURN_CLOSED))
+									{
+										final ReturnEntryModel returnEntryModel = returnEntry;
+										final OrderEntryModel orderEntryModel = (OrderEntryModel) returnEntryModel.getOrderEntry();
+
+										refundList.add(orderEntryModel);
+										returnList.add(returnEntryModel);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return the modelService
+	 */
+	@Override
+	public ModelService getModelService()
+	{
+		return modelService;
+	}
+
+	/**
+	 * @param modelService
+	 *           the modelService to set
+	 */
+	@Override
+	public void setModelService(final ModelService modelService)
+	{
+		this.modelService = modelService;
+	}
+
+	/**
+	 * @return the mplNotificationSaveService
+	 */
+	public MplNotificationService getMplNotificationSaveService()
+	{
+		return mplNotificationSaveService;
+	}
+
+	/**
+	 * @param mplNotificationSaveService
+	 *           the mplNotificationSaveService to set
+	 */
+	public void setMplNotificationSaveService(final MplNotificationService mplNotificationSaveService)
+	{
+		this.mplNotificationSaveService = mplNotificationSaveService;
+	}
+
+
+	/**
+	 * @return the mplPaymentService
+	 */
+	public MplPaymentService getMplPaymentService()
+	{
+		return mplPaymentService;
+	}
+
+	/**
+	 * @param mplPaymentService
+	 *           the mplPaymentService to set
+	 */
+	public void setMplPaymentService(final MplPaymentService mplPaymentService)
+	{
+		this.mplPaymentService = mplPaymentService;
+	}
+}
