ab4183234b75bf32d9cdf85935997e3117fbe603
Abhijit Gupta <gupta.abhijit@tcs.com>
2017-06-22 13:54:47 +0530

    985717:TPR-1345: Cancellation reason gap between CSCP and Web-Fix

--- ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/account/cancelreturn/impl/CancelReturnFacadeImpl.java
+++ ext/marketplace/marketplacefacades/src/com/tisl/mpl/facades/account/cancelreturn/impl/CancelReturnFacadeImpl.java
@@ -1359,7 +1359,7 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 						 * RETURN_SCHEDULE)) { if (returnLogisticsCheck) { //LOG.info("Setting Type of Return::::::" +
 						 * returnLogisticsCheck);
 						 * sendTicketRequestData.setTicketSubType(MarketplacecommerceservicesConstants.RETURN_TYPE_RSP); }
-						 *
+						 * 
 						 * }
 						 */
 					}
@@ -1934,23 +1934,32 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 	{
 
 		final List orderCancelEntries = new ArrayList();
-
+		String key = null;
 		//Get the reason from Global Code master
 		String reasonDescription = null;
-		final List<CancellationReasonModel> cancellationReasonList = mplOrderService.getCancellationReason();
-		for (final CancellationReasonModel cancellationReason : cancellationReasonList)
+		//Map<String,String> cancelReasonMap= new HashMap<String, String>();
+		if (Integer.parseInt(reasonCode) > 5)
 		{
-			if (cancellationReason.getReasonCode().equalsIgnoreCase(reasonCode))
-			{
-				if (StringUtils.isNotEmpty(cancellationReason.getReasonDescription()))
-				{
-					reasonDescription = cancellationReason.getReasonDescription();
-				}
-				break;
-			}
+			key = reasonCode + "_onetouch";
+			reasonDescription = configurationService.getConfiguration().getString(key);
+			reasonDescription = reasonDescription.trim();
 		}
+		else
+		{
+			final List<CancellationReasonModel> cancellationReasonList = mplOrderService.getCancellationReason();
+			for (final CancellationReasonModel cancellationReason : cancellationReasonList)
+			{
+				if (cancellationReason.getReasonCode().equalsIgnoreCase(reasonCode))
+				{
+					if (StringUtils.isNotEmpty(cancellationReason.getReasonDescription()))
+					{
+						reasonDescription = cancellationReason.getReasonDescription();
+					}
+					break;
+				}
+			}
 
-
+		}
 		final List<AbstractOrderEntryModel> orderEntries = associatedEntries(subOrderModel, transactionId);
 		for (final AbstractOrderEntryModel orderEntryData : orderEntries)
 		{
@@ -2018,9 +2027,9 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 	/*
 	 * private MplOrderCancelRequest buildCancelRequest(final AbstractOrderEntryModel orderEntryData, final String
 	 * reasonCode, final OrderData subOrderDetails, final OrderModel subOrderModel) throws OrderCancelException {
-	 *
+	 * 
 	 * final List orderCancelEntries = new ArrayList();
-	 *
+	 * 
 	 * //Get the reason from Global Code master String reasonDescription = null; final List<CancellationReasonModel>
 	 * cancellationReasonList = mplOrderService.getCancellationReason(); for (final CancellationReasonModel
 	 * cancellationReason : cancellationReasonList) { if
@@ -2036,16 +2045,16 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 	 * orderCancelEntry : orderCancelRequest.getEntriesToCancel()) { final AbstractOrderEntryModel orderEntry =
 	 * orderCancelEntry.getOrderEntry(); final List<PaymentTransactionModel> tranactions = new
 	 * ArrayList<PaymentTransactionModel>( subOrderModel.getPaymentTransactions());
-	 *
+	 * 
 	 * if (CollectionUtils.isNotEmpty(tranactions)) { for (final PaymentTransactionModel transaction : tranactions) { if
 	 * (CollectionUtils.isNotEmpty(transaction.getEntries())) { for (final PaymentTransactionEntryModel entry :
 	 * transaction.getEntries()) { if (entry.getPaymentMode() != null && entry.getPaymentMode().getMode() != null &&
 	 * entry.getPaymentMode().getMode().equalsIgnoreCase(MarketplaceFacadesConstants.PAYMENT_METHOS_COD)) {
 	 * orderCancelRequest.setAmountToRefund(NumberUtils.DOUBLE_ZERO); return orderCancelRequest; } } } } }
-	 *
+	 * 
 	 * double deliveryCost = 0D; if (orderEntry.getCurrDelCharge() != null) { deliveryCost =
 	 * orderEntry.getCurrDelCharge().doubleValue(); }
-	 *
+	 * 
 	 * refundAmount = orderEntryData.getNetAmountAfterAllDisc().doubleValue() + deliveryCost; } //Setting Refund Amount
 	 * orderCancelRequest.setAmountToRefund(new Double(refundAmount)); return orderCancelRequest; }
 	 */
@@ -2068,7 +2077,7 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 	 * OrderCancelRecordsHandlerException { final OrderCancelRecordEntryModel result =
 	 * this.getOrderCancelRecordsHandler().createRecordEntry(orderCancelRequest, userService.getCurrentUser());
 	 * //Initiate Refund initiateRefund(subOrderDetails, subOrderModel, result);
-	 *
+	 * 
 	 * }
 	 */
 
@@ -2553,9 +2562,9 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 
 	/*
 	 * @desc Saving order history for cancellation as OMS is not sending
-	 *
+	 * 
 	 * @param subOrderData
-	 *
+	 * 
 	 * @param subOrderModel
 	 */
 	private void createHistoryEntry(final AbstractOrderEntryModel orderEntryModel, final OrderModel orderModel,
@@ -4517,11 +4526,11 @@ public class CancelReturnFacadeImpl implements CancelReturnFacade
 
 	/*
 	 * (non-Javadoc) <<<<<<< HEAD
-	 * 
+	 *
 	 * =======
-	 * 
+	 *
 	 * >>>>>>> refs/remotes/origin/TCS_DEV_MASTER
-	 * 
+	 *
 	 * @see com.tisl.mpl.facades.account.cancelreturn.CancelReturnFacade#checkReturnLogisticsForApp(de.hybris.platform.
 	 * commercefacades.order.data.OrderData, java.lang.String, java.lang.String)
 	 */
