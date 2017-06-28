bfc4fdcffae95016d8d4b861165b4b674a089856
Abhijit Gupta <gupta.abhijit@tcs.com>
2017-06-28 16:04:45 +0530

    985717:TPR-1345: Return modification

--- ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/MiscsController.java
+++ ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/MiscsController.java
@@ -221,13 +221,13 @@ public class MiscsController extends BaseController
 	private CustomerFacade customerFacade;
 	/*
 	 * @Resource private ModelService modelService;
-	 * 
+	 *
 	 * @Autowired private ForgetPasswordFacade forgetPasswordFacade;
-	 * 
+	 *
 	 * @Autowired private ExtendedUserServiceImpl userexService;
-	 * 
+	 *
 	 * @Autowired private WishlistFacade wishlistFacade;
-	 * 
+	 *
 	 * @Autowired private MplSellerMasterService mplSellerInformationService;
 	 */
 	@Autowired
@@ -254,7 +254,7 @@ public class MiscsController extends BaseController
 	private FieldSetBuilder fieldSetBuilder;
 	/*
 	 * @Resource(name = "i18NFacade") private I18NFacade i18NFacade;
-	 * 
+	 *
 	 * @Autowired private MplCommerceCartServiceImpl mplCommerceCartService;
 	 */
 	@Autowired
@@ -282,17 +282,17 @@ public class MiscsController extends BaseController
 	 * @Resource(name = "mplPaymentFacade") private MplPaymentFacade mplPaymentFacade; private static final String
 	 * APPLICATION_TYPE = "application/json"; public static final String EMAIL_REGEX =
 	 * "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
-	 *
+	 * 
 	 * /**
-	 *
+	 * 
 	 * /*
-	 *
+	 * 
 	 * @Resource(name = "mplPaymentFacade") private MplPaymentFacade mplPaymentFacade; private static final String
 	 * APPLICATION_TYPE = "application/json"; public static final String EMAIL_REGEX =
 	 * "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
-	 *
+	 * 
 	 * /**
-	 *
+	 * 
 	 * @return the configurationService
 	 */
 	@Autowired
@@ -710,9 +710,9 @@ public class MiscsController extends BaseController
 
 	/*
 	 * restriction set up interface to save the data comming from seller portal
-	 * 
+	 *
 	 * @param restrictionXML
-	 * 
+	 *
 	 * @return void
 	 */
 	@RequestMapping(value = "/{baseSiteId}/miscs/restrictionServer", method = RequestMethod.POST)
@@ -1420,7 +1420,7 @@ public class MiscsController extends BaseController
 	 * final MarketplaceDeliveryModeData deliveryModeData = new MarketplaceDeliveryModeData(); final
 	 * MplZoneDeliveryModeValueModel MplZoneDeliveryModeValueModel = mplCheckoutFacade
 	 * .populateDeliveryCostForUSSIDAndDeliveryMode(deliveryMode, MarketplaceFacadesConstants.INR, ussid);
-	 *
+	 * 
 	 * if (null != MplZoneDeliveryModeValueModel) { if (null != MplZoneDeliveryModeValueModel.getValue()) { final
 	 * PriceData priceData = formPriceData(MplZoneDeliveryModeValueModel.getValue()); if (null != priceData) {
 	 * deliveryModeData.setDeliveryCost(priceData); } } if (null != MplZoneDeliveryModeValueModel.getDeliveryMode() &&
@@ -1433,11 +1433,11 @@ public class MiscsController extends BaseController
 	 * MplZoneDeliveryModeValueModel.getDeliveryMode().getName()) {
 	 * deliveryModeData.setName(MplZoneDeliveryModeValueModel.getDeliveryMode().getName()); } if (null != ussid) {
 	 * deliveryModeData.setSellerArticleSKU(ussid); }
-	 *
+	 * 
 	 * } return deliveryModeData; } =======
-	 *
+	 * 
 	 * @param code
-	 *
+	 * 
 	 * @return >>>>>>> origin/GOLDEN_PROD_SUPPORT_07122016
 	 */
 	@RequestMapping(value = "/{baseSiteId}/checkBrandOrCategory", method = RequestMethod.GET)
@@ -1889,7 +1889,9 @@ public class MiscsController extends BaseController
 								if (entry.getTransactionId().equalsIgnoreCase(transactionId))
 								{
 									orderEntry = entry;
-									if (orderEntry.isGiveAway() == true || orderEntry.isIsBOGOapplied() == true)
+									if ((orderEntry.isGiveAway() == true || orderEntry.isIsBOGOapplied() == true)
+											&& !oneTouchCrmObj.getTicketType().equalsIgnoreCase(
+													MarketplacewebservicesConstants.RETURN_TICKET))
 									{
 										continue outer;
 									}
@@ -1898,6 +1900,7 @@ public class MiscsController extends BaseController
 							}
 							else
 							{
+								output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
 								output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
 								output.setRemarks(MarketplacewebservicesConstants.BLANK_TRANSACTION_ID);
 								outputList.add(output);
@@ -1945,6 +1948,7 @@ public class MiscsController extends BaseController
 						}
 						else
 						{
+							output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
 							consignmentStatus = MarketplacewebservicesConstants.NO_CONSIGNMENT_FOUND;
 							output.setTransactionId(oneTouchCrmObj.getTransactionId());
 							output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
@@ -2028,73 +2032,78 @@ public class MiscsController extends BaseController
 											SalesApplication.CALLCENTER, oneTouchCrmObj.getPincode(), orderEntriesModel, subOrderModel,
 											codSelfShipData, oneTouchCrmObj.getUSSID(), oneTouchCrmObj.getTransactionId());
 									//Return is successfull
-									for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
+									//for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
+									//{
+									output = new OneTouchCancelReturnDTO();
+									if (resultFlag)
 									{
-										output = new OneTouchCancelReturnDTO();
-										if (resultFlag)
-										{
-											output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
-											output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-											output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_S);
-											output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_S);
-											outputList.add(output);
-										}
-										//Return is failure
-										else
-										{
-											output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-											output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
-											output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
-											output.setRemarks(MarketplacewebservicesConstants.ERROR_IN_OMS);
-											outputList.add(output);
-										}
+										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
+										//output.setTransactionId(abstractOrderEntryModel.getTransactionID());
+										output.setTransactionId(oneTouchCrmObj.getTransactionId());
+										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_S);
+										output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_S);
+										outputList.add(output);
 									}
+									//Return is failure
+									else
+									{
+										output.setTransactionId(oneTouchCrmObj.getTransactionId());
+										//output.setTransactionId(abstractOrderEntryModel.getTransactionID());
+										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
+										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
+										output.setRemarks(MarketplacewebservicesConstants.ERROR_IN_OMS);
+										outputList.add(output);
+									}
+									//}
 								}
 								//Pincode is not serviceable
 								else
 								{
-									for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
-									{
-										output = new OneTouchCancelReturnDTO();
-										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
-										output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
-										output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_F);
-										output.setRemarks(MarketplacewebservicesConstants.PINCODE_NOT_SERVICEABLE);
-										outputList.add(output);
-									}
+									//for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
+									//{
+									output = new OneTouchCancelReturnDTO();
+									output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
+									//output.setTransactionId(abstractOrderEntryModel.getTransactionID());
+									output.setTransactionId(oneTouchCrmObj.getTransactionId());
+									output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
+									output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_F);
+									output.setRemarks(MarketplacewebservicesConstants.PINCODE_NOT_SERVICEABLE);
+									outputList.add(output);
+									//}
 								}
 							}
 							//---------IF CONSIGNMENT IS ALREADY RETURNED--------
 							else
 							{
 
-								for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
+								//for (final AbstractOrderEntryModel abstractOrderEntryModel : orderEntriesModel)
+								//{
+								output = new OneTouchCancelReturnDTO();
+								///new addition to handle CS cockpit issues
+								if (consignmentStatus.equalsIgnoreCase("RETURN_INITIATED"))
 								{
-									output = new OneTouchCancelReturnDTO();
-									///new addition to handle CS cockpit issues
-									if (consignmentStatus.equalsIgnoreCase("RETURN_INITIATED"))
-									{
-										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
-										output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-										output.setServiceability(serviceabilty == true ? "S" : "F");
-										//output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_S);
-										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_S);
-										output.setRemarks(serviceabilty == true ? MarketplacewebservicesConstants.RETURN_ALREADY_INITIATED_CSCP
-												: MarketplacewebservicesConstants.PINCODE_NOT_SERVICEABLE);
-										outputList.add(output);
-									}
-									else
-									{
-										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
-										output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-										output.setServiceability(serviceabilty == true ? "S" : "F");
-										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
-										output.setRemarks(MarketplacewebservicesConstants.RETURN_ALREADY_INITIATED);
-										outputList.add(output);
-									}
-
+									output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
+									//output.setTransactionId(abstractOrderEntryModel.getTransactionID());
+									output.setTransactionId(oneTouchCrmObj.getTransactionId());
+									output.setServiceability(serviceabilty == true ? "S" : "F");
+									//output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_S);
+									output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_S);
+									output.setRemarks(serviceabilty == true ? MarketplacewebservicesConstants.RETURN_ALREADY_INITIATED_CSCP
+											: MarketplacewebservicesConstants.PINCODE_NOT_SERVICEABLE);
+									outputList.add(output);
 								}
+								else
+								{
+									output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
+									//output.setTransactionId(abstractOrderEntryModel.getTransactionID());
+									output.setTransactionId(oneTouchCrmObj.getTransactionId());
+									output.setServiceability(serviceabilty == true ? "S" : "F");
+									output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_F);
+									output.setRemarks(MarketplacewebservicesConstants.RETURN_ALREADY_INITIATED);
+									outputList.add(output);
+								}
+
+								//}
 							}
 						}
 					}
