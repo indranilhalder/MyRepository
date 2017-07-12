62ec4e53e9758899966c1f84958da2e41aa49fae
Madhavan <madhavan.krishnan@borngroup.com>
2017-07-12 16:13:29 +0530

    Stdandard size implementation

--- ext/luxury/luxurystoreaddon/src/com/tisl/lux/core/translator/LuxuryStandardSizeTranslator.java
+++ ext/luxury/luxurystoreaddon/src/com/tisl/lux/core/translator/LuxuryStandardSizeTranslator.java
@@ -15,6 +15,8 @@ import de.hybris.platform.jalo.security.JaloSecurityException;
 import de.hybris.platform.servicelayer.model.ModelService;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
 import de.hybris.platform.servicelayer.search.SearchResult;
+import de.hybris.platform.servicelayer.util.ServicesUtil;
+import de.hybris.platform.util.Config;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -30,7 +32,7 @@ import com.tisl.mpl.core.model.PcmProductVariantModel;
 
 
 /**
- * @author citrisys
+ * @author Madhavan
  *
  */
 public class LuxuryStandardSizeTranslator extends AbstractSpecialValueTranslator
@@ -64,50 +66,63 @@ public class LuxuryStandardSizeTranslator extends AbstractSpecialValueTranslator
 	@Override
 	public void performImport(final String cellValue, final Item processedItem) throws ImpExException
 	{
-		try
+		if (Config.getBoolean("luxury.stdsize.translator.enabled", false))
 		{
-			if (processedItem.getAttribute("productSource") instanceof PcmProductVariant)
+			try
 			{
-				final PcmProductVariant productSource = (PcmProductVariant) processedItem.getAttribute("productSource");
-				final String brandName = (String) processedItem.getAttribute("name");
-
-				final PcmProductVariantModel productModel = modelService.get(productSource.getPK());
-
-				final List<String> categoryPKs = new ArrayList<String>();
-				final Collection<CategoryModel> superCategories = productModel.getSupercategories();
-				for (final CategoryModel categoryModel : superCategories)
+				if (processedItem.getAttribute("productSource") instanceof PcmProductVariant)
 				{
-					categoryPKs.add("'" + categoryModel.getCode() + "'");
+
+					final String brandCode = (String) processedItem.getAttribute("brandCode");
+
+					final PcmProductVariant productSource = (PcmProductVariant) processedItem.getAttribute("productSource");
+					final PcmProductVariantModel productModel = modelService.get(productSource.getPK());
+
+					ServicesUtil.validateParameterNotNull(brandCode, "BrandCode cannot be null");
+					ServicesUtil.validateParameterNotNull(productModel.getSize(), "BrandSize cannot be null");
+
+					final List<String> categoryCodes = new ArrayList<String>();
+					final Collection<CategoryModel> superCategories = productModel.getSupercategories();
+					for (final CategoryModel categoryModel : superCategories)
+					{
+						categoryCodes.add("'" + categoryModel.getCode() + "'");
+					}
+					final String categories = String.join(",", categoryCodes);
+					ServicesUtil.validateParameterNotNull(categories, "Category cannot be null");
+
+
+					final Map params = new HashMap();
+					final String queryString = "select {pk} from {StandardSizeColor as ssc}"
+							+ " where {ssc:brand} = ?brand and {ssc:brandSize} = ?brandSize" + " and {ssc:category} in (" + categories
+							+ ")";
+
+					params.put("brand", brandCode);
+					params.put("brandSize", productModel.getSize());
+
+					LOG.info("brandCode - " + brandCode + "branSize - " + productModel.getSize());
+					LOG.info("product - " + productSource.getPK() + "categories - " + categories);
+					LOG.info("Query String - " + queryString);
+
+					final SearchResult queryResult = flexibleSearchService.search(queryString, params);
+
+					ServicesUtil.validateIfSingleResult(queryResult.getResult(), "No matching records found",
+							"More than one record found");
+
+					final StandardSizeColorModel sModel = (StandardSizeColorModel) queryResult.getResult().get(0);
+					final String standardSize = sModel.getStandardSize();
+					productModel.setBrandSize(productModel.getSize());
+					productModel.setSize(standardSize);
+					modelService.save(productModel);
 				}
-				final String categories = String.join(",", categoryPKs);
-
-				final Map params = new HashMap();
-
-				final String queryString = "select {pk} from {StandardSizeColor as ssc}"
-						+ " where {ssc:brand} = ?brand and {ssc:brandSize} = ?brandSize" + " and {ssc:category} in (" + categories
-						+ ")";
-
-				params.put("brand", brandName);
-				params.put("brandSize", productModel.getSize());
-				//params.put("categories", categoryPKs);
-
-				final SearchResult queryResult = flexibleSearchService.search(queryString, params);
-
-
-				final StandardSizeColorModel sModel = (StandardSizeColorModel) queryResult.getResult().get(0);
-				final String standardSize = sModel.getStandardSize();
-				productModel.setBrandSize(productModel.getSize());
-				productModel.setSize(standardSize);
-				modelService.save(productModel);
 			}
-		}
-		catch (final JaloInvalidParameterException e)
-		{
-			LOG.error(e);
-		}
-		catch (final JaloSecurityException e)
-		{
-			LOG.error(e);
+			catch (final JaloInvalidParameterException e)
+			{
+				LOG.error(e);
+			}
+			catch (final JaloSecurityException e)
+			{
+				LOG.error(e);
+			}
 		}
 	}
 }
