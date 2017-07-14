429605a3b74c3afe8cbfd2c6de311be05b7b7338
668514 <668514@01HW363100.India.TCS.com>
2017-07-14 12:49:40 +0530

    <970506>:pancard IQA fix

--- ext/marketplace/marketplacefacades/src/com/tisl/mpl/facade/pancard/MplPancardFacade.java
+++ ext/marketplace/marketplacefacades/src/com/tisl/mpl/facade/pancard/MplPancardFacade.java
@@ -6,6 +6,7 @@ package com.tisl.mpl.facade.pancard;
 import de.hybris.platform.core.model.PancardInformationModel;
 import de.hybris.platform.core.model.order.OrderModel;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.xml.bind.JAXBException;
@@ -44,10 +45,10 @@ public interface MplPancardFacade
 
 	//For sending pancard details to SP through PI and save data into database for new pancard entry
 	public void setPanCardDetailsAndPIcall(String orderreferancenumber, List<String> transactionidList, String customername,
-			String pancardnumber, MultipartFile file);
+			String pancardnumber, MultipartFile file) throws IllegalStateException, IOException, JAXBException;
 
 	public void refreshPanCardDetailsAndPIcall(List<PancardInformationModel> pModelList, PancardInformationModel pModel,
-			String pancardnumber, MultipartFile file);
+			String pancardnumber, MultipartFile file) throws IllegalStateException, IOException, JAXBException;
 
 	public List<OrderModel> getOrderForCode(String orderreferancenumber);
 
