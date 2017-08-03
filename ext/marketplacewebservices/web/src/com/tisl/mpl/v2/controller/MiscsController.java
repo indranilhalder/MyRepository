a5b8afddaaf3a7d6f1c9955fca3623bc1ed3ebd6
SSreya <s.sreya@tcs.com>
2017-08-03 16:38:33 +0530

    985717: Sonar fixes for miscscontroller and cancelreturnfacadeImpl

--- ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/MiscsController.java
+++ ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/MiscsController.java
@@ -225,13 +225,13 @@ public class MiscsController extends BaseController
 	private CustomerFacade customerFacade;
 	/*
 	 * @Resource private ModelService modelService;
-	 * 
+	 *
 	 * @Autowired private ForgetPasswordFacade forgetPasswordFacade;
-	 * 
+	 *
 	 * @Autowired private ExtendedUserServiceImpl userexService;
-	 * 
+	 *
 	 * @Autowired private WishlistFacade wishlistFacade;
-	 * 
+	 *
 	 * @Autowired private MplSellerMasterService mplSellerInformationService;
 	 */
 	@Autowired
@@ -258,7 +258,7 @@ public class MiscsController extends BaseController
 	private FieldSetBuilder fieldSetBuilder;
 	/*
 	 * @Resource(name = "i18NFacade") private I18NFacade i18NFacade;
-	 * 
+	 *
 	 * @Autowired private MplCommerceCartServiceImpl mplCommerceCartService;
 	 */
 	@Autowired
@@ -286,23 +286,23 @@ public class MiscsController extends BaseController
 	 * @Resource(name = "mplPaymentFacade") private MplPaymentFacade mplPaymentFacade; private static final String
 	 * APPLICATION_TYPE = "application/json"; public static final String EMAIL_REGEX =
 	 * "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
-	 * 
+	 *
 	 * /**
-	 * 
+	 *
 	 * /*
-	 * 
+	 *
 	 * @Resource(name = "mplPaymentFacade") private MplPaymentFacade mplPaymentFacade; private static final String
 	 * APPLICATION_TYPE = "application/json"; public static final String EMAIL_REGEX =
 	 * "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
 	 * @return the configurationService
 	 */
 	@Autowired
@@ -727,9 +727,9 @@ public class MiscsController extends BaseController
 
 	/*
 	 * restriction set up interface to save the data comming from seller portal
-	 * 
+	 *
 	 * @param restrictionXML
-	 * 
+	 *
 	 * @return void
 	 */
 	@RequestMapping(value = "/{baseSiteId}/miscs/restrictionServer", method = RequestMethod.POST)
@@ -1455,7 +1455,7 @@ public class MiscsController extends BaseController
 	 * final MarketplaceDeliveryModeData deliveryModeData = new MarketplaceDeliveryModeData(); final
 	 * MplZoneDeliveryModeValueModel MplZoneDeliveryModeValueModel = mplCheckoutFacade
 	 * .populateDeliveryCostForUSSIDAndDeliveryMode(deliveryMode, MarketplaceFacadesConstants.INR, ussid);
-	 *
+	 * 
 	 * if (null != MplZoneDeliveryModeValueModel) { if (null != MplZoneDeliveryModeValueModel.getValue()) { final
 	 * PriceData priceData = formPriceData(MplZoneDeliveryModeValueModel.getValue()); if (null != priceData) {
 	 * deliveryModeData.setDeliveryCost(priceData); } } if (null != MplZoneDeliveryModeValueModel.getDeliveryMode() &&
@@ -1468,11 +1468,11 @@ public class MiscsController extends BaseController
 	 * MplZoneDeliveryModeValueModel.getDeliveryMode().getName()) {
 	 * deliveryModeData.setName(MplZoneDeliveryModeValueModel.getDeliveryMode().getName()); } if (null != ussid) {
 	 * deliveryModeData.setSellerArticleSKU(ussid); }
-	 *
+	 * 
 	 * } return deliveryModeData; } =======
-	 *
+	 * 
 	 * @param code
-	 *
+	 * 
 	 * @return >>>>>>> origin/GOLDEN_PROD_SUPPORT_07122016
 	 */
 	@RequestMapping(value = "/{baseSiteId}/checkBrandOrCategory", method = RequestMethod.GET)
@@ -2097,21 +2097,19 @@ public class MiscsController extends BaseController
 									 * cancelReturnFacade.associatedEntries( subOrderModel, oneTouchCrmObj.getTransactionId());
 									 */
 
-									String consignmentStatusCheck = null;
 									boolean deliverymodeValidator = true;
 									checkloop: for (final AbstractOrderEntryModel orderEntry1 : orderEntriesModel)
 									{
 										LOG.debug("===Inside checkloop check====");
 										checkList.add(orderEntry1.getTransactionID());
 										deliveryCheckFlag = true;
-										consignmentStatusCheck = "";
 										for (final ConsignmentEntryModel con : orderEntry1.getConsignmentEntries())
 										{
 											LOG.debug("===Inside ConsignmentEntryModel loop===="
 													+ con.getConsignment().getStatus().getCode());
 											if (StringUtils.isNotEmpty(con.getConsignment().getStatus().getCode()))
 											{
-												consignmentStatusCheck = con.getConsignment().getStatus().getCode();
+												//consignmentStatusCheck = con.getConsignment().getStatus().getCode();
 											}
 											LOG.debug("===========DeliveryMode:====****======"
 													+ orderEntry1.getMplDeliveryMode().getDeliveryMode().getCode());
@@ -2398,7 +2396,7 @@ public class MiscsController extends BaseController
 									{
 										output.setOrderRefNum(oneTouchCrmObj.getOrderRefNum());
 										output.setTransactionId(abstractOrderEntryModel.getTransactionID());
-										output.setServiceability(serviceabilty == true ? "S" : "F");
+										output.setServiceability(serviceabilty ? "S" : "F");
 										//output.setServiceability(MarketplacewebservicesConstants.VALID_FLAG_S);
 										output.setValidFlag(MarketplacewebservicesConstants.VALID_FLAG_S);
 										output.setRemarks(serviceabilty ? MarketplacewebservicesConstants.RETURN_ALREADY_INITIATED_CSCP
