2019c3e21c964222ca77de2277119b20c6503223
PrasadReddy Venna <prasad.v@techouts.com>
2017-09-23 22:13:11 +0530

    Egv product controller

--- ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/controllers/pages/ProductPageController.java
+++ ext/marketplace/marketplacestorefront/web/src/com/tisl/mpl/storefront/controllers/pages/ProductPageController.java
@@ -119,8 +119,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import atg.taglib.json.util.JSONException;
-
 import com.google.gson.Gson;
 import com.granule.json.JSON;
 import com.granule.json.JSONArray;
@@ -166,8 +164,11 @@ import com.tisl.mpl.storefront.constants.ModelAttributetConstants;
 import com.tisl.mpl.storefront.constants.RequestMappingUrlConstants;
 import com.tisl.mpl.storefront.controllers.ControllerConstants;
 import com.tisl.mpl.storefront.controllers.helpers.FrontEndErrorHelper;
+import com.tisl.mpl.storefront.web.forms.EgvDetailForm;
 import com.tisl.mpl.storefront.web.forms.SellerInformationDetailsForm;
 import com.tisl.mpl.util.ExceptionUtil;
+
+import atg.taglib.json.util.JSONException;
 
 
 
@@ -3793,6 +3794,60 @@ public class ProductPageController extends MidPageController
 	}
 
 
+	@RequestMapping(value = ControllerConstants.Views.Fragments.Product.PRODUCT_CODE_GIFT_CART, method = RequestMethod.GET)
+	public String getGitProductDetails(@PathVariable(ControllerConstants.Views.Fragments.Product.PRODUCT_CODE) String productCode,
+			final Model model, final HttpServletRequest request)
+	{
+		try
+		{
+
+			if (null != productCode)
+			{
+				productCode = productCode.toUpperCase();
+			}
+			final ProductModel productModel = productService.getProductForCode(productCode);
+			populateProductDetailForDisplay(productModel, model, request);
+
+			final String msdjsURL = configurationService.getConfiguration().getString("msd.js.url");
+			final Boolean isMSDEnabled = Boolean.valueOf(configurationService.getConfiguration().getString("msd.enabled"));
+			final String msdRESTURL = configurationService.getConfiguration().getString("msd.rest.url");
+			model.addAttribute(new ReviewForm());
+			model.addAttribute(ModelAttributetConstants.PAGE_TYPE, PageType.PRODUCT.name());
+			model.addAttribute(ModelAttributetConstants.PRODUCT_CATEGORY_TYPE, productModel.getProductCategoryType());
+			model.addAttribute(ModelAttributetConstants.MSD_JS_URL, msdjsURL);
+			model.addAttribute(ModelAttributetConstants.IS_MSD_ENABLED, isMSDEnabled);
+			model.addAttribute(ModelAttributetConstants.MSD_REST_URL, msdRESTURL);
+
+			if (productModel instanceof PcmProductVariantModel)
+			{
+				final PcmProductVariantModel variantProductModel = (PcmProductVariantModel) productModel;
+				model.addAttribute(ModelAttributetConstants.PRODUCT_SIZE, variantProductModel.getSize());
+			}
+			getViewForPage(model);
+
+		}
+		catch (final EtailBusinessExceptions e)
+		{
+			ExceptionUtil.etailBusinessExceptionHandler(e, null);
+
+		}
+		catch (final EtailNonBusinessExceptions e)
+		{
+			ExceptionUtil.etailNonBusinessExceptionHandler(e);
+		}
+		catch (final Exception e)
+		{
+			ExceptionUtil.etailNonBusinessExceptionHandler(new EtailNonBusinessExceptions(e,
+					MarketplacecommerceservicesConstants.E0000));
+		}
+		final EgvDetailForm egvDetailsform = new EgvDetailForm();
+		model.addAttribute("egvDetailsform", egvDetailsform);
+
+		final ContentPageModel contentPage = getContentPageForLabelOrId("egvPDPPage");
+		storeCmsPageInModel(model, contentPage);
+		setUpMetaDataForContentPage(model, contentPage);
+		return "pages/layout/egvPDPResponsive";
+	}
 
 
 	/**
