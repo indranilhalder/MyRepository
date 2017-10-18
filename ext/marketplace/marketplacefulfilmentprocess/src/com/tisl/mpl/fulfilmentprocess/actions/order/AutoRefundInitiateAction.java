7b879adaab0dd23f8a785aac95beb252e1883758
Shiva <Shiva@Tech>
2017-10-18 10:44:58 +0530

    Bug FIx for Cancel Refund

--- ext/marketplace/marketplacefulfilmentprocess/src/com/tisl/mpl/fulfilmentprocess/actions/order/AutoRefundInitiateAction.java
+++ ext/marketplace/marketplacefulfilmentprocess/src/com/tisl/mpl/fulfilmentprocess/actions/order/AutoRefundInitiateAction.java
@@ -655,10 +655,12 @@ public class AutoRefundInitiateAction extends AbstractProceduralAction<OrderProc
 		double cashBackAmt=0;
 		if(null !=orderEntry &&  null != orderEntry.getWalletApportionPaymentInfo() && null!= orderEntry.getWalletApportionPaymentInfo().getWalletCardList()){
 			for(WalletCardApportionDetailModel cardApportionDetail : orderEntry.getWalletApportionPaymentInfo().getWalletCardList()){
+				if(null != cardApportionDetail && null!= cardApportionDetail.getBucketType()){
 				if(cardApportionDetail.getBucketType().equalsIgnoreCase("CASHBACK")){
 					cashBackAmt += Double.parseDouble(cardApportionDetail.getQcApportionValue()) +  Double.parseDouble( null != cardApportionDetail.getQcDeliveryValue() ? cardApportionDetail.getQcDeliveryValue() : ""+0 )
 							+Double.parseDouble( null != cardApportionDetail.getQcSchedulingValue() ? cardApportionDetail.getQcSchedulingValue() : ""+0 )+
 							Double.parseDouble( null != cardApportionDetail.getQcShippingValue() ? cardApportionDetail.getQcShippingValue() : ""+0 );
+				}
 				}
 			}
 		}
