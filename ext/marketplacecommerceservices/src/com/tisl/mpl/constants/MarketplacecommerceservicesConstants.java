3b2fcd0d6c695805041813ab7e4891aba3e86b37
Atonu Ganguly <atonu.ganguly@tcs.com>
2017-10-24 16:05:05 +0530

    1047001 : <> : NA Conflict fix

--- ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/constants/MarketplacecommerceservicesConstants.java
@@ -2278,10 +2278,12 @@ public final class MarketplacecommerceservicesConstants extends GeneratedMarketp
 	public final static String PINCODE_RESPONSE_DATA_PDP = "PincodeResponseDataForPDP".intern();
 	public static final String KM = "km".intern();
 	public final static String PINCODE_MODEL_PDP = "PincodeModelPDP".intern();
-	public static final String POS_QUERY_FOR_SELLER_AND_SLAVE = "select {pos:pk} from {PointOfService As pos} where {pos.slaveId} in (".intern();
+	public static final String POS_QUERY_FOR_SELLER_AND_SLAVE = "select {pos:pk} from {PointOfService As pos} where {pos.slaveId} in ("
+			.intern();
 	public static final String POS_QUERY_FOR_SELLER_AND_SLAVE2 = ") and {pos.active}=?active".intern();
 	public static final String POS_SLAVEID = "slaveId".intern();
 	public static final String POS_ACTIVE = "active".intern();
 	public static final String STORE_DISPLAY_PDP = "pdp.store.display".intern();
 
+	public static final String PAYTM = "Paytm".intern();//Paytm
 }
