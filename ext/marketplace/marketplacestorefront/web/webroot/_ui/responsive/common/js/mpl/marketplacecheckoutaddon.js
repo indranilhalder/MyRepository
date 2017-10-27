b2cccc7574ebaea2bd9efa921e1d622023e1745a
nbhanushali <nbhanushali@tataunistore.com>
2017-10-27 21:54:19 +0530

    wallet fix

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/marketplacecheckoutaddon.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/common/js/mpl/marketplacecheckoutaddon.js
@@ -5664,12 +5664,10 @@ $(".edit_address").click(function(){
 
 
 function useWalletForPaymentAjax(){
-
-	$("body").append("<div id='no-click' style='opacity:0.5; background:#000; z-index: 100000; width:100%; height:100%; position: fixed; top: 0; left:0;'></div>");
-	$("body").append('<div class="loaderDiv" style="position: fixed; left: 45%;top:45%;z-index: 10000"><img src="'+staticHost+'/_ui/responsive/common/images/red_loader.gif" class="spinner"></div>');
 	
+	if ($(window).width() < 768 && $("#walletContainerId").hasClass("giftCheckoutContainerTable")) {
 	ACC.singlePageCheckout.onPaymentModeSelection('Cliq Cash','','','false');
-	
+	}
 	
 	$(".cliqCashApplyAlert").hide();
 	$(".topPlaceOrderBtn").hide();
@@ -5680,7 +5678,8 @@ function useWalletForPaymentAjax(){
 	 $("#viewPaymentCOD").show();
 	 $("#paytmId").show();
 	var staticHost = $('#staticHost').val();
-	
+	$("body").append("<div id='no-click' style='opacity:0.5; background:#000; z-index: 100000; width:100%; height:100%; position: fixed; top: 0; left:0;'></div>");
+	$("body").append('<div class="loaderDiv" style="position: fixed; left: 45%;top:45%;z-index: 10000"><img src="'+staticHost+'/_ui/responsive/common/images/red_loader.gif" class="spinner"></div>');
 
 	//Populating data to Order Details section
 	/*var defaultCCText = $("#finalCliqCashUsedDefault").text(); 
