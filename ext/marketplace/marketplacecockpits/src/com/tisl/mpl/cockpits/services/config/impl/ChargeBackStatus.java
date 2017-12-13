eab0bbbd450d9e62c1093c69caa92eb5a8bf36e0
884206 <884206@01HW1057067.India.TCS.com>
2017-12-13 19:10:46 +0530

    668514:TISHS-83:Charge Back Null check and order update date fomrat fix

--- ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/services/config/impl/ChargeBackStatus.java
+++ ext/marketplace/marketplacecockpits/src/com/tisl/mpl/cockpits/services/config/impl/ChargeBackStatus.java
@@ -23,8 +23,7 @@ public class ChargeBackStatus extends AbstractSimpleCustomColumnConfiguration <S
 			final PaymentTransactionEntryModel paymentEntry = (PaymentTransactionEntryModel) itemModel;
 			final PaymentTransactionModel paymentTransMod = paymentEntry.getPaymentTransaction();
 			
-			if(paymentTransMod != null && paymentTransMod.getPlannedAmount().equals(Double.valueOf(0.00)))
-			{
+			if(paymentTransMod != null && paymentTransMod.getPlannedAmount() !=null && paymentTransMod.getPlannedAmount().equals(Double.valueOf(0.00))){
 				paymentstatus = PAYMENT_STATUS;
 			}
 			else
