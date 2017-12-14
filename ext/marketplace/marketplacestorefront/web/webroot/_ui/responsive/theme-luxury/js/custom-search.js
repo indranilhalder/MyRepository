4ec70935f3ea3b8f92d047bcba3012f30df46fcb
unknown <mathialagan.r@TWGC01A728.wearefmg.net>
2017-12-14 11:29:00 +0530

    bug fix

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/js/custom-search.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/js/custom-search.js
@@ -4,7 +4,7 @@
 $('input[type=radio][name=searchCategory]').change(function() {
 	$(".select-list .dropdown li.selected").attr('id',$('input[name="searchCategory"]:checked').val());
 	var searchCategoryID=$('input[name="searchCategory"]:checked').attr('id');
-	var searchCategoryName=$("label[for='"+searchCategoryID+"']").text();
+	var searchCategoryName=$(this).siblings("label[for='"+searchCategoryID+"']").text();
 	$("#selectedCategoryName").val(searchCategoryName);
 	$("#js-site-search-input").focus();
 	$("#js-site-search-input").trigger("keydown");
