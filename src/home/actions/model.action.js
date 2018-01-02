403babbdf83d2911b130645124f765cf6a1eee26
Basavannavva <Basavannevva@xelpmoc.in>
2018-01-02 17:28:58 +0530

    Changes in model.action

--- src/home/actions/model.action.js
+++ src/home/actions/model.action.js
@@ -2,7 +2,7 @@ export const MOBILE_MODEL_TYPE_SHOW = "MOBILE_MODEL_TYPE_SHOW";
 export const MOBILE_MODEL_TYPE_HIDE = "MOBILE_MODEL_TYPE_HIDE";
 
 // Actions for Creating User
-export function modelTypeShow(modelType, visibility, OwnProps) {
+export function mobileModelTypeShow(modelType, visibility, OwnProps) {
   return {
     type: MOBILE_MODEL_TYPE_SHOW,
     modelType,
@@ -11,7 +11,7 @@ export function modelTypeShow(modelType, visibility, OwnProps) {
   };
 }
 
-export function modelTypeHide(modelType) {
+export function mobileModelTypeHide(modelType) {
   return {
     type: MOBILE_MODEL_TYPE_HIDE,
     modelType
