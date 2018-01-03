ef73cdcd4423915b0ffd3ac280d4ccf5c86f8e27
Manish <mkjha9555@gmail.com>
2018-01-03 19:11:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TaTA cLIQ</h1>
+          <h1 className="App-title">Welcome to React World</h1>
         </header>
         <Input />
       </div>
