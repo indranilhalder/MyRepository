e42e70929d123f4b4339c060f107e95f6ec27d88
dileep <dileep@xelpmoc.in>
2018-01-04 19:49:27 +0530

    styles removed

--- src/general/components/LogInButton.js
+++ src/general/components/LogInButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./LogInButtonStyles.css";
+
 import { Button } from "xelpmoc-core";
 import MediaQuery from "react-responsive";
 import PropTypes from "prop-types";
@@ -13,32 +13,28 @@ export default class LogInButton extends React.Component {
     return (
       <div>
         <MediaQuery query="(min-device-width: 1024px)">
-          <div className={styles.base}>
-            <Button
-              label={"Sign in"}
-              width={150}
-              height={40}
-              borderRadius={20}
-              backgroundColor={"#FF1744"}
-              onClick={this.onClick}
-              loading={this.props.loading}
-              textStyle={{ color: "#FFF", fontSize: 14 }}
-            />
-          </div>
+          <Button
+            label={"Sign in"}
+            width={150}
+            height={40}
+            borderRadius={20}
+            backgroundColor={"#FF1744"}
+            onClick={this.onClick}
+            loading={this.props.loading}
+            textStyle={{ color: "#FFF", fontSize: 14 }}
+          />
         </MediaQuery>
         <MediaQuery query="(max-device-width:1023px)">
-          <div className={styles.base}>
-            <Button
-              backgroundColor={"#FF1744"}
-              label={"Login"}
-              width={100}
-              height={40}
-              borderRadius={20}
-              onClick={this.onClick}
-              loading={this.props.loading}
-              textStyle={{ color: "#FFF", fontSize: 14 }}
-            />
-          </div>
+          <Button
+            backgroundColor={"#FF1744"}
+            label={"Login"}
+            width={100}
+            height={40}
+            borderRadius={20}
+            onClick={this.onClick}
+            loading={this.props.loading}
+            textStyle={{ color: "#FFF", fontSize: 14 }}
+          />
         </MediaQuery>
       </div>
     );
