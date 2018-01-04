8b539c00423d09ecd79e2d83b91b60dfe33f3b46
Basavannavva <Basavannevva@xelpmoc.in>
2018-01-04 15:35:55 +0530

    index file has been Changed

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as api from "../src/lib/apiRequest";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import user from "../src/auth/reducers/user.reducer";
+import registerServiceWorker from "./registerServiceWorker";
 
 const rootReducer = combineReducers({
   user
@@ -29,3 +30,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+registerServiceWorker();
