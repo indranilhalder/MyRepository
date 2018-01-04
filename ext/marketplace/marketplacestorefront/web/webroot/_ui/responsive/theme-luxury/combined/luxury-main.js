c9890dd527dc69ad3e4cf346d4579c68b45ee320
umamaheswaran <umamaheswaran.periyasamy@borngroup.com>
2018-01-04 11:31:20 +0530

    grunt js

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/combined/luxury-main.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/combined/luxury-main.js
@@ -1091,17 +1091,6 @@ function getRating(key, productCode, category) {
         $("#ratingDiv .gig-rating-readReviewsLink").text(data.streamInfo.ratingCount + " REVIEWS")), 
         $("#customer").text("Customer Reviews (" + data.streamInfo.ratingCount + ")");
     });
-    var ratingsParams = {
-        categoryID: category,
-        streamID: productCode,
-        containerID: "ratingDiv",
-        linkedCommentsUI: "commentsDiv",
-        showCommentButton: "true",
-        onAddReviewClicked: function(response) {
-            CheckUserLogedIn();
-        }
-    };
-    gigya.comments.showRatingUI(ratingsParams);
 }
 
 function CheckUserLogedIn() {
@@ -17411,9 +17400,6 @@ TATA.CommonFunctions = {
                 $(this).addClass("current"), $("#" + tab_id).addClass("current");
             }), $("ul.tabs li, .mega-menu.tab-content").mouseout(function() {
                 $(".tab-content").removeClass("current"), $("ul.tabs li").removeClass("current");
-                $('.tabs > .tab-link:first-child').addClass('current');
-                $('#main-nav > .mega-menu:first-child').addClass('current');
-                $('#main-nav > .mega-menu:first-child > li > a').fadeIn(600);
             }), $(".mega-menu.tab-content").mouseover(function() {
                 var tab_id = $(this).attr("id");
                 $("ul.tabs li").removeClass("current"), $(".tab-content").removeClass("current"), 
@@ -19266,27 +19252,3 @@ $("#popUpExpAddress").on("hidden.bs.modal", function() {
 }), $(".card_exp_month, .card_exp_year").on("change", function() {
     $(this).css("color", "#000");
 });
-
-var window_width = $(window).width();
-if (window_width > 768) {
-    executeForDesktop();
-}
-
-function executeForDesktop() {
-    //by deafult show first sub menu
-    $('.tabs > .tab-link:first-child').addClass('current');
-    $('#main-nav > .mega-menu:first-child').addClass('current');
-
-    // fade in effect
-    $('.tabs .tab-link').mouseover(function() {
-        var second_level_menu = $(this).data('tab');
-        $('#main-nav > .mega-menu > li > a').css('display', 'none');
-        $('#main-nav > #' + second_level_menu + '> li > a').fadeIn(600);
-    });
-
-    //for sub sub menu fade in effect
-    $('.mega-menu > li').hover(function() {
-        $(this).find('.sub-menu .sub-menu-inner').css('display', 'none');
-        $(this).find('.sub-menu .sub-menu-inner').fadeIn(600);
-    });
-}
