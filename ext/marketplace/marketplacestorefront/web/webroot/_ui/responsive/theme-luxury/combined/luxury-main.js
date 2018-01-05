22e33d9a3f4a1f181d31f0c051a7d4d74c9e2ebf
avyas-tul <avyas@tataunistore.com>
2018-01-05 12:47:14 +0530

    LMA-36

--- ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/combined/luxury-main.js
+++ ext/marketplace/marketplacestorefront/web/webroot/_ui/responsive/theme-luxury/combined/luxury-main.js
@@ -19292,3 +19292,8 @@ $(document).ready(function() {
         });
     }
 });
+$('.collapse-title').bind('click',function(){
+    $(this).find('.glyphicon').toggleClass('glyphicon-minus');
+    var data_id = $(this).data('id');
+    $('#'+data_id).slideToggle(300);
+});
