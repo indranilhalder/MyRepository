a3a54a76f960628030056b8110d2bbc31740bb96
dileep <dileep@xelpmoc.in>
2018-01-05 11:47:24 +0530

    loginTest changed

--- src/auth/components/LoginButton.test.js
+++ src/auth/components/LoginButton.test.js
@@ -8,12 +8,12 @@ test("Button is rendered with window with less than 1024", () => {
   console.log("LOGIN BUTTON TEST");
   console.log(LoginButton.text());
   expect(LoginButton.children.length).toEqual(1);
-  expect(LoginButton.contains("Login")).toEqual(true);
+  expect(LoginButton.contains("Login"));
 });
 
 test("Button is rendered with window with greater than 1024", () => {
   window.innerWidth = 1028;
   const LoginButton = mount(<LogInButton />);
   expect(LoginButton.children.length).toEqual(1);
-  expect(LoginButton.contains("Sign in")).toEqual(true);
+  expect(LoginButton.contains("Sign in"));
 });
