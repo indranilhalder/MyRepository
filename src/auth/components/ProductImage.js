6e522f09b12500ef59095c88096cd34a080f65f1
dileep <dileep@xelpmoc.in>
2018-01-08 18:27:06 +0530

    product image component created

---
+++ src/auth/components/ProductImage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./ProductImage.css";
+import { Image } from "xelpmoc-core";
+import PropTypes from "prop-types";
+export default class ProductImage extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.imageHolder}>
+          <Image image={this.props.image} />
+        </div>
+      </div>
+    );
+  }
+}
+ProductImage.propTypes = {
+  image: PropTypes.string
+};
