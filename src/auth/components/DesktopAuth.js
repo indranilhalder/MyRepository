c93aede58310ff1d6263bb57b46fbf09377e57f1
dileep <dileep@xelpmoc.in>
2018-01-08 14:34:09 +0530

    Desktop modal added

---
+++ src/auth/components/DesktopAuth.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Login from "./Login";
+import Signup from "./SignUp";
+import SocialButtons from "./SocialButtons";
+import styles from "./DesktopAuthStyles.css";
+export default class DesktopAuth extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.left}>
+          <div className={styles.loginHolder}>
+            <h2 className={styles.header}>Welcome back</h2>
+            <Login />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.signUpHolder}>
+            <h2 className={styles.header}>New to Cliq</h2>
+            <Signup />
+          </div>
+        </div>
+        <div className={styles.socialButtonHolder}>
+          <SocialButtons />
+        </div>
+      </div>
+    );
+  }
+}
