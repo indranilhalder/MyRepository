f793c5a350a01617a5db5b2bc4271fdd93b5d0ae
shruti_R <shruti45.rugi@gmail.com>
2018-01-09 11:38:09 +0530

    testcase deleted

--- src/general/components/TestCase.js
+++
@@ -1,9 +0,0 @@
-test("adds true to equal true", () => {
-  expect(true).toBe(true);
-});
-
-describe("Adding 2 and 3 expecting 5", () => {
-  it("Add function", () => {
-    expect(2 + 3).toBe(5);
-  });
-});
