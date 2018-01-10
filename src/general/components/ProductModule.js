86144909a05b5b1079fbce8f6d1b9498b68f08c6
dileep <dileep@xelpmoc.in>
2018-01-10 18:45:54 +0530

    spread operator added

--- src/general/components/ProductModule.js
+++ src/general/components/ProductModule.js
@@ -3,7 +3,7 @@ import ProductImage from "./ProductImage";
 import ProductDescription from "./ProductDescription";
 import styles from "./ProductModule.css";
 import downloadIcon from "./img/download.svg";
-import PropTypes from "prop-types";
+
 export default class ProductModule extends React.Component {
   onDownload = () => {
     if (this.props.onDownload) {
@@ -23,23 +23,11 @@ export default class ProductModule extends React.Component {
         </div>
         <div className={styles.content} />
         <ProductDescription
-          title={this.props.title}
-          description={this.props.description}
-          price={this.props.price}
+          {...this.props}
           icon={downloadIcon}
-          discountPrice={this.props.discountPrice}
           onDownload={this.onDownload}
         />
       </div>
     );
   }
 }
-ProductModule.PropTypes = {
-  productImage: PropTypes.string,
-  title: PropTypes.string,
-  description: PropTypes.string,
-  price: PropTypes.number,
-  discountPrice: PropTypes.number,
-  onDownload: PropTypes.func,
-  onClick: PropTypes.func
-};
