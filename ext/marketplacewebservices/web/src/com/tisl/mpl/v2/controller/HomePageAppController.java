05074246825604d6e31cb63ac0e9bb5a4fb23b59
Ipsita Pathak <ipsita.pathak@tcs.com>
2018-01-11 18:55:39 +0530

    712001:NU-35:Changes in controller

--- ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/HomePageAppController.java
+++ ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/HomePageAppController.java
@@ -10,34 +10,63 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tisl.mpl.constants.MarketplacecommerceservicesConstants;
+import com.tisl.mpl.exception.EtailBusinessExceptions;
+import com.tisl.mpl.exception.EtailNonBusinessExceptions;
+import com.tisl.mpl.util.ExceptionUtil;
+import com.tisl.mpl.wsdto.ComponentRequestDTO;
+import com.tisl.mpl.wsdto.ThemeOffersDTO;
+
 
 /**
  * @author TCS
  *
  */
+
 @Controller
-@RequestMapping(value = "/{baseSiteId}/cms/{userId}/page")
+@RequestMapping(value = "/{baseSiteId}/cms/{userId}/page", headers = "Accept=application/xml,application/json")
 public class HomePageAppController
 {
 
-	@RequestMapping(value = "/homepage", method = RequestMethod.POST, consumes = "application/json")
-	@ResponseBody
-	public void getHomePageComponents(@RequestParam final String mcvid, @RequestParam final String lat,
-			@RequestParam final String lng, @RequestParam final String pincode, @RequestParam final String channel,
-			@RequestParam final String isPwa)
-	{
-		System.out.println("API LAyer will handle this");
-
-	}
-
 	@RequestMapping(value = "/themeOffer", method = RequestMethod.POST, consumes = "application/json")
 	@ResponseBody
-	public void getThemeOffersComponents(@RequestParam final String mcvid, @RequestParam final String lat,
+	public ThemeOffersDTO getThemeOffersComponents(@RequestParam final String mcvid, @RequestParam final String lat,
 			@RequestParam final String lng, @RequestParam final String pincode, @RequestParam final String channel,
-			@RequestParam final String isPwa, @RequestBody final )
+			@RequestParam final String isPwa, @RequestBody final ComponentRequestDTO componentRequestDTO)
 	{
-
+		final ThemeOffersDTO themeOffersDTO = null;
+		try
+		{
+			//
+			System.out.println("Theme offers dto" + componentRequestDTO);
+		}
+		catch (final EtailNonBusinessExceptions e)
+		{
+			ExceptionUtil.etailNonBusinessExceptionHandler(e);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		catch (final EtailBusinessExceptions e)
+		{
+			ExceptionUtil.etailBusinessExceptionHandler(e, null);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		return themeOffersDTO;
 
 	}
-
 }
