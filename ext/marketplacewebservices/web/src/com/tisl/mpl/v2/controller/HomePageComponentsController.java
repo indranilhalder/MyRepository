ffc8517772a173463985b7448bbec77de19b2121
Rajdeep <rajdeep.sahoo@tcs.com>
2018-01-12 20:47:22 +0530

    1120752:NU-34:New controller for components

---
+++ ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/HomePageComponentsController.java
@@ -0,0 +1,72 @@
+/**
+ *
+ */
+package com.tisl.mpl.v2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tisl.mpl.constants.MarketplacecommerceservicesConstants;
+import com.tisl.mpl.exception.EtailBusinessExceptions;
+import com.tisl.mpl.exception.EtailNonBusinessExceptions;
+import com.tisl.mpl.util.ExceptionUtil;
+import com.tisl.mpl.wsdto.ThemeOffersDTO;
+
+
+/**
+ * @author TCS
+ *
+ */
+@Controller
+@RequestMapping(value = "/{baseSiteId}/cms/{userId}/page", headers = "Accept=application/xml,application/json")
+public class HomePageComponentsController
+{
+
+	@RequestMapping(value = "/themeOfferComponent", method = RequestMethod.POST, consumes = "application/json")
+	@ResponseBody
+	public ThemeOffersDTO getThemeOffersComponent(@RequestParam final String mcvid, @RequestParam final String lat,
+			@RequestParam final String lng, @RequestParam final String pincode, @RequestParam final String channel,
+			@RequestParam final String isPwa)
+	{
+		final ThemeOffersDTO themeOffersDTO = new ThemeOffersDTO();
+		try
+		{
+			//
+			themeOffersDTO.setError("error");
+			themeOffersDTO.setErrorCode("6002");
+			themeOffersDTO.setErrorMessage("status message");
+			System.out.println("Theme offers dto" + themeOffersDTO);
+		}
+		catch (final EtailNonBusinessExceptions e)
+		{
+			ExceptionUtil.etailNonBusinessExceptionHandler(e);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		catch (final EtailBusinessExceptions e)
+		{
+			ExceptionUtil.etailBusinessExceptionHandler(e, null);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		return themeOffersDTO;
+	}
+
+}
