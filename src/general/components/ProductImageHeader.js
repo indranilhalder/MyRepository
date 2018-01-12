ee04d6976cac1a3ce85690a403c350f31d8d33e3
unknown <shitanshuroy@gmail.com>
2018-01-12 08:35:04 +0530

    Image takes a logo a header label and description

---
+++ src/general/components/ProductImageHeader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Image } from "xelpmoc-core";
+import styles from "./ProductImageHeader.css";
+export default class ProductImageHeader extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.content}>
+          <Image img={this.props.image} color="tranparent" />
+          {this.props.logo && (
+            <div className={styles.logo}>{this.props.logo}</div>
+          )}
+          {this.props.name && (
+            <div className={styles.name}>{this.props.name}</div>
+          )}
+          {this.props.label && (
+            <div className={styles.label}>{this.props.label}</div>
+          )}
+          {this.props.description && (
+            <div className={styles.description}>{this.props.description}</div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
