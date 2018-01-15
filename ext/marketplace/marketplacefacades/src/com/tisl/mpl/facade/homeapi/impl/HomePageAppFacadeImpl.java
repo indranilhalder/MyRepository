6a3f8353ef3e25f89b558d1fcd3e53aa677407a7
Rajdeep <rajdeep.sahoo@tcs.com>
2018-01-15 22:11:49 +0530

    1120752:NU-35:Theme offers component

--- ext/marketplace/marketplacefacades/src/com/tisl/mpl/facade/homeapi/impl/HomePageAppFacadeImpl.java
+++ ext/marketplace/marketplacefacades/src/com/tisl/mpl/facade/homeapi/impl/HomePageAppFacadeImpl.java
@@ -3,13 +3,18 @@
  */
 package com.tisl.mpl.facade.homeapi.impl;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.beanutils.BeanToPropertyValueTransformer;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tisl.mpl.core.model.BuyBoxModel;
@@ -17,10 +22,10 @@ import com.tisl.mpl.facade.homeapi.HomePageAppFacade;
 import com.tisl.mpl.marketplacecommerceservices.service.BuyBoxService;
 import com.tisl.mpl.service.HomePageAppService;
 import com.tisl.mpl.wsdto.ComponentRequestDTO;
+import com.tisl.mpl.wsdto.HomeProductsDTO;
 import com.tisl.mpl.wsdto.ThemeOffersDTO;
 import com.tisl.mpl.wsdto.ThemeOffersJSONDTO;
 import com.tisl.mpl.wsdto.ThemeOffersRequestDTO;
-import com.tisl.mpl.wsdto.productsDTO;
 
 
 /**
@@ -29,6 +34,8 @@ import com.tisl.mpl.wsdto.productsDTO;
  */
 public class HomePageAppFacadeImpl implements HomePageAppFacade
 {
+	private static final Logger LOG = Logger.getLogger(HomePageAppFacadeImpl.class);
+
 	@Resource(name = "homePageAppService")
 	private HomePageAppService homePageAppService;
 
@@ -51,18 +58,6 @@ public class HomePageAppFacadeImpl implements HomePageAppFacade
 		this.buyBoxService = buyBoxService;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.tisl.mpl.facade.homeapi.HomePageAppFacade#getAdobeTargetData()
-	 */
-	@Override
-	public void getAdobeTargetDataOfferWidget(final ComponentRequestDTO ComponentRequestDTO)
-	{
-		// YTODO Auto-generated method stub
-		final String st = homePageAppService.getAdobeTargetDataOfferWidget(ComponentRequestDTO);
-	}
-
 	/**
 	 * @return the homePageAppService
 	 */
@@ -83,6 +78,19 @@ public class HomePageAppFacadeImpl implements HomePageAppFacade
 	/*
 	 * (non-Javadoc)
 	 *
+	 * @see com.tisl.mpl.facade.homeapi.HomePageAppFacade#getAdobeTargetData()
+	 */
+	@Override
+	public void getAdobeTargetDataOfferWidget(final ComponentRequestDTO ComponentRequestDTO)
+	{
+		// YTODO Auto-generated method stub
+		final String st = homePageAppService.getAdobeTargetDataOfferWidget(ComponentRequestDTO);
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see
 	 * com.tisl.mpl.facade.homeapi.HomePageAppFacade#getThemeOffersComponentDTO(com.tisl.mpl.wsdto.ThemeOffersRequestDTO,
 	 * com.tisl.mpl.wsdto.ThemeOffersJSONDTO)
@@ -92,14 +100,16 @@ public class HomePageAppFacadeImpl implements HomePageAppFacade
 			final String themeOffersJSONString)
 	{
 		final ObjectMapper mapper = new ObjectMapper();
-
+		List<HomeProductsDTO> finalProductsDTO = new ArrayList<HomeProductsDTO>();
+		final ThemeOffersDTO finalThemeOffersDTO = new ThemeOffersDTO();
+		ThemeOffersJSONDTO themeOffersJSONDTO = null;
 		try
 		{
 			final List<String> productCodes = new ArrayList<String>();
 			List<BuyBoxModel> buyBoxModelList = new ArrayList<BuyBoxModel>();
-			final ThemeOffersJSONDTO themeOffersJSONDTO = mapper.readValue(themeOffersJSONString, ThemeOffersJSONDTO.class);
-			final List<productsDTO> productsdTO = themeOffersJSONDTO.getItemIds();
-			for (final productsDTO productdto : productsdTO)
+			themeOffersJSONDTO = mapper.readValue(themeOffersJSONString, ThemeOffersJSONDTO.class);
+			final List<HomeProductsDTO> productsdto = themeOffersJSONDTO.getItemIds();
+			for (final HomeProductsDTO productdto : productsdto)
 			{
 				if (StringUtils.isNotEmpty(productdto.getPrdId()))
 				{
@@ -113,11 +123,75 @@ public class HomePageAppFacadeImpl implements HomePageAppFacade
 				buyBoxModelList = buyBoxService.buyboxPrice(commaSepartedProductCodes);
 			}
 
+			if (CollectionUtils.isNotEmpty(buyBoxModelList))
+			{
+				finalProductsDTO = this.compareDataWithBuyBox(themeOffersJSONDTO.getItemIds(), buyBoxModelList);
+
+			}
+			finalThemeOffersDTO.setOffers(themeOffersJSONDTO.getItems());
+			finalThemeOffersDTO.setProducts(finalProductsDTO);
 		}
-		catch (final Exception e)
+		catch (final IOException | ClassCastException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
 		{
-			e.printStackTrace();
+			LOG.error("HomePageAppFacadeImpl error occured:: " + e.getMessage());
 		}
-		return null;
+
+		return finalThemeOffersDTO;
+	}
+
+	/**
+	 * @param themeOffersJSONDTO
+	 * @param buyBoxModelList
+	 * @return
+	 */
+	private List<HomeProductsDTO> compareDataWithBuyBox(final List<HomeProductsDTO> productsdto,
+			final List<BuyBoxModel> buyBoxModelList) throws ArrayIndexOutOfBoundsException, ClassCastException,
+			IllegalAccessException, InvocationTargetException, NoSuchMethodException
+	{
+		// YTODO Auto-generated method stub
+		final List<HomeProductsDTO> finalProductsDTO = new ArrayList<HomeProductsDTO>();
+		HomeProductsDTO productdto = null;
+		final List<String> listingIds = (List<String>) CollectionUtils.collect(productsdto, new BeanToPropertyValueTransformer(
+				"prdId"));
+		for (final BuyBoxModel buyBoxModel : buyBoxModelList)
+		{
+			final String buyBoxListingIdLwrCase = buyBoxModel.getProduct().toLowerCase();
+			if (listingIds.contains(buyBoxListingIdLwrCase))
+			{
+				productdto = (HomeProductsDTO) CollectionUtils.find(productsdto, new Predicate()
+				{
+					@Override
+					public boolean evaluate(final Object obj)
+					{
+						final HomeProductsDTO finder = (HomeProductsDTO) obj;
+						return finder.getPrdId().equals(buyBoxListingIdLwrCase);
+					}
+				});
+
+				if (buyBoxModel.getAvailable().intValue() > 0)
+				{
+					finalProductsDTO.add(productdto);
+				}
+				else
+				{
+					continue;
+				}
+
+				if (buyBoxModel.getSpecialPrice() != null && Double.compare(buyBoxModel.getSpecialPrice().doubleValue(), 0.0) > 0)
+				{
+					productdto.getDiscountedPrice().setDoubleValue(buyBoxModel.getSpecialPrice());
+					productdto.getDiscountedPrice().setFormattedValue(buyBoxModel.getSpecialPrice().toString());
+				}
+				if (buyBoxModel.getMrp() != null && Double.compare(buyBoxModel.getMrp().doubleValue(), 0.0) > 0)
+				{
+					productdto.getMrpPrice().setDoubleValue(buyBoxModel.getMrp());
+					productdto.getMrpPrice().setFormattedValue(buyBoxModel.getMrp().toString());
+				}
+
+
+			}
+
+		}
+		return finalProductsDTO;
 	}
 }
--- ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/HomePageComponentsController.java
+++ ext/marketplacewebservices/web/src/com/tisl/mpl/v2/controller/HomePageComponentsController.java
@@ -3,6 +3,8 @@
  */
 package com.tisl.mpl.v2.controller;
 
+import de.hybris.platform.util.localization.Localization;
+
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
@@ -13,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tisl.mpl.constants.MarketplacecommerceservicesConstants;
+import com.tisl.mpl.exception.EtailBusinessExceptions;
+import com.tisl.mpl.exception.EtailNonBusinessExceptions;
 import com.tisl.mpl.facade.homeapi.HomePageAppFacade;
+import com.tisl.mpl.util.ExceptionUtil;
 import com.tisl.mpl.wsdto.ThemeOffersDTO;
 import com.tisl.mpl.wsdto.ThemeOffersRequestDTO;
 
@@ -40,12 +46,48 @@ public class HomePageComponentsController
 		ThemeOffersDTO themeOffersDTO = new ThemeOffersDTO();
 
 		final String themeOfferJsonString = themeOffersRequestDTO.getContent();
-		themeOffersDTO = homePageAppFacade.getThemeOffersComponentDTO(themeOffersRequestDTO, themeOfferJsonString);
-		themeOffersDTO.setError("error");
-		themeOffersDTO.setErrorCode("6002");
-		themeOffersDTO.setErrorMessage("status message");
-		System.out.println("Theme offers dto" + themeOffersDTO);
-
+		try
+		{
+			themeOffersDTO = homePageAppFacade.getThemeOffersComponentDTO(themeOffersRequestDTO, themeOfferJsonString);
+			//here we have to decide whether we are going to use  datamapper  or not
+			//		themeOffersDTO.setError("error");
+			//		themeOffersDTO.setErrorCode("6002");
+			//		themeOffersDTO.setErrorMessage("status message");
+			//		System.out.println("Theme offers dto" + themeOffersDTO);
+		}
+		catch (final EtailNonBusinessExceptions e)
+		{
+			ExceptionUtil.etailNonBusinessExceptionHandler(e);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		catch (final EtailBusinessExceptions e)
+		{
+			ExceptionUtil.etailBusinessExceptionHandler(e, null);
+			if (null != e.getErrorMessage())
+			{
+				themeOffersDTO.setError(e.getErrorMessage());
+			}
+			if (null != e.getErrorCode())
+			{
+				themeOffersDTO.setErrorCode(e.getErrorCode());
+			}
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
+		catch (final Exception e)
+		{
+			ExceptionUtil.getCustomizedExceptionTrace(e);
+			themeOffersDTO.setError(Localization.getLocalizedString(MarketplacecommerceservicesConstants.B9004));
+			themeOffersDTO.setErrorCode(MarketplacecommerceservicesConstants.B9004);
+			themeOffersDTO.setStatus(MarketplacecommerceservicesConstants.ERROR_FLAG);
+		}
 		return themeOffersDTO;
 	}
 }
