9b9ec5b627602c3e12346d2d53f93c332b092912
abi121 <abhishek@xelpmoc.in>
2018-01-17 17:15:33 +0530

    onSelectcheckMArck

--- src/general/components/BrandImage.js
+++ src/general/components/BrandImage.js
@@ -1,31 +1,30 @@
 import React from "react";
 import styles from "./BrandImage.css";
 import { Image } from "xelpmoc-core";
-import PropTypes from "prop-types";
 export default class BrandImage extends React.Component {
-  onSelected = () => {
-    if (this.props.onSelect) {
-      this.props.onSelect();
+  handleClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
     }
-  };
+  }
   render() {
+    let iconClass = styles.checkIcon;
+    if (this.props.selected) {
+      iconClass = styles.checkIconActive;
+    }
     return (
-      <div className={styles.base} onClick={this.onSelected}>
-        <div className={this.props.selected ? styles.active : styles.inactive}>
-          <div className={styles.imageHolder}>
-            <Image image={this.props.source} />
+      <div>
+        <center>
+          <div className={styles.base} onClick={() => this.handleClick()}>
+            <div className={styles.circle}>
+              <div className={iconClass} />
+              <div className={styles.imageHolder}>
+                <Image image={this.props.image} />
+              </div>
+            </div>
           </div>
-        </div>
+        </center>
       </div>
     );
   }
 }
-BrandImage.propTypes = {
-  source: PropTypes.string,
-  selected: PropTypes.bool,
-  onSelect: PropTypes.func
-};
-BrandImage.defaultProps = {
-  source: "",
-  selected: false
-};
