3ea57f0aa9db37b5e4bb330cb509fd9068bfca4e
abi121 <abhishek@xelpmoc.in>
2018-01-18 14:30:02 +0530

    work On Single Select Component

---
+++ src/general/components/SingleSelect.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./SingleSelect.css";
+import Gym from "./img/Gym.svg";
+import { Image } from "xelpmoc-core";
+import PropTypes from "prop-types";
+
+export default class SingleSelect extends React.Component {
+  SingleSelect() {
+    if (this.props.onClick) {
+      this.props.onClick(this.props.text);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div
+          className={styles.singleSelectBase}
+          onClick={() => this.SingleSelect()}
+        >
+          <div className={styles.SelectIcon}>
+            <Image image={this.props.image} color="transparent" />
+          </div>
+        </div>
+        <div className={styles.Gym}>{this.props.text}</div>
+      </div>
+    );
+  }
+}
+SingleSelect.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func
+};
