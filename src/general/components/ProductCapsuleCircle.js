a4f1858e83d0b8b041c6dfafa3da38e1947fde03
Truptimayee031 <shruti@xelpmoc.in>
2018-01-18 18:07:26 +0530

    saves products

---
+++ src/general/components/ProductCapsuleCircle.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./ProductCapsuleCircle.css";
+import { Image } from "xelpmoc-core";
+import PropTypes from "prop-types";
+
+export default class ProductCapsuleCircle extends React.Component {
+  handleClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
+  render() {
+    return (
+      <div className={styles.base} onClick={() => this.handleClick}>
+        <div className={styles.imageHolder}>
+          <Image image={this.props.image} />
+        </div>
+      </div>
+    );
+  }
+}
+ProductCapsuleCircle.propTypes = {
+  onClick: PropTypes.func
+};
