3a4c319efd16d3f64734e0edddbc2b6225c03f21
Venna <prasad.v@techouts.com>
2018-01-23 20:54:16 +0530

    EGV Mobile WEb Services Changes

---
+++ ext/marketplacewebservices/src/com/tisl/mpl/service/MplEgvWalletService.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.tisl.mpl.service;
+
+import de.hybris.platform.core.model.order.AbstractOrderModel;
+import de.hybris.platform.core.model.user.CustomerModel;
+
+import com.tisl.mpl.pojo.response.CustomerWalletDetailResponse;
+import com.tisl.mpl.pojo.response.QCCustomerRegisterResponse;
+import com.tisl.mpl.wsdto.ApplyCliqCashWsDto;
+import com.tisl.mpl.wsdto.EgvWalletCreateResponceWsDTO;
+import com.tisl.mpl.wsdto.UserCliqCashWsDto;
+
+/**
+ * @author TUL
+ *
+ */
+public interface MplEgvWalletService
+{
+	public QCCustomerRegisterResponse createWalletContainer(CustomerModel currentCustomer);
+    
+	public EgvWalletCreateResponceWsDTO verifyOtpAndCreateWallet(CustomerModel currentCustomer,String otp);
+	public UserCliqCashWsDto getCustomerWalletAmount(CustomerWalletDetailResponse customerWalletDetailData);
+	public UserCliqCashWsDto getUserCliqCashDetails(CustomerModel currentCustomer);
+	ApplyCliqCashWsDto applyCLiqCash(AbstractOrderModel order,Double walletAmount);
+
+
+	
+	//public UserCliqCashWsDto getUserCliqCashDetails(CustomerModel currentCustomer);
+
+
+}
