f1bbbf99c7aad6b7e0e9be12c3865c2f0c5af0e9
1022570 <1022570@01HW587462.India.TCS.com>
2018-01-24 19:44:00 +0530

    1022570:TISUAT-6259:code fix

--- ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/daos/impl/OrderModelDaoImpl.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/daos/impl/OrderModelDaoImpl.java
@@ -159,7 +159,7 @@ public class OrderModelDaoImpl implements OrderModelDao
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see com.tisl.mpl.marketplacecommerceservices.daos.OrderModelDao#getOrder(java.util.Date)
 	 */
 	@Override
@@ -207,7 +207,7 @@ public class OrderModelDaoImpl implements OrderModelDao
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see com.tisl.mpl.marketplacecommerceservices.daos.OrderModelDao#getOrder(java.util.Date)
 	 */
 	@Override
@@ -329,9 +329,7 @@ public class OrderModelDaoImpl implements OrderModelDao
 			if (orderId != null)
 			{
 				orderModel.setCode(orderId);
-				//fix for SDI-3630 -- fix for handling orders with cancelled transaction--starts
-				orderModel.setVersionID(null);
-				//fix for SDI-3630 -- fix for handling orders with cancelled transaction--ends
+
 				orderModel = flexibleSearchService.getModelByExample(orderModel);
 			}
 		}
@@ -348,7 +346,7 @@ public class OrderModelDaoImpl implements OrderModelDao
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see com.tisl.mpl.marketplacecommerceservices.daos.OrderModelDao#getOrderByAgent(java.lang.String,
 	 * java.lang.String)
 	 */
