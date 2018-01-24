595bc71070f078d85c2aa671ad4dc4b1ded6f565
ShitanshuRoy <shitanshuroy@gmail.com>
2018-01-24 11:07:45 +0530

    removed logo from utils

--- src/home/components/utils.js
+++ src/home/components/utils.js
@@ -2,7 +2,6 @@ export function transformData(datum) {
   return {
     image: datum.imageURL,
     video: datum.video,
-    logo: datum.brandLogo,
     title: datum.title,
     description: datum.description,
     price: datum.mrpPrice && datum.mrpPrice.formattedValue,
