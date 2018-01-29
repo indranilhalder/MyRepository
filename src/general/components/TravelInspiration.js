23bd6e12ec3796380b732e000470af938cc57168
gangeshkumar <gangesh@xelpmoc.in>
2018-01-29 12:08:32 +0530

    made TravelInspiration component

--- src/general/components/TravelInspiration.js
+++ src/general/components/TravelInspiration.js
@@ -2,21 +2,31 @@ import React from "react";
 import styles from "./TravelInspiration.css";
 import { Image } from "xelpmoc-core";
 import PropTypes from "prop-types";
-import image from "./img/travelb.jpg";
-import travelBackImage from "./img/backgroungt.jpg";
+import travelBackImage from "./img/background.jpg";
 export default class TravelInspiration extends React.Component {
+  handleClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.travelMain}>
-          <Image image={this.props.travelBackImage} color="transparent" />
-          <div className={styles.travelHolder}>
-            <Image image={this.props.travelImage} color="transparent" />
-            <div className={styles.overlay}>
-              <div className={styles.header}>{this.props.travelHeaderText}</div>
-              <div className={styles.label}>{this.props.travelText}</div>
+        <div className={styles.travelHolder}>
+          <Image image={this.props.travelImage} color="transparent" />
+          <div className={styles.overlay}>
+            <div className={styles.header}>{this.props.travelHeaderText}</div>
+            <div className={styles.label}>
+              {this.props.travelText}
               <div className={styles.buttonBox}>
-                <div className={styles.button}>{this.props.readMore}</div>
+                <div
+                  className={styles.button}
+                  onClick={() => {
+                    this.handleClick();
+                  }}
+                >
+                  {this.props.readMore}
+                </div>
               </div>
             </div>
           </div>
@@ -26,15 +36,14 @@ export default class TravelInspiration extends React.Component {
   }
 }
 TravelInspiration.propTypes = {
-  traveImage: PropTypes.string,
-  travelBackImage: PropTypes.string,
+  travelImage: PropTypes.string,
   travelHeaderText: PropTypes.string,
   travelText: PropTypes.string,
-  readMore: PropTypes.string
+  readMore: PropTypes.string,
+  onClick: PropTypes.func
 };
 TravelInspiration.defaultProps = {
-  travelImage: image,
-  travelBackImage: travelBackImage,
+  travelImage: travelBackImage,
   readMore: "Read More",
   travelHeaderText: "Parisian Chic",
   travelText:
