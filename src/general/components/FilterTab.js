8e652bc79982ed0caca592a59cc4ebe71a56d992
abi121 <abhishek@xelpmoc.in>
2018-01-29 18:47:36 +0530

    selected Filter

---
+++ src/general/components/FilterTab.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./FilterTab.css";
+import PropTypes from "prop-types";
+
+export default class SelectedFilter extends React.Component {
+  handleClick = () => {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  };
+  render() {
+    let classActive = styles.dataHolder;
+    if (this.props.selected) {
+      classActive = styles.selectedActive;
+    }
+    return (
+      <div className={styles.base}>
+        <div className={classActive} onClick={() => this.handleClick()}>
+          <div className={styles.selectedText}>{this.props.text}</div>
+          {this.props.value && (
+            <div className={styles.selected}>{this.props.value}</div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+SelectedFilter.propTypes = {
+  text: PropTypes.string,
+  value: PropTypes.string,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func
+};
