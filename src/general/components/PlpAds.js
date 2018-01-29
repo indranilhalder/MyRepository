189f604072e3d2723d3327eebc6fac04776be8c2
gangeshkumar <gangesh@xelpmoc.in>
2018-01-29 17:26:22 +0530

    remove File.svg and change class

--- src/general/components/PlpAds.js
+++ src/general/components/PlpAds.js
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./PlpAds.css";
 import { Icon } from "xelpmoc-core";
 import PropTypes from "prop-types";
-import iconImageURL from "./img/Fil.svg";
 import Button from "../../general/components/Button";
 
 export default class PlpAds extends React.Component {
@@ -16,14 +15,14 @@ export default class PlpAds extends React.Component {
       <div className={styles.base}>
         <div className={styles.plpInnerBox}>
           <div className={styles.icon}>
-            <Icon image={iconImageURL} size={40} />
+            <Icon image={this.props.Image} size={40} />
           </div>
-          <div className={styles.plpBox}>{this.props.plpText}</div>
+          <div className={styles.plpBox}>{this.props.Text}</div>
           <div className={styles.buttonHolder}>
             <Button
               type="hollow"
               color="#fff"
-              label={"See now"}
+              label={this.props.buttonText}
               onClick={() => this.handleClick()}
               width={100}
             />
@@ -34,12 +33,12 @@ export default class PlpAds extends React.Component {
   }
 }
 PlpAds.propTypes = {
-  plpText: PropTypes.string,
-  buttonText: PropTypes.string,
+  Text: PropTypes.string,
+  Image: PropTypes.string,
   onClick: PropTypes.func
 };
 PlpAds.defaultProps = {
-  bannerImage: iconImageURL,
-  plpText: "See the best sellers in Women Top Wear",
+  Image: Image,
+  Text: "See the best sellers in Women Top Wear",
   buttonText: "See now"
 };
