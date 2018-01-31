a920a66959ca8a398d072f6c0356d9403deeb612
abi121 <abhishek@xelpmoc.in>
2018-01-31 10:17:32 +0530

    add props type in centerAddjs file

--- src/general/components/CenterAdd.js
+++ src/general/components/CenterAdd.js
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./CenterAdd.css";
+import PropTypes from "prop-types";
 
 export default class CenterAdd extends React.Component {
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.textHolder}>M</div>
+        <div className={styles.textHolder}>{this.props.text}</div>
       </div>
     );
   }
 }
+
+CenterAdd.propTypes = {
+  text: PropTypes.string
+};
