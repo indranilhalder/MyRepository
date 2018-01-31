eb44b67bcb804d3af73a6987a98567d22ae7186a
Truptimayee031 <shruti@xelpmoc.in>
2018-01-31 12:30:48 +0530

    mediaquery device width

--- src/general/components/ConnectButton.js
+++ src/general/components/ConnectButton.js
@@ -1,19 +1,39 @@
 import React from "react";
 import { Image } from "xelpmoc-core";
 import PropTypes from "prop-types";
+import MediaQuery from "react-responsive";
 import styles from "./ConnectButton.css";
 import { Icon, CircleButton } from "xelpmoc-core";
 import iconImageURL from "../../home/components/img/Connect_Small.svg";
 export default class ConnectButton extends React.Component {
+  handleClick = () => {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  };
   render() {
     return (
       <div className={styles.connectButtonHolder}>
-        <CircleButton
-          color={"transparent"}
-          size={50}
-          icon={<Icon image={iconImageURL} size={30} />}
-        />
+        <MediaQuery query="(min-device-width: 1025px)">
+          <CircleButton
+            color={"transparent"}
+            size={40}
+            icon={<Icon image={iconImageURL} size={25} />}
+            onClick={this.handleClick}
+          />
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 1024px)">
+          <CircleButton
+            color={"transparent"}
+            size={50}
+            icon={<Icon image={iconImageURL} size={30} />}
+            onClick={this.handleClick}
+          />
+        </MediaQuery>
       </div>
     );
   }
 }
+ConnectButton.propTypes = {
+  onClick: PropTypes.func
+};
--- src/general/components/ProductImage.js
+++ src/general/components/ProductImage.js
@@ -4,17 +4,25 @@ import { Image } from "xelpmoc-core";
 import PropTypes from "prop-types";
 import ConnectButton from "./ConnectButton.js";
 export default class ProductImage extends React.Component {
+  handleClick = () => {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  };
   render() {
     return (
       <div className={styles.base}>
         <div className={styles.imageHolder}>
           <Image image={this.props.image} />
-          {this.props.ConnectButton && <ConnectButton />}
         </div>
+        {this.props.connectButton && (
+          <ConnectButton onClick={this.handleClick} />
+        )}
       </div>
     );
   }
 }
 ProductImage.propTypes = {
-  image: PropTypes.string
+  image: PropTypes.string,
+  ConnectButton: PropTypes.element
 };
