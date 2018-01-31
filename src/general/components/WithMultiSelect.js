cc9ab92fdb4b0adee920128e5694873e16096de4
ShitanshuRoy <shitanshuroy@gmail.com>
2018-01-31 18:02:03 +0530

    removed space

--- src/general/components/WithMultiSelect.js
+++ src/general/components/WithMultiSelect.js
@@ -30,7 +30,6 @@ export default function withMultiSelect(Component, ownProps) {
     }
     render() {
       const children = this.props.children;
-
       const childrenWithProps = React.Children.map(children, (child, i) => {
         return React.cloneElement(child, {
           selected: this.state.selected.includes(child.props.value),
