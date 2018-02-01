afa70c599dfe9863b2536123c80fba31ceaa43a2
Truptimayee031 <shruti@xelpmoc.in>
2018-02-01 17:54:59 +0530

    product list view

--- src/general/components/ProductModule.js
+++ src/general/components/ProductModule.js
@@ -3,10 +3,10 @@ import ProductImage from "./ProductImage";
 import ProductDescription from "./ProductDescription";
 import PropTypes from "prop-types";
 import ConnectButton from "./ConnectButton.js";
-
 import styles from "./ProductModule.css";
 import downloadIcon from "./img/download.svg";
 import downloadIconWhite from "./img/downloadWhite.svg";
+import ProductInfo from "./ProductInfo.js";
 export default class ProductModule extends React.Component {
   onDownload = () => {
     if (this.props.onDownload) {
@@ -23,7 +23,6 @@ export default class ProductModule extends React.Component {
       this.props.onConnect();
     }
   };
-
   render() {
     let downloadImage = downloadIcon;
     if (this.props.isWhite) {
@@ -31,18 +30,38 @@ export default class ProductModule extends React.Component {
     }
     return (
       <div className={styles.base}>
-        <div className={styles.imageHolder} onClick={this.onClick}>
+        {/* {this.props.view === "grid" && ( */}
+        <div
+          className={
+            this.props.view === "grid"
+              ? styles.imageHolder
+              : styles.ListimageHolder
+          }
+          onClick={this.onClick}
+        >
           <ProductImage image={this.props.productImage} />
           {this.props.onConnect && (
             <ConnectButton onClick={this.handleConnect} />
           )}
         </div>
-        <div className={styles.content}>
+        <div
+          className={
+            this.props.view === "grid" ? styles.content : styles.Listcontent
+          }
+        >
           <ProductDescription
             {...this.props}
             icon={downloadImage}
             onDownload={this.onDownload}
           />
+          {this.props.view === "list" && (
+            <ProductInfo
+              averageRating={4}
+              totalNoOfReviews="65"
+              offerText="25% offers from Rs. 35,000"
+              bestDeliveryInfo="Tuesday, Sep 12"
+            />
+          )}
         </div>
       </div>
     );
@@ -54,4 +73,7 @@ ProductModule.propTypes = {
   onDownload: PropTypes.func,
   isWhite: PropTypes.bool,
   onConnect: PropTypes.func
+};
+ProductModule.defaultProps = {
+  view: "grid"
 };
