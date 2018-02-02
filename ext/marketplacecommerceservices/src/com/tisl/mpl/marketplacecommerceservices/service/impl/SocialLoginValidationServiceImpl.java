e28471d3656b69f674db69576b185d611f2df849
985717 <gupta.abhijit@tcs.com>
2018-02-02 16:15:28 +0530

    985717:<>:Service classes for social access token validation

---
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/marketplacecommerceservices/service/impl/SocialLoginValidationServiceImpl.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package com.tisl.mpl.marketplacecommerceservices.service.impl;
+
+
+
+import com.tisl.mpl.marketplacecommerceservices.service.SocialLoginValidationService;
+
+
+/**
+ * @author TCS
+ *
+ */
+
+public class SocialLoginValidationServiceImpl implements SocialLoginValidationService
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.tisl.mpl.marketplacecommerceservices.service.SocialLoginValidationService#test()
+	 */
+	@Override
+	public String test()
+	{
+		// YTODO Auto-generated method stub
+		return "Perfectly fine";
+	}
+	//Implementation classes
+}
