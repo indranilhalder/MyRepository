8a526264f4eb563e67f83593b90459e30f9ba544
gangeshkumar <gangesh@xelpmoc.in>
2018-02-05 17:02:18 +0530

    made ReviewPage component

--- src/general/components/ReviewPage.js
+++ src/general/components/ReviewPage.js
@@ -1,34 +1,32 @@
 import React from "react";
 import styles from "./ReviewPage.css";
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 import StarRating from "./StarRating.js";
 export default class ReviewPage extends React.Component {
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.reviewHolder}>
+        {this.props.rating && (
           <div className={styles.starHolder}>
-            <StarRating />
+            <StarRating averageRating={this.props.rating} />
           </div>
-
-          {this.props.heading && (
-            <div className={styles.heading}>{this.props.heading}</div>
-          )}
-
-          {this.props.text && (
-            <div className={styles.text}>{this.props.text}</div>
-          )}
-
-          {this.props.label && (
-            <div className={styles.dateTimeBox}>{this.props.label}</div>
-          )}
-        </div>
+        )}
+        {this.props.heading && (
+          <div className={styles.heading}>{this.props.heading}</div>
+        )}
+        {this.props.text && (
+          <div className={styles.text}>{this.props.text}</div>
+        )}
+        {this.props.label && (
+          <div className={styles.dateTimeBox}>{this.props.label}</div>
+        )}
       </div>
     );
   }
 }
 ReviewPage.propTypes = {
-  text: propTypes.string,
-  label: propTypes.string,
-  heading: propTypes.string
+  text: PropTypes.string,
+  label: PropTypes.string,
+  heading: PropTypes.string,
+  rating: PropTypes.number
 };
