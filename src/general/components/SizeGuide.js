f37c58868ea264b0267a65c4f5d4aada5c364c54
gangeshkumar <gangesh@xelpmoc.in>
2018-02-05 14:52:51 +0530

    made sizeguide component

---
+++ src/general/components/SizeGuide.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./SizeGuide.css";
+import propTypes from "prop-types";
+export default class SizeGuide extends React.Component {
+  handleClick(val) {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    const data = this.props.data;
+    console.log(data);
+    return (
+      <div className={styles.base}>
+        <div className={styles.holder}>
+          {data.map((datum, i) => {
+            return (
+              <div className={styles.sizeBox} key={i}>
+                <div className={styles.sizeText} key={datum.key}>
+                  {datum.key}
+                </div>
+                <div className={styles.sizeNumber} value={datum.value}>
+                  {datum.value}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+SizeGuide.propTypes = {
+  key: propTypes.string,
+  value: propTypes.number
+};
