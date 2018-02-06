3c4767b2d6b43ce7dd2e7372bac58e7de4eec3c4
Truptimayee031 <shruti@xelpmoc.in>
2018-02-06 14:49:01 +0530

    input focus and blur

--- src/general/components/Input2.js
+++ src/general/components/Input2.js
@@ -2,48 +2,64 @@ import React from "react";
 import styles from "./Input2.css";
 import PropTypes from "prop-types";
 export default class Input2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false
+    };
+  }
   handleFocus(event) {
     if (this.props.onFocus) {
       this.props.onFocus(event);
     }
+    this.setState({ focused: true });
+  }
+  handleBlur(event) {
+    if (this.props.onBlur) {
+      this.props.onBlur(event);
+    }
+    this.setState({ focused: false });
   }
   render() {
     return (
       <div className={styles.container}>
-        <div
-          className={this.props.hollow ? styles.hollow : styles.box}
-          style={{
-            paddingLeft: `${this.props.leftchildsize - 10}px`,
-            paddingRight: `${this.props.rightchildsize - 10}px`,
-            height: this.props.hollow
-              ? `${this.props.hollowheight}px`
-              : `${this.props.boxheight}px`
-          }}
-        >
-          <input
-            type="text"
-            className={styles.inputBox}
-            hollow={this.props.hollow}
-            onFocus={event => this.handleFocus(event)}
-            {...this.props}
-          />
+        <div className={this.state.focused ? styles.focused : styles.wrapper}>
+          <div
+            className={this.props.hollow ? styles.hollow : styles.box}
+            style={{
+              paddingLeft: `${this.props.leftchildsize - 10}px`,
+              paddingRight: `${this.props.rightchildsize - 10}px`,
+              height: this.props.hollow
+                ? `${this.props.hollowheight}px`
+                : `${this.props.boxheight}px`
+            }}
+          >
+            <input
+              type="text"
+              className={styles.inputBox}
+              hollow={this.props.hollow}
+              onFocus={event => this.handleFocus(event)}
+              onBlur={event => this.handleBlur(event)}
+              {...this.props}
+            />
+          </div>
+          {this.props.leftchild && (
+            <div
+              className={styles.boxIconLeft}
+              style={{ width: this.props.leftchildsize }}
+            >
+              {this.props.leftchild}
+            </div>
+          )}
+          {this.props.rightchild && (
+            <div
+              className={styles.boxIconRight}
+              style={{ width: this.props.rightchildsize }}
+            >
+              {this.props.rightchild}
+            </div>
+          )}
         </div>
-        {this.props.leftchild && (
-          <div
-            className={styles.boxIconLeft}
-            style={{ width: this.props.leftchildsize }}
-          >
-            {this.props.leftchild}
-          </div>
-        )}
-        {this.props.rightchild && (
-          <div
-            className={styles.boxIconRight}
-            style={{ width: this.props.rightchildsize }}
-          >
-            {this.props.rightchild}
-          </div>
-        )}
       </div>
     );
   }
