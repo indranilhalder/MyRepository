82a93a2bc023b5eeb2241840061c3f8f3be23ce9
john2193 <janardan@xelpmoc.in>
2018-02-07 13:59:33 +0530

    cupon details

---
+++ src/pdp/components/CuponDetails.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./CuponDetails.css";
+import PropTypes from "prop-types";
+export default class CuponDetails extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        {this.props.productOfferPromotion &&
+          this.props.productOfferPromotion.map((promotion, i) => {
+            return (
+              <div className={styles.cuponCard} key={i}>
+                <div className={styles.headerText}>
+                  {promotion.promotionTitle}
+                </div>
+                <div className={styles.promotionDetailsText}>
+                  {promotion.promotionDetail}
+                </div>
+                <div className={styles.dataHolder}>
+                  <div className={styles.amountExpireHolder}>
+                    <div className={styles.dataHeader}>Valid till</div>
+                    <div className={styles.dataInformation}>
+                      {promotion.validTill.formattedDate}
+                    </div>
+                  </div>
+                  <div className={styles.amountExpireHolder}>
+                    <div className={styles.dataHeader}>Min.bag amount</div>
+                    <div className={styles.dataInformation}>
+                      Rs.
+                      {promotion.validTill.amount}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+}
