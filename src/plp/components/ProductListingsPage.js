469de54421c11f6cb2a0bf16626ab34aa09eaf3d
Basavannavva <Basavannevva@xelpmoc.in>
2018-02-08 13:02:42 +0530

    changes done

--- src/plp/components/ProductListingsPage.js
+++ src/plp/components/ProductListingsPage.js
@@ -20,7 +20,7 @@ class ProductListingsPage extends Component {
     } else if (this.props.productListings !== null) {
       return (
         <Plp
-          {...this.props}
+          history={this.props.history}
           searchresult={this.props.productListings.searchresult}
           facetData={this.props.productListings.facetdata}
           showSort={this.props.showSort}
