acd426def808098dc2c4ac90dcd2f26cd45472e5
ShitanshuRoy <shitanshuroy@gmail.com>
2018-02-12 16:50:03 +0530

    removed console

--- src/pdp/containers/ProductDescriptionContainer.js
+++ src/pdp/containers/ProductDescriptionContainer.js
@@ -49,7 +49,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 const mapStateToProps = state => {
-  console.log(state);
   return {
     loading: state.productDescription.loading,
     productDetails: state.productDescription.productDetails
