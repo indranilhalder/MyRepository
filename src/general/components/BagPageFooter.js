bf076d84b8d6237341a87b7caed7ddcd67f4ca00
abi121 <abhishek@xelpmoc.in>
2018-02-12 17:53:14 +0530

    add onClick functions

--- src/general/components/BagPageFooter.js
+++ src/general/components/BagPageFooter.js
@@ -3,6 +3,16 @@ import styles from "./BagPageFooter.css";
 import { Icon } from "xelpmoc-core";
 import PropTypes from "prop-types";
 export default class BagPageFooter extends React.Component {
+  handleClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
+  removeClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
   render() {
     return (
       <div className={styles.base}>
@@ -11,9 +21,19 @@ export default class BagPageFooter extends React.Component {
             <div className={styles.iconHolder}>
               <Icon image={this.props.image} />
             </div>
-            <div className={styles.saveLabel}>{this.props.saveText}</div>
+            <div
+              className={styles.saveLabel}
+              onClick={() => this.handleClick()}
+            >
+              {this.props.saveText}
+            </div>
           </div>
-          <div className={styles.removeLabel}>{this.props.removeText}</div>
+          <div
+            className={styles.removeLabel}
+            onClick={() => this.removeClick()}
+          >
+            {this.props.removeText}
+          </div>
         </div>
       </div>
     );
