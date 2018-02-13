e4431f6bedd45f4c71a94b4a8872083d4ec13605
abi121 <abhishek@xelpmoc.in>
2018-02-13 10:52:47 +0530

    chnage in css class name

--- src/general/components/BagPageFooter.js
+++ src/general/components/BagPageFooter.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./BagPageFooter.css";
 import { Icon } from "xelpmoc-core";
 import PropTypes from "prop-types";
+import image from "./img/download.svg";
 export default class BagPageFooter extends React.Component {
   onSave() {
     if (this.props.onSave) {
@@ -19,7 +20,7 @@ export default class BagPageFooter extends React.Component {
         <div className={styles.wrapper}>
           <div className={styles.saveButton} onClick={() => this.onSave()}>
             <div className={styles.iconHolder}>
-              <Icon image={this.props.image} />
+              <Icon image={image} />
             </div>
             <div className={styles.saveLabel}>{this.props.saveText}</div>
           </div>
