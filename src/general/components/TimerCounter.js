ac149b0ba75a42db9b6c765ae59c43b4d9496c40
abi121 <abhishek@xelpmoc.in>
2018-02-13 17:00:40 +0530

    Add days in Timer Component

--- src/general/components/TimerCounter.js
+++ src/general/components/TimerCounter.js
@@ -1,20 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Countdown from "react-countdown-now";
 import PropTypes from "prop-types";
-
-const renderTimer = ({ hours, minutes, seconds }) => {
+const renderTimer = ({ days, hours, minutes, seconds }) => {
+  const finalHour = days * 24 + parseInt(hours, 10);
   return (
     <span>
-      {hours}:{minutes}:{seconds}
+      {finalHour}:{minutes}:{seconds}
     </span>
   );
 };
 export default class Counter extends React.Component {
   render() {
     let endTime = this.props.endTime;
-    let finalTime = new Date(endTime).getTime();
-    return <Countdown date={Date.now() + finalTime} renderer={renderTimer} />;
+    let endDate = new Date(endTime).getDate();
+    let finalTime = new Date().getDate();
+    let differDate = endDate - finalTime;
+    let dateObj = differDate * 3600000 * 24;
+    let hours = new Date(endTime).getHours();
+    let finalHour = hours * 3600000;
+    let getMinutes = new Date(endTime).getMinutes();
+    let minutes = getMinutes * 60000;
+    let seconds = new Date(endTime).getSeconds();
+    let calculatedSeconds = seconds * 1000;
+    let finalEndDate = finalHour + dateObj + minutes + calculatedSeconds;
+    return (
+      <Countdown date={Date.now() + finalEndDate} renderer={renderTimer} />
+    );
   }
 }
 Counter.propTypes = {
