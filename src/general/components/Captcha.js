77a8f92b1881c84f68b850ac479e3886e2667ab5
Basavannavva <Basavannevva@xelpmoc.in>
2018-02-14 17:23:24 +0530

    Added Captcha

---
+++ src/general/components/Captcha.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Recaptcha from "react-grecaptcha";
+import Config from "../../lib/config";
+const verifyCallback = response => console.log(response);
+const expiredCallback = () => {};
+const LOCALE = "en";
+const THEME = "light";
+class Captcha extends Component {
+  render() {
+    return (
+      <div>
+        <Recaptcha
+          sitekey={Config.reCAPTCHASiteKey}
+          callback={verifyCallback}
+          expiredCallback={expiredCallback}
+          locale={LOCALE}
+          data-theme={THEME}
+        />
+      </div>
+    );
+  }
+}
+
+export default Captcha;
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,7 @@
 export default {
   facebook: "1444012285724567",
   google:
-    "367761167032-apbr4v0nndom1cafs9inrrnkk7iag5be.apps.googleusercontent.com"
+    "367761167032-apbr4v0nndom1cafs9inrrnkk7iag5be.apps.googleusercontent.com",
+  reCAPTCHASiteKey: "6LeSUUYUAAAAAEGquZLRc-8D0ysCnWfhafxi7zv6",
+  reCAPTCHASecreteKey: "6LeSUUYUAAAAAPMh2nNS4r3KG_S9KJq74eNkXuhp"
 };
