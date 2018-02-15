965ff96a111c2881c3374013df0787d6bbb9bfb8
abi121 <abhishek@xelpmoc.in>
2018-02-15 15:26:17 +0530

    changes in className

---
+++ src/Cart/components/DeliveryAddressCopy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./DeliveryAddressCopy.css";
+import CheckBox from "../../general/components/CheckBox.js";
+export default class DeliveryAddressCopy extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.confirm}>
+          {this.props.confirmTitle}
+          <div className={styles.circle}> 1</div>
+        </div>
+        <div className={styles.titleContainer}>
+          <div className={styles.titleAddress}>{this.props.addressTitle}</div>
+          <div className={styles.titleDescription}>
+            {this.props.addressDescription}
+            <div className={styles.checkCircle}>
+              <CheckBox selected={this.props.selected} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
