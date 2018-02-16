38387bb24ba207db5e87b77d259609b03814e7b3
Basavannavva <Basavannevva@xelpmoc.in>
2018-02-16 13:10:20 +0530

    removed unwanted code

--- src/cart/components/CartPage.js
+++ src/cart/components/CartPage.js
@@ -3,14 +3,7 @@ import React, { Component } from "react";
 class CartPage extends Component {
   componentWillMount() {
     this.props.getUserCart();
-    this.props.applyCoupon();
-    this.props.getUserAddress();
-    this.props.selectDeliveryModes();
-
-    let cartValues = {};
-    cartValues.total = 9000.0;
-    this.props.getEmiBankDetails(cartValues);
-
+    this.props.getEmiBankDetails();
     this.props.getNetBankDetails();
   }
   render() {
