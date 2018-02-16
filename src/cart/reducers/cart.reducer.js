22b738b16904273c63fc52ea69ce3bb4c61c921f
Basavannavva <Basavannevva@xelpmoc.in>
2018-02-16 19:20:54 +0530

    Changes done

--- src/cart/reducers/cart.reducer.js
+++ src/cart/reducers/cart.reducer.js
@@ -4,8 +4,18 @@ const cart = (
     status: null,
     error: null,
     loading: false,
+
     userCart: null,
+    userCartStatus: null,
+    userCartError: null,
+
     cartDetails: null,
+    cartDetailsStatus: null,
+    cartDetailsError: null,
+
+    couponStatus: null,
+    couponError: null,
+
     deliveryModes: null,
     userAddress: null,
     netBankDetails: null,
@@ -16,79 +26,79 @@ const cart = (
   switch (action.type) {
     case cartActions.PRODUCT_CART_DETAILS_REQUEST:
       return Object.assign({}, state, {
-        status: action.status,
+        cartDetailsStatus: action.status,
         loading: true
       });
 
     case cartActions.PRODUCT_CART_DETAILS_SUCCESS:
       return Object.assign({}, state, {
-        status: action.status,
+        cartDetailsStatus: action.status,
         cartDetails: action.cartDetails,
         loading: false
       });
 
     case cartActions.PRODUCT_CART_DETAILS_FAILURE:
       return Object.assign({}, state, {
-        status: action.status,
-        error: action.error,
+        cartDetailsStatus: action.status,
+        cartDetailsError: action.error,
         loading: false
       });
 
     case cartActions.USER_CART_REQUEST:
       return Object.assign({}, state, {
-        status: action.status,
+        userCartStatus: action.status,
         loading: true
       });
 
     case cartActions.USER_CART_SUCCESS:
       return Object.assign({}, state, {
-        status: action.status,
+        userCartStatus: action.status,
         userCart: action.userCart,
         loading: false
       });
 
     case cartActions.USER_CART_FAILURE:
       return Object.assign({}, state, {
-        status: action.status,
-        error: action.error,
+        userCartStatus: action.status,
+        userCartError: action.error,
         loading: false
       });
     case cartActions.PRODUCT_CART_REQUEST:
       return Object.assign({}, state, {
-        status: action.status,
+        cartDetailsStatus: action.status,
         loading: true
       });
 
     case cartActions.PRODUCT_CART_SUCCESS:
       return Object.assign({}, state, {
-        status: action.status,
+        cartDetailsStatus: action.status,
         cartDetails: action.cartDetails,
         loading: false
       });
 
     case cartActions.PRODUCT_CART_FAILURE:
       return Object.assign({}, state, {
-        status: action.status,
-        error: action.error,
+        cartDetailsStatus: action.status,
+        cartDetailsError: action.error,
         loading: false
       });
 
     case cartActions.APPLY_COUPON_REQUEST:
       return Object.assign({}, state, {
-        status: action.status,
+        couponStatus: action.status,
         loading: true
       });
 
     case cartActions.APPLY_COUPON_SUCCESS:
       return Object.assign({}, state, {
-        status: action.status,
+        couponStatus: action.status,
         loading: false
       });
 
     case cartActions.APPLY_COUPON_FAILURE:
       return Object.assign({}, state, {
-        status: action.status,
-        error: action.error,
+        couponStatus: action.status,
+        couponError: action.error,
         loading: false
       });
 
