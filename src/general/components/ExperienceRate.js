e6a61a7bfe848d6a1b5f870661d7945a18d3964e
gangeshkumar <gangesh@xelpmoc.in>
2018-02-16 13:48:34 +0530

    change class and functionality

--- src/general/components/ExperienceRate.js
+++ src/general/components/ExperienceRate.js
@@ -2,17 +2,51 @@ import React from "react";
 import styles from "./ExperienceRate.css";
 import { Icon } from "xelpmoc-core";
 import badIcon from "./img/bad.svg";
-
+import activeBadIcon from "./img/Bad_Red.svg";
+import PropTypes from "prop-types";
 export default class ExperienceRate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+  }
+  onSelectReting() {
+    console.log(this.props.val);
+    this.setState({ selected: !this.state.selected });
+  }
   render() {
+    let activeIcon = styles.hiddenIcon;
+    let hiddenIconM = styles.active;
+    if (this.state.selected) {
+      activeIcon = styles.active;
+    }
     return (
       <div className={styles.base}>
         <div className={styles.retingIconHolder}>
-          <div className={styles.iconSet}>
-            <Icon image={badIcon} size={36} />
+          <div className={styles.experienceRateingHolder}>
+            <div
+              className={styles.iconHolder}
+              onClick={() => this.onSelectReting()}
+            >
+              <div className={hiddenIconM}>
+                <Icon image={badIcon} size={36} />
+              </div>
+              <div className={activeIcon}>
+                <Icon image={activeBadIcon} size={36} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
+ExperienceRate.propTypes = {
+  placeholder: PropTypes.string,
+  placeHolderCardName: PropTypes.string,
+  buttonText: PropTypes.string,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+  onLoveChange: PropTypes.func
+};
