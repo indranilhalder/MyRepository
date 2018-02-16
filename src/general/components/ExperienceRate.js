3fbe1116ec240f89ac13fb11accbd7c7121cb3f9
gangeshkumar <gangesh@xelpmoc.in>
2018-02-16 14:16:09 +0530

    change class and functionality

--- src/general/components/ExperienceRate.js
+++ src/general/components/ExperienceRate.js
@@ -5,23 +5,22 @@ import badIcon from "./img/bad.svg";
 import activeBadIcon from "./img/Bad_Red.svg";
 import PropTypes from "prop-types";
 export default class ExperienceRate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: true
-    };
+  handleClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
   }
   render() {
     let activeIcon = styles.hiddenIcon;
     let hiddenIconM = styles.active;
-    if (this.state.selected) {
+    if (this.props.selected) {
       activeIcon = styles.active;
     }
     return (
       <div className={styles.base}>
         <div className={styles.retingIconHolder}>
           <div className={styles.experienceRateingHolder}>
-            <div className={styles.iconHolder} selected={true}>
+            <div className={styles.iconHolder} onClick={this.handleClick}>
               <div className={hiddenIconM}>
                 <Icon image={badIcon} size={36} />
               </div>
@@ -36,10 +35,13 @@ export default class ExperienceRate extends React.Component {
   }
 }
 ExperienceRate.propTypes = {
-  placeholder: PropTypes.string,
-  placeHolderCardName: PropTypes.string,
-  buttonText: PropTypes.string,
+  value: PropTypes.string,
   selected: PropTypes.bool,
   onClick: PropTypes.func,
-  onLoveChange: PropTypes.func
+  selectedItem: PropTypes.string
+};
+ExperienceRate.defaultProps = {
+  selected: true,
+  value: "bad",
+  selectedItem: "one"
 };
