917b1c92046cbb4cb804b570a15fabc3d89a7533
Prasad Reddy <prasad.v@techouts.com>
2018-02-18 11:12:39 +0530

    Egv Fico Changes

--- ext/marketplacecommerceservices/src/com/tisl/mpl/integration/job/SalesOrderXMLUtility.java
+++ ext/marketplacecommerceservices/src/com/tisl/mpl/integration/job/SalesOrderXMLUtility.java
@@ -791,7 +791,7 @@ public class SalesOrderXMLUtility
 	private double getTotalAmount(List<MerchantInfoXMlData> merchantInfoList, double tAmount)
 	{try{
 		for(MerchantInfoXMlData merchantInfoXMlTotal:merchantInfoList){
-			tAmount +=merchantInfoXMlTotal.getScheduleDelCharge()+merchantInfoXMlTotal.getShipmentCharge()+merchantInfoXMlTotal.getExpressDelCharge()+merchantInfoXMlTotal.getProductAmount();	
+			tAmount +=merchantInfoXMlTotal.getProductAmount();	
 		}
 		return tAmount;
 	}catch(Exception exception){
