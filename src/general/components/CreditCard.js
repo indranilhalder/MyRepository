da0ee57173f64c0108a981b2702f7336a82af440
gangeshkumar <gangesh@xelpmoc.in>
2018-02-19 15:38:54 +0530

    payment accordion

--- src/general/components/CreditCard.js
+++ src/general/components/CreditCard.js
@@ -60,34 +60,6 @@ export default class CreditCard extends React.Component {
   }
 
   render() {
-    const options = [
-      { value: "January", label: "January" },
-      { value: "February", label: "February" },
-      { value: "March", label: "March" },
-      { value: "April", label: "April" },
-      { value: "May", label: "May" },
-      { value: "June", label: "June" },
-      { value: "July", label: "July" },
-      { value: "August", label: "August" },
-      { value: "September", label: "September" },
-      { value: "October", label: "October" },
-      { value: "November", label: "November" },
-      { value: "December", label: "December" }
-    ];
-    const optionsYear = [
-      { value: "2018", label: "2018" },
-      { value: "2019", label: "2019" },
-      { value: "2020", label: "2020" },
-      { value: "2021", label: "2021" },
-      { value: "2022", label: "2022" },
-      { value: "2023", label: "2023" },
-      { value: "2024", label: "2024" },
-      { value: "2025", label: "2025" },
-      { value: "2026", label: "2026" },
-      { value: "2027", label: "2027" },
-      { value: "2028", label: "2028" },
-      { value: "2029", label: "2029" }
-    ];
     return (
       <div className={styles.base}>
         <div className={styles.cardDetails}>
@@ -124,7 +96,7 @@ export default class CreditCard extends React.Component {
             <div className={styles.dropDownBox}>
               <SelectBox
                 borderNone={true}
-                options={options}
+                options={this.props.options}
                 placeholder="Expiry month"
                 onChange={changedValue => this.monthChange(changedValue)}
                 selected={
@@ -138,7 +110,7 @@ export default class CreditCard extends React.Component {
             <div className={styles.dropDownBox}>
               <SelectBox
                 borderNone={true}
-                options={optionsYear}
+                options={this.props.optionsYear}
                 placeholder="Expiry year"
                 onChange={changedValue => this.onYearChange(changedValue)}
                 selected={
@@ -209,5 +181,7 @@ CreditCard.propTypes = {
   buttonText: PropTypes.string,
   selected: PropTypes.bool,
   onClick: PropTypes.func,
-  onSaveData: PropTypes.func
+  onSaveData: PropTypes.func,
+  optionsYear: PropTypes.string,
+  options: PropTypes.string
 };
