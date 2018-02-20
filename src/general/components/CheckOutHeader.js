e5ce0fad6597e5d783a571b0451f78823a4ebcbd
abi121 <abhishek@xelpmoc.in>
2018-02-20 16:37:06 +0530

    checkout header

--- src/general/components/CheckOutHeader.js
+++ src/general/components/CheckOutHeader.js
@@ -1,18 +1,19 @@
 import React from "react";
 import styles from "./CheckOutHeader.css";
+import PropTypes from "prop-types";
 export default class CheckOutHeader extends React.Component {
   render() {
     return (
       <div className={styles.base}>
         <div className={styles.confirm}>
           {this.props.confirmTitle}
-          <div className={styles.circle}>{this.props.orderNumber}</div>
+          <div className={styles.circle}>{this.props.indexNumber}</div>
         </div>
       </div>
     );
   }
 }
 
-CheckOutHeader.defaultProps = {
-  orderNumber: "1"
+CheckOutHeader.propTypes = {
+  indexNumber: PropTypes.string
 };
