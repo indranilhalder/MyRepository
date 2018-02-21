c216405da8d47c912b8bb332c5ea9aa49ff69d3c
ShitanshuRoy <shitanshuroy@gmail.com>
2018-02-21 18:50:23 +0530

    emi selection works

--- src/cart/components/EmiAccordian.js
+++ src/cart/components/EmiAccordian.js
@@ -5,8 +5,99 @@ import EmiCartSelect from "./EmiCartSelect";
 export default class EmiAccordian extends React.Component {
   render() {
     return (
-      <GridSelect elementWidthMobile={100}>
+      <GridSelect elementWidthMobile={100} offset={0}>
         <EmiCartSelect
+          value={1}
+          title="KOTAK MAHINDRA BANK, LTD."
+          options={[
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹23966.24",
+              term: "3",
+              interestPayable: "₹1760.71"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹12206.38",
+              term: "6",
+              interestPayable: "₹3100.30"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹8288.25",
+              term: "9",
+              interestPayable: "₹4456.22"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹6330.54",
+              term: "12",
+              interestPayable: "₹5828.45"
+            }
+          ]}
+        />
+        <EmiCartSelect
+          value={2}
+          title="KOTAK MAHINDRA BANK, LTD."
+          options={[
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹23966.24",
+              term: "3",
+              interestPayable: "₹1760.71"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹12206.38",
+              term: "6",
+              interestPayable: "₹3100.30"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹8288.25",
+              term: "9",
+              interestPayable: "₹4456.22"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹6330.54",
+              term: "12",
+              interestPayable: "₹5828.45"
+            }
+          ]}
+        />
+        <EmiCartSelect
+          value={3}
+          title="KOTAK MAHINDRA BANK, LTD."
+          options={[
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹23966.24",
+              term: "3",
+              interestPayable: "₹1760.71"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹12206.38",
+              term: "6",
+              interestPayable: "₹3100.30"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹8288.25",
+              term: "9",
+              interestPayable: "₹4456.22"
+            },
+            {
+              interestRate: "15.0",
+              monthlyInstallment: "₹6330.54",
+              term: "12",
+              interestPayable: "₹5828.45"
+            }
+          ]}
+        />
+        <EmiCartSelect
+          value={4}
           title="KOTAK MAHINDRA BANK, LTD."
           options={[
             {
--- src/cart/components/EmiCartSelect.js
+++ src/cart/components/EmiCartSelect.js
@@ -1,19 +1,30 @@
 import React from "react";
 import CheckBox from "../../general/components/CheckBox";
 import EmiCard from "../../pdp/components/EmiCard";
-import styles from "./EmiAccordian.css";
+import styles from "./EmiCartSelect.css";
 export default class EmiAccordian extends React.Component {
+  handleClick() {
+    if (this.props.selectItem) {
+      this.props.selectItem();
+    }
+  }
   render() {
     return (
       <div className={this.props.selected ? styles.baseSelected : styles.base}>
         <div className={styles.header}>
           {this.props.title}
-          <div className={styles.check}>
-            <CheckBox />
+          <div
+            className={styles.checkHolder}
+            onClick={() => this.handleClick()}
+          >
+            <CheckBox selected={this.props.selected} />
           </div>
         </div>
-        {/* {this.props.selected && <EmiCard options={this.props.options} />} */}
-        <EmiCard options={this.props.options} />
+        {this.props.selected && (
+          <div className={styles.options}>
+            <EmiCard options={this.props.options} />
+          </div>
+        )}
       </div>
     );
   }
