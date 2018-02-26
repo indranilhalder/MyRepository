f2efd5243e5293565cfb412d573e437b3b057006
Prakash Sanker <praks5432@gmail.com>
2018-02-26 17:31:33 +0530

    set content type to application/javascript

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.get("*.js", function(req, res, next) {
   req.url = req.url + ".gz";
   res.set("Content-Encoding", "gzip");
+  res.set("Content-Type", "application/javascript");
   next();
 });
 
