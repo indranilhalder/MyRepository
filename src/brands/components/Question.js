512f6ab912164ab6794f50c31114ae39bd12ada9
john2193 <janardan@xelpmoc.in>
2018-02-27 15:01:36 +0530

    rename file and add propstype

--- src/brands/components/Question.js
+++ src/brands/components/Question.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Question.css";
 import PropTypes from "prop-types";
 import GridSelect from "../../general/components/GridSelect";
-import ColorCheckbox from "../../general/components/ColorCheckbox";
+import CheckBoxPoint from "../../general/components/CheckBoxPoint";
 export default class Question extends React.Component {
   render() {
     const label = [{ label: "Yes, sure" }, { label: "No thanks" }];
@@ -12,7 +12,7 @@ export default class Question extends React.Component {
         <div className={styles.answerHolder}>
           <GridSelect limit={1} offset={0} elementWidthMobile={100}>
             {label.map((val, i) => {
-              return <ColorCheckbox label={val.label} value={i} />;
+              return <CheckBoxPoint label={val.label} value={i} />;
             })}
           </GridSelect>
         </div>
