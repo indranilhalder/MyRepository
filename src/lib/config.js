cd3ca25bab14f35f9272c8fde930d1b95b449b83
Prakash Sanker <praks5432@gmail.com>
2018-02-27 13:57:35 +0530

    login/signup fixes

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,7 @@
 export default {
-  facebook: "410493949364507",
+  facebook: "1444012285724567", // TODO change this based on env.
   google:
     "367761167032-apbr4v0nndom1cafs9inrrnkk7iag5be.apps.googleusercontent.com"
 };
+
+// 410493949364507
