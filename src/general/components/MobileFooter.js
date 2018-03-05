9282dd4a1d558ac292fd85f704f300cfb943bdcb
ShitanshuRoy <shitanshuroy@gmail.com>
2018-03-05 16:49:56 +0530

    mobile footer is completely controlled now

--- src/general/components/MobileFooter.js
+++ src/general/components/MobileFooter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./MobileFooter.css";
 import MobileFooterItem from "./MobileFooterItem";
-import withMultiSelect from "../../higherOrderComponents/withMultiSelect";
+import PropTypes from "prop-types";
 import homeIcon from "./img/home_icon.png";
 import categoriesIcon from "./img/categories_icon.png";
 import brandsIcon from "./img/brands_icon.png";
@@ -12,47 +12,54 @@ import categoriesIconRed from "./img/categories_icon_red.png";
 import brandsIconRed from "./img/brands_icon_red.png";
 import userIconRed from "./img/myCliq_icon_red.png";
 import myBagIconRed from "./img/myBag_icon_red.png";
-const MobileFooterBase = class MobileFooterBase extends React.Component {
-  render() {
-    return <div className={styles.base}>{this.props.children}</div>;
-  }
-};
-const MobileFooterWithSelect = withMultiSelect(MobileFooterBase);
+
 export default class MobileFooter extends React.Component {
   render() {
     return (
-      <MobileFooterWithSelect limit={1} selected={["home"]}>
+      <div className={styles.base}>
         <MobileFooterItem
           activeIcon={homeIconRed}
           basicIcon={homeIcon}
           value="home"
           text="Home"
+          selected={this.props.selected === "home" ? true : false}
         />
         <MobileFooterItem
           activeIcon={categoriesIconRed}
           basicIcon={categoriesIcon}
           value="categories"
           text="Categories"
+          selected={this.props.selected === "categories" ? true : false}
         />
         <MobileFooterItem
           activeIcon={brandsIconRed}
           basicIcon={brandsIcon}
           value="brands"
           text="Brands"
+          selected={this.props.selected === "brands" ? true : false}
         />
         <MobileFooterItem
           activeIcon={userIconRed}
           basicIcon={userIcon}
           value="profile"
           text="My Cliq"
+          selected={this.props.selected === "profile" ? true : false}
         />
         <MobileFooterItem
           activeIcon={myBagIconRed}
           basicIcon={myBagIcon}
           value="bag"
           text="My Bag"
+          selected={this.props.selected === "bag" ? true : false}
         />
-      </MobileFooterWithSelect>
+      </div>
     );
   }
 }
+
+MobileFooter.propTypes = {
+  selected: PropTypes.oneOf(["home", "categories", "brands", "profile", "bag"])
+};
+MobileFooter.defaultProps = {
+  selected: "home"
+};
