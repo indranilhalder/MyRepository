485ecafc522047ec51ce4fa9ed4b16beb2484751
SriramCGXelpmoc <sriram@xelpmoc.in>
2018-03-08 18:40:44 +0530

    Removed Sample Data

--- src/cart/components/CheckoutNetbanking.js
+++ src/cart/components/CheckoutNetbanking.js
@@ -11,38 +11,7 @@ export default class CheckoutNetBanking extends React.Component {
         <NetBanking
           onSelect={val => console.log(val)}
           selected={["1"]}
-          bankList={[
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/bank-of-1.gif",
-              value: "1"
-            },
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/CBicons_03.png",
-              value: "2"
-            },
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/UNITED.png",
-              value: "3"
-            },
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/UNION.png",
-              value: "4"
-            },
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/UNION.png",
-              value: "6"
-            },
-            {
-              image:
-                "https://competitiondigest.com/wp-content/uploads/2014/12/UNITED.png",
-              value: "7"
-            }
-          ]}
+          bankList={[]}
         />
       </ManueDetails>
     );
