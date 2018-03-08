bb27e4ffa2b9a826d96471632617a8fe7986ffe3
john2193 <janardan@xelpmoc.in>
2018-03-08 10:32:41 +0530

    followed brands

--- src/brands/components/BrandsToolTip.js
+++ src/brands/components/BrandsToolTip.js
@@ -1,52 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Icon } from "xelpmoc-core";
 import styles from "./BrandsToolTip.css";
-import { Icon, Image } from "xelpmoc-core";
+import deleteIcon from "../../general/components/img/delete.svg";
 export default class BrandsToolTip extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openPopup: false
-    };
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
-  }
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.setState({ openPopup: false });
-    }
-  }
-  setWrapperRef(node) {
-    this.wrapperRef = node;
-  }
-  handleClick() {
-    this.setState({ openPopup: !this.state.openPopup });
-  }
   render() {
-    let className = styles.toolTip;
-
+    let className = styles.delete;
     return (
-      <div
-        className={styles.base}
-        onClick={() => this.handleClick()}
-        ref={this.setWrapperRef}
-      >
-        <div className={styles.brandsIconHolder}>
-          <div
-            className={styles.logoHolder}
-            style={{
-              backgroundImage: `url(${this.props.logo})`,
-              backgroundSize: "80%",
-              backgroundPosition: "center"
-            }}
-          />
-        </div>
-        <div className={this.state.openPopup ? styles.openToolTip : className}>
-          <div className={styles.optionList}>Visit Brand</div>
-          <div className={styles.optionList}>Remove Brand</div>
+      <div className={styles.base}>
+        <div className={styles.iconAndToolTip}>
+          <div className={this.props.delete ? styles.onDelete : className}>
+            <Icon image={deleteIcon} size={18} />
+          </div>
+          <div className={styles.brandsIconHolder}>
+            <div
+              className={styles.logoHolder}
+              onClick={() => this.handleClick()}
+              style={{
+                backgroundImage: `url(${this.props.logo})`,
+                backgroundSize: "60%",
+                backgroundPosition: "center"
+              }}
+            />
+          </div>
         </div>
       </div>
     );
