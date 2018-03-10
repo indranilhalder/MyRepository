97b3824fee776857ba03795f16f3582ca40610fc
ShitanshuRoy <shitanshuroy@gmail.com>
2018-03-10 20:02:36 +0530

    fixed functions

--- src/plp/components/FilterMobile.js
+++ src/plp/components/FilterMobile.js
@@ -92,15 +92,12 @@ export default class FilterMobile extends React.Component {
         </div>
         <div className={styles.footer}>
           <div className={styles.buttonHolder}>
-            <div className={styles.button} onClick={this.onClear}>
+            <div className={styles.button} onClick={() => this.onClear()}>
               Clear
             </div>
           </div>
           <div className={styles.buttonHolder}>
-            <div
-              className={styles.redButton}
-              onClick={() => this.onApply(this.state.selected)}
-            >
+            <div className={styles.redButton} onClick={() => this.onApply()}>
               Apply
             </div>
           </div>
