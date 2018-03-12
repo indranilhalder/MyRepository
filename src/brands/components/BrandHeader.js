be7139cde640865b4f27b42f75ad28c49db701e8
InshaNazeer <insha@xelpmoc.in>
2018-03-12 17:33:04 +0530

    change in css

--- src/brands/components/BrandHeader.js
+++ src/brands/components/BrandHeader.js
@@ -5,7 +5,7 @@ import searchIcon from "./img/Search.svg";
 import iconImageURL from "../../general/components/img/arrowBack.svg";
 import searchRedIcon from "./img/SearchRed.svg";
 import PropTypes from "prop-types";
-import { CircleButton, Icon } from "xelpmoc-core";
+import { Icon } from "xelpmoc-core";
 import Input2 from "../../general/components/Input2.js";
 export default class BrandHeader extends React.Component {
   constructor(props) {
@@ -24,44 +24,49 @@ export default class BrandHeader extends React.Component {
       this.props.onSearch(val);
     }
   }
+  onClickIcon() {
+    if (this.state.searchBar) {
+      this.setState({ searchBar: false });
+    } else {
+      this.setState({ searchBar: true });
+    }
+  }
   render() {
+    let search = searchIcon;
+    if (this.state.searchBar) {
+      search = cancelIcon;
+    }
     return (
       <div className={styles.base}>
         <div className={styles.InformationHeader}>
           <div className={styles.backHolder} onClick={() => this.onClickBack()}>
-            <Icon image={iconImageURL} size={18} />
+            <Icon image={iconImageURL} size={16} />
+          </div>
+          <div
+            className={styles.searchHolder}
+            onClick={() => this.onClickIcon()}
+          >
+            <Icon image={search} size={16} />
           </div>
           {!this.state.searchBar && (
             <div className={styles.searchWithText}>
               <div className={styles.textBox}>{this.props.text}</div>
-              <div
-                className={styles.searchHolder}
-                onClick={() => this.setState({ searchBar: true })}
-              >
-                <Icon image={searchIcon} size={18} />
-              </div>
             </div>
           )}
           {this.state.searchBar && (
             <div className={styles.searchWithInputRedHolder}>
               <div className={styles.searchRedHolder}>
-                <Icon image={searchRedIcon} size={18} />
+                <Icon image={searchRedIcon} size={16} />
               </div>
               <div className={styles.input}>
                 <Input2
                   onChange={val => this.onSearch(val)}
                   textStyle={{ fontSize: 14 }}
-                  height={33}
+                  height={30}
                   isWhite={true}
                   borderColor={"#212121"}
                   borderBottom={"0px solid #212121"}
                 />
-              </div>
-              <div
-                className={styles.crossIcon}
-                onClick={() => this.setState({ searchBar: false })}
-              >
-                <Icon image={cancelIcon} size={18} />
               </div>
             </div>
           )}
