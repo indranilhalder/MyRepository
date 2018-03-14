fc4a4499fb33c18faa984aab9d0db911c9083e5d
InshaNazeer <insha@xelpmoc.in>
2018-03-14 12:00:11 +0530

    break two components

--- src/account/components/OrderReturnAddressDetails.js
+++ src/account/components/OrderReturnAddressDetails.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./OrderReturnAddressDetails.css";
 import PropTypes from "prop-types";
-import UnderLinedButton from "../../general/components/UnderLinedButton";
 export default class OrderReturnAddressDetails extends React.Component {
   onCancel() {
     if (this.props.onClick) {
@@ -20,43 +19,12 @@ export default class OrderReturnAddressDetails extends React.Component {
         {this.props.subAddress && (
           <div className={styles.address}>{this.props.subAddress}</div>
         )}
-        {this.props.date && (
-          <div className={styles.dateAndTimeHolder}>
-            {this.props.date && (
-              <div className={styles.dateHolder}>
-                <div className={styles.date}>Date</div>
-                <div className={styles.dateInfo}>{this.props.date}</div>
-              </div>
-            )}
-            {this.props.time && (
-              <div className={styles.timeHolder}>
-                <div className={styles.time}>Time</div>
-                <div className={styles.timeInfo}>{this.props.time}</div>
-              </div>
-            )}
-          </div>
-        )}
-        {this.props.underlineButtonLabel && (
-          <div className={styles.cancelHolder}>
-            <div className={styles.cancel} onClick={() => this.onCancel()}>
-              <UnderLinedButton
-                label={this.props.underlineButtonLabel}
-                color={this.props.underlineButtonColour}
-              />
-            </div>
-          </div>
-        )}
       </div>
     );
   }
 }
 OrderReturnAddressDetails.propTypes = {
   address: PropTypes.string,
-  underlineButtonLabel: PropTypes.string,
-  underlineButtonColour: PropTypes.string,
-  time: PropTypes.string,
-  date: PropTypes.string,
   subAddress: PropTypes.string,
-  addressType: PropTypes.string,
-  onCancel: PropTypes.func
+  addressType: PropTypes.string
 };
---
+++ src/account/components/OrderReturnDateAndTimeDetails.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./OrderReturnDateAndTimeDetails.css";
+import PropTypes from "prop-types";
+import UnderLinedButton from "../../general/components/UnderLinedButton";
+export default class OrderReturnDateAndTimeDetails extends React.Component {
+  onCancel() {
+    if (this.props.onCancel) {
+      this.props.onCancel();
+    }
+  }
+  render() {
+    return (
+      <div className={styles.base}>
+        {this.props.date && (
+          <div className={styles.dateAndTimeHolder}>
+            {this.props.date && (
+              <div className={styles.dateHolder}>
+                <div className={styles.date}>Date</div>
+                <div className={styles.dateInfo}>{this.props.date}</div>
+              </div>
+            )}
+            {this.props.time && (
+              <div className={styles.timeHolder}>
+                <div className={styles.time}>Time</div>
+                <div className={styles.timeInfo}>{this.props.time}</div>
+              </div>
+            )}
+          </div>
+        )}
+        {this.props.underlineButtonLabel && (
+          <div className={styles.cancelHolder}>
+            <div className={styles.cancel} onClick={() => this.onCancel()}>
+              <UnderLinedButton
+                label={this.props.underlineButtonLabel}
+                color={this.props.underlineButtonColour}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+OrderReturnDateAndTimeDetails.propTypes = {
+  underlineButtonLabel: PropTypes.string,
+  underlineButtonColour: PropTypes.string,
+  time: PropTypes.string,
+  date: PropTypes.string,
+  onCancel: PropTypes.func
+};
