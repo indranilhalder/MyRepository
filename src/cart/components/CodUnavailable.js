0f8f2875172ecbdfd550bedcb37ad596d6aceff2
unknown <shitanshuroy@gmail.com>
2018-03-15 22:46:25 +0530

    added icon

---
+++ src/cart/components/CodUnavailable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icon } from "xelpmoc-core";
+import styles from "./CodUnavailable.css";
+export default class CodUnavailable extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.icon} />
+      </div>
+    );
+  }
+}
