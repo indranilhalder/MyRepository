6f82555fd2203069676054aa1a4d86f395216b71
ShitanshuRoy <shitanshuroy@gmail.com>
2018-03-16 10:23:47 +0530

    added fixes

--- src/cart/components/CodUnavailable.js
+++ src/cart/components/CodUnavailable.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Icon } from "xelpmoc-core";
-import chashIcon from "./img/cod.svg";
+import cashIcon from "./img/cod.svg";
+import PropTypes from "prop-types";
+import arrowIcon from "../../general/components/img/down-arrow.svg";
 import styles from "./CodUnavailable.css";
 export default class CodUnavailable extends React.Component {
   render() {
     return (
       <div className={styles.base}>
         <div className={styles.icon}>
-          <Icon image={chashIcon} size={25} />
+          <Icon image={cashIcon} size={30} />
         </div>
         <div className={styles.head}>Cash On Delivery</div>
         <div className={styles.message}>({this.props.message})</div>
         <div className={styles.arrow}>
-          <Icon image={chashIcon} size={25} />
+          <Icon image={arrowIcon} size={12} />
         </div>
       </div>
     );
@@ -21,4 +23,7 @@ export default class CodUnavailable extends React.Component {
 
 CodUnavailable.defaultProps = {
   message: "Not allowed for CiQ and PiQ option"
+};
+CodUnavailable.propTyps = {
+  message: PropTypes.string
 };
