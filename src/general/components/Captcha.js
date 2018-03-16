2afc569133324ce6eeac9320181f0f8d33c80ad7
SriramCGXelpmoc <sriram@xelpmoc.in>
2018-03-16 18:57:52 +0530

    Captcha handling

--- src/general/components/Captcha.js
+++ src/general/components/Captcha.js
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 const LOCALE_ENGLISH = "en";
 const THEME = "light";
 const CASH_ON_DELIVERY = "COD";
+const env = process.env;
+
 class Captcha extends Component {
   verifyCallback = response => {
     if (response) {
@@ -17,7 +19,7 @@ class Captcha extends Component {
   render() {
     return (
       <Recaptcha
-        sitekey={Config.reCaptChaSiteKey}
+        sitekey={env.REACT_APP_RECAPTCHA_SITE_KEY}
         callback={this.verifyCallback}
         locale={LOCALE_ENGLISH}
         data-theme={THEME}
