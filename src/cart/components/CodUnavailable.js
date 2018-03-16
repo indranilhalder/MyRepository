94b828ae05df00af1d7ed583ba7c61217cfd5dd1
unknown <shitanshuroy@gmail.com>
2018-03-16 08:34:26 +0530

    added icon

--- src/cart/components/CodUnavailable.js
+++ src/cart/components/CodUnavailable.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Icon } from "xelpmoc-core";
+import chashIcon from "./img/cod.svg";
 import styles from "./CodUnavailable.css";
 export default class CodUnavailable extends React.Component {
   render() {
     return (
       <div className={styles.base}>
-        <div className={styles.icon} />
+        <div className={styles.icon}>
+          <Icon image={chashIcon} size={25} />
+        </div>
+        <div className={styles.head}>Cash On Delivery</div>
+        <div className={styles.message}>({this.props.message})</div>
+        <div className={styles.arrow}>
+          <Icon image={chashIcon} size={25} />
+        </div>
       </div>
     );
   }
 }
+
+CodUnavailable.defaultProps = {
+  message: "Not allowed for CiQ and PiQ option"
+};
