f87afad51604c219a3166155a72b4811fce7a9f7
InshaNazeer <insha@xelpmoc.in>
2018-03-17 11:12:03 +0530

    makeSizeAndQuantityPopUp

---
+++ src/pdp/components/SizeAndQuantityPopUp.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./SizeAndQuantityPopUp.css";
+import PropTypes from "prop-types";
+export default class SizeAndQuantityPopUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gender: props.gender ? props.gender : ""
+    };
+  }
+  onChange(val) {
+    if (this.props.onChange) {
+      this.props.onChange(val);
+    }
+  }
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.textHolder}>
+          <div className={styles.text}>{this.props.data}</div>
+        </div>
+      </div>
+    );
+  }
+}
+SizeAndQuantityPopUp.propTypes = {
+  data: PropTypes.string
+};
+SizeAndQuantityPopUp.defaultProps = {
+  data: "Please select a size & quantity to continue"
+};
