0cb527698f756c60581d44c174eb5c2dbdb675a2
Basavannavva <Basavannevva@xelpmoc.in>
2018-03-17 10:52:46 +0530

    Chages done

--- src/auth/reducers/user.reducer.js
+++ src/auth/reducers/user.reducer.js
@@ -25,7 +25,6 @@ const user = (
       });
 
     case userActions.LOGIN_USER_SUCCESS:
-      console.log(action.user);
       let userDetails = {};
       if (action.user.customerInfo.emailId) {
         userDetails.userName = action.user.customerInfo.emailId;
@@ -37,7 +36,6 @@ const user = (
       userDetails.firstName = action.user.customerInfo.firstName;
       userDetails.gender = action.user.customerInfo.gender;
       userDetails.lastName = action.user.customerInfo.lastName;
-      console.log(userDetails);
 
       Cookies.createCookie(LOGGED_IN_USER_DETAILS, JSON.stringify(userDetails));
       return Object.assign({}, state, {
