116ed22bc2ada9c423cf9b9c4049b9e7ff167c0c
InshaNazeer <insha@xelpmoc.in>
2018-03-20 12:39:24 +0530

    modification done

--- src/account/actions/account.actions.js
+++ src/account/actions/account.actions.js
@@ -2,8 +2,7 @@ import { SUCCESS, REQUESTING, ERROR } from "../../lib/constants";
 import * as Cookie from "../../lib/Cookie";
 import {
   CUSTOMER_ACCESS_TOKEN,
-  LOGGED_IN_USER_DETAILS,
-  FAILURE_UPPERCASE
+  LOGGED_IN_USER_DETAILS
 } from "../../lib/constants";
 export const USER_CART_PATH = "v2/mpl/users";
 
@@ -17,6 +16,7 @@ export const GET_WISHLIST_FAILURE = "GET_WISHLIST_FAILURE";
 
 export const CURRENT_PAGE = 0;
 export const PAGE_SIZE = 10;
+export const PLATFORM_NUMBER = 2;
 
 export function getAllOrdersRequest() {
   return {
@@ -97,13 +97,13 @@ export function getWishList() {
           JSON.parse(userDetails).userName
         }/getAllWishlist?access_token=${
           JSON.parse(customerCookie).access_token
-        }&isPwa=true&platformNumber=2`
+        }&isPwa=true&platformNumber=${PLATFORM_NUMBER}`
       );
       const resultJson = await result.json();
-      if (resultJson.status === FAILURE_UPPERCASE) {
-        throw new Error(resultJson.error);
+      if (resultJson.errors) {
+        throw new Error(`${resultJson.errors[0].message}`);
       }
-      dispatch(getWishlistSuccess(resultJson.wishList[0]));
+      dispatch(getWishlistSuccess(resultJson.wishList[0])); //we sre getting response wishlit[0]
     } catch (e) {
       dispatch(getWishlistFailure(e.message));
     }
