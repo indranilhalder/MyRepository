5a568b5ba9c460703379a80520e36ded3424bbb9
y-aakarsh <aakarsh@xelpmoc.in>
2018-03-20 16:54:19 +0530

    Adding PR changs in

--- src/account/components/UserCoupons.js
+++ src/account/components/UserCoupons.js
@@ -14,10 +14,9 @@ export default class UserCoupons extends React.Component {
           userCoupons.unusedCouponsList.map(coupon => (
             <div className={styles.cardHolder}>
               <MyCoupons
-                heading="2 Rs. Voucher"
+                heading={coupon.description}
                 image="../../general/components/img/coupon-1.svg"
                 couponNumber={coupon.couponCode}
-                label={coupon.description}
                 maxRedemption="Max Redemption:"
                 maxRedemptionValue={coupon.redemptionQtyLimitPerUser}
                 creationDate="Creation Date"
