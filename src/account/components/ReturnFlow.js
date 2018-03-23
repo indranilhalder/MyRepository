66d420f9afb4cd45d6013faf13b27d41e011cb53
y-aakarsh <aakarsh@xelpmoc.in>
2018-03-23 19:32:13 +0530

    Adding Return flow files

---
+++ src/account/components/ReturnFlow.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import ReturnReasonForm from "./ReturnReasonForm.js";
+import ReturnModes from "./ReturnModes.js";
+import {
+  RETURNS,
+  RETURNS_REASON,
+  RETURNS_MODES,
+  RETURNS_STORE_MAP,
+  RETURNS_STORE_BANK_FORM,
+  RETURNS_STORE_FINAL,
+  RETURN_TO_STORE
+} from "../../lib/constants";
+export default class ReturnFlow extends React.Component {
+  componentDidMount() {
+    this.props.returnProductDetails();
+    this.props.getReturnRequest();
+  }
+
+  renderReturnReason = () => {
+    return (
+      <div>
+        <ReturnReasonForm onContinue={() => this.addReasonForReturn()} />
+      </div>
+    );
+  };
+
+  goToReturnMode = val => {};
+  renderReturnModes = () => {
+    return (
+      <div>
+        <ReturnModes selectMode={val => this.goToReturnMode(val)} />
+      </div>
+    );
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <React.Fragment>
+        <Route
+          exact
+          path={`${RETURNS}${RETURNS_REASON}`}
+          component={ReturnReasonForm}
+        />
+        <Route
+          exact
+          path={`${RETURNS}${RETURNS_MODES}`}
+          component={ReturnModes}
+        />
+        {/* need to call return bia store pick up  routes change component according to route */}
+        <Route
+          exact
+          path={`${RETURNS}${RETURN_TO_STORE}${RETURNS_STORE_MAP}`}
+          component={ReturnReasonForm}
+        />
+        <Route
+          exact
+          path={`${RETURNS}${RETURN_TO_STORE}${RETURNS_STORE_BANK_FORM}`}
+          component={ReturnReasonForm}
+        />
+        <Route
+          exact
+          path={`${RETURNS}${RETURN_TO_STORE}${RETURNS_STORE_FINAL}`}
+          component={ReturnReasonForm}
+        />
+        {/* end of need to call return bia store pick up  routes */}
+      </React.Fragment>
+    );
+  }
+}
