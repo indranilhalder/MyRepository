a4139cf456cb48cce5081d63fa2c59a6c4c1f86d
ShitanshuRoy <shitanshuroy@gmail.com>
2018-03-27 15:27:45 +0530

    clean up

--- src/pdp/components/PdpElectronics.js
+++ src/pdp/components/PdpElectronics.js
@@ -178,7 +178,6 @@ export default class PdpElectronics extends React.Component {
     );
   }
   render() {
-    console.log(this.props);
     const productData = this.props.productDetails;
     const mobileGalleryImages = productData.galleryImagesList
       .map(galleryImageList => {
