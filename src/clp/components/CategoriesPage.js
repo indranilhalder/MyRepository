b329a8ca0493230304a2f3e1f38f0343d64ab58c
ShitanshuRoy <shitanshuroy@gmail.com>
2018-03-27 11:39:23 +0530

    fixed web url

--- src/clp/components/CategoriesPage.js
+++ src/clp/components/CategoriesPage.js
@@ -21,7 +21,8 @@ export default class CategoriesPage extends React.Component {
     this.props.history.push(urlSuffix);
   }
   handleViewAll(webURL) {
-    this.props.history.replace(webURL);
+    const urlSuffix = webURL.replace(TATA_CLIQ_ROOT, "$1");
+    this.props.history.push(urlSuffix);
   }
   handleOpenL1(val) {
     this.setState({ openIndex: val });
