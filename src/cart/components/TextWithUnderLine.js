a19d994a5c1ee6afbadf1eb43c67a1acc3f09824
InshaNazeer <insha@xelpmoc.in>
2018-03-30 19:28:20 +0530

    makeTextWithUnderLine

---
+++ src/cart/components/TextWithUnderLine.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./TextWithUnderLine.css";
+import PropTypes from "prop-types";
+import UnderLinedButton from "../../general/components/UnderLinedButton";
+
+export default class TextWithUnderLine extends React.Component {
+  onClick() {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.headingText}>{this.props.heading}</div>
+        <div className={styles.button} onClick={() => this.onClick()}>
+          <UnderLinedButton label={this.props.buttonLabel} />
+        </div>
+      </div>
+    );
+  }
+}
+TextWithUnderLine.propTypes = {
+  onClick: PropTypes.func,
+  buttonLabel: PropTypes.string,
+  heading: PropTypes.string
+};
