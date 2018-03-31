a20232b41a05fbc0b73b8800896da1398d8756aa
Prakash Sanker <praks5432@gmail.com>
2018-03-31 16:12:41 +0530

    remove console log

--- src/home/components/ProductCapsules.js
+++ src/home/components/ProductCapsules.js
@@ -40,7 +40,6 @@ export default class ProductCapsules extends React.Component {
 
     const productCapsulesData = this.props.feedComponentData;
     const data = this.props.feedComponentData.data;
-    console.log(data);
     if (!data) {
       return null;
     }
