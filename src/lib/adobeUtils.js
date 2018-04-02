4a8398810db929d1be818f47bba259782be9502c
Prakash Sanker <praks5432@gmail.com>
2018-04-02 15:35:59 +0530

    add DTM for home page

--- src/lib/adobeUtils.js
+++ src/lib/adobeUtils.js
@@ -1,10 +1,34 @@
 import { getCookieValue } from "./Cookie.js";
 import { setInterval, clearInterval } from "timers";
+import * as constants from "../lib/constants.js";
+
 export const ADOBE_TARGET_COOKIE_NAME =
   "AMCV_E9174ABF55BA76BA7F000101%40AdobeOrg";
 export const ADOBE_TARGET_SPLIT_VALUE = "%7C";
 export const ADOBE_TARGET_MCMID = "MCMID";
 export const ADOBE_TARGET_WAIT_TIME = 2000;
+
+export function setDataLayer(routerProps) {
+  const path = routerProps.path;
+  if (path === constants.HOME_ROUTER) {
+    window.digitalData = getDigitalDataForHome();
+  }
+}
+
+function getDigitalDataForHome() {
+  return {
+    page: {
+      category: {
+        primaryCategory: "homepage"
+      },
+      pageInfo: {
+        pageName: "homepage",
+        subDomain: "www",
+        domain: ""
+      }
+    }
+  };
+}
 
 export async function getMcvId() {
   return new Promise((resolve, reject) => {
