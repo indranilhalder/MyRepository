77e279eae8379200a92ae3a304716f77e614a71c
Basavannavva <Basavannevva@xelpmoc.in>
2018-04-03 11:43:39 +0530

    Removed console

--- src/cart/components/CartPage.js
+++ src/cart/components/CartPage.js
@@ -214,7 +214,6 @@ class CartPage extends React.Component {
     this.setState({ changePinCode: true });
   };
   render() {
-    console.log(this.props.cart.appliedCouponResult);
     const globalAccessToken = Cookie.getCookie(GLOBAL_ACCESS_TOKEN);
     const cartDetailsForAnonymous = Cookie.getCookie(
       CART_DETAILS_FOR_ANONYMOUS
