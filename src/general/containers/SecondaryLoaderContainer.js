a9ded8e94a2059edecd9907877aa2d889ebe772f
ShitanshuRoy <shitanshuroy@gmail.com>
2018-04-04 21:20:43 +0530

    clean up

--- src/general/containers/SecondaryLoaderContainer.js
+++ src/general/containers/SecondaryLoaderContainer.js
@@ -2,15 +2,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import SecondaryLoader from "../components/SecondaryLoader";
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     setHeaderText: text => {
-//       dispatch(setHeaderText(text));
-//     }
-//   };
-// };
 const mapStateToProps = state => {
-  //console.log(state);
   return {
     display: state.secondaryLoader.display
   };
