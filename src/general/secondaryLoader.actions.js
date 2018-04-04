037ab46b2e5a1c3084fe0f2b0b0aba58c029dce3
ShitanshuRoy <shitanshuroy@gmail.com>
2018-04-04 21:07:30 +0530

    clean up

--- src/general/secondaryLoader.actions.js
+++ src/general/secondaryLoader.actions.js
@@ -1,14 +1,12 @@
 export const SHOW_LOADER = "SHOW_LOADER";
 export const HIDE_LOADER = "HIDE_LOADER";
 export function showSecondaryLoader() {
-  console.log("show called");
   return {
     type: SHOW_LOADER
   };
 }
 
 export function hideSecondaryLoader() {
-  console.log("hide called");
   return {
     type: HIDE_LOADER
   };
