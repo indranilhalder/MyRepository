019a8b68416e2d012e0b83c0f605329cf05701cd
john2193 <janardan@xelpmoc.in>
2018-04-07 17:29:56 +0530

    order card

---
+++ src/cart/components/OrderDetailsCard.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+import OrderPlacedAndId from "../../account/components/OrderPlacedAndId";
+import OrderCard from "../../account/components/OrderCard";
+import PriceAndLink from "../../account/components/PriceAndLink";
+import styles from "./OrderDetailsCard.css";
+export default class OrderDetailsCard extends React.Component {
+  render() {
+    return (
+      <div className={styles.base}>
+        <div className={styles.orderIdHolder}>
+          <OrderPlacedAndId orderId={this.props.orderId} />
+        </div>
+        <OrderCard
+          imageUrl={this.props.imageUrl}
+          productName={this.props.productName}
+          price={this.props.price}
+          discountPrice={this.props.discountPrice}
+        >
+          <div className={styles.quantityHolder}>
+            <div className={styles.quantityLabel}>Qty :</div>
+            <div className={styles.quantityAmount}>{this.props.quantity}</div>
+          </div>
+        </OrderCard>
+        <PriceAndLink
+          onViewDetails={() => this.onViewDetails()}
+          price={this.props.totalPrice}
+        />
+        <div className={styles.informationDataHolder}>
+          {this.props.deliveredAddress && (
+            <div className={styles.addressHolder}>
+              <div className={styles.deliveredTo}>Delivery Address: </div>
+              <div className={styles.address}>
+                {this.props.deliveredAddress}
+              </div>
+            </div>
+          )}
+          {this.props.standardDelivery && (
+            <div className={styles.addressHolder}>
+              <div className={styles.deliveredTo}>Standard Delivery: </div>
+              <div className={styles.address}>
+                {this.props.standardDelivery}
+              </div>
+            </div>
+          )}
+          {this.props.standardDelivery && (
+            <div className={styles.orderSoldBy}>
+              <div className={styles.labelText}>Sold by:</div>
+              <div className={styles.infoText}>{this.props.soldBy}</div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+OrderDetailsCard.propTypes = {
+  imageUrl: PropTypes.string,
+  productName: PropTypes.string,
+  price: PropTypes.string,
+  discountPrice: PropTypes.string,
+  quantity: PropTypes.string,
+  totalPrice: PropTypes.string,
+  deliveredAddress: PropTypes.string,
+  standardDelivery: PropTypes.string,
+  soldBy: PropTypes.string
+};
