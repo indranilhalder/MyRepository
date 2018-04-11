336f23b11765385a2c1d7612c2ed1467d3062249
ShitanshuRoy <shitanshuroy@gamil.com>
2018-04-11 14:34:27 +0530

    text change

--- src/plp/components/FilterMobile.js
+++ src/plp/components/FilterMobile.js
@@ -165,7 +165,7 @@ export default class FilterMobile extends React.Component {
                   {facetData[this.state.filterSelectedIndex].key === BRAND && (
                     <div className={styles.search}>
                       <SearchInput
-                        placeholder="Search brands"
+                        placeholder="Search by brands"
                         onChange={val => this.onBrandSearch(val)}
                       />
                     </div>
@@ -192,7 +192,7 @@ export default class FilterMobile extends React.Component {
         >
           <div className={styles.buttonHolder}>
             <div className={styles.button} onClick={() => this.onClear()}>
-              Clear
+              Reset
             </div>
           </div>
           <div className={styles.buttonHolder}>
