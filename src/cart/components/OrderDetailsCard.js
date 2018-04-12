76c8aaa6bbde80fc8e454d0d439893bffb6bafd3
Basavannavva <Basavannevva@xelpmoc.in>
2018-04-12 18:01:50 +0530

    Image Display Issue

--- src/cart/components/OrderDetailsCard.js
+++ src/cart/components/OrderDetailsCard.js
@@ -22,7 +22,7 @@ export default class OrderDetailsCard extends React.Component {
           <OrderPlacedAndId orderId={this.props.orderId} />
         </div>
         <OrderCard
-          imageUrl={this.props.productDetails.imageUrl}
+          imageUrl={this.props.productDetails.imageURL}
           productName={this.props.productDetails.productName}
           price={this.props.productDetails.pricevalue}
           discountPrice=""
