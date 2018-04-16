7b0050d08ff2654ac415740e7fe16fc2c3318fcd
InshaNazeer <insha@xelpmoc.in>
2018-04-16 18:08:32 +0530

    solve content issue

--- src/home/components/ContentCard.js
+++ src/home/components/ContentCard.js
@@ -21,12 +21,7 @@ export default class ContentCard extends React.Component {
         <div className={styles.content}>
           <div className={styles.overlay}>
             <div className={styles.header}>{this.props.header}</div>
-            <div
-              className={styles.label}
-              onClick={() => {
-                this.handleClick();
-              }}
-            >
+            <div className={styles.label}>
               {this.props.description}
               <div className={styles.buttonBox}>
                 <div className={styles.button}>
