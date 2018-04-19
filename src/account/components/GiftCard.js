775829eb35b10ec90a90d0cc53a329eef538e147
InshaNazeer <insha@xelpmoc.in>
2018-04-19 13:59:27 +0530

    solve issues

--- src/account/components/GiftCard.js
+++ src/account/components/GiftCard.js
@@ -78,12 +78,10 @@ export default class GiftCard extends React.Component {
           return false;
         }
         if (
-          !(
-            this.state.amountText <=
-              this.props.giftCardsDetails.amountOptions.maxPrice.value &&
-            this.state.amountText >=
-              this.props.giftCardsDetails.amountOptions.minPrice.value
-          )
+          this.state.amountText >=
+            this.props.giftCardsDetails.amountOptions.maxPrice.value ||
+          this.state.amountText <=
+            this.props.giftCardsDetails.amountOptions.minPrice.value
         ) {
           this.props.displayToast(
             `Amount Should be less then ${
