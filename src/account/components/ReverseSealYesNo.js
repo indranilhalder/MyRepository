3b5796496877fecf4c3c1eaf298c1b3f82245756
john2193 <janardan@xelpmoc.in>
2018-04-20 15:05:01 +0530

    reverse seal yes no

---
+++ src/account/components/ReverseSealYesNo.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "./ReverseSealYesNo.css";
+import UnderLinedButton from "../../general/components/UnderLinedButton.js";
+import GridSelect from "../../general/components/GridSelect";
+import YesNoQuestion from "./YesNoQuestion";
+export default class ReverseSealYesNo extends React.Component {
+  render() {
+    const options = [
+      {
+        confirmation: "Yes",
+        text: "I have the seal"
+      },
+      {
+        confirmation: "No",
+        text: "I don't have the seal"
+      }
+    ];
+    return (
+      <div className={styles.base}>
+        <div className={styles.headerText}>
+          Do You have the reverse seal with you?
+        </div>
+        <div className={styles.moreInfoButtonHolder}>
+          <div className={styles.button}>
+            <UnderLinedButton
+              size="14px"
+              fontFamily="regular"
+              color="#000"
+              label="More Info"
+              onClick={() => this.moreInfo()}
+            />
+          </div>
+        </div>
+        <div className={styles.yesNoQuestionHolder}>
+          {options &&
+            options.length > 0 && (
+              <GridSelect
+                limit={1}
+                offset={0}
+                elementWidthMobile={100}
+                selected={[]}
+              >
+                {options.map((val, i) => {
+                  return (
+                    <YesNoQuestion
+                      text={val.text}
+                      confirmation={val.confirmation}
+                      value={i}
+                    />
+                  );
+                })}
+              </GridSelect>
+            )}
+        </div>
+      </div>
+    );
+  }
+}
