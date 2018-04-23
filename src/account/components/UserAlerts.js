d843845199f85985dc110c1357293745dd8ecece
john2193 <janardan@xelpmoc.in>
2018-04-23 15:39:15 +0530

    remove console

--- src/account/components/UserAlerts.js
+++ src/account/components/UserAlerts.js
@@ -5,7 +5,6 @@ import ShippingCommenced from "../../blp/components/ShippingCommenced";
 export default class UserAlerts extends React.Component {
   render() {
     const { userAlerts } = this.props;
-    console.log(this.props);
     return (
       <div className={styles.base}>
         {userAlerts &&
