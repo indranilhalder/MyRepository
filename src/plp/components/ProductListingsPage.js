064047ab5348488be2fdd37800f7d612db24b6a7
Prakash Sanker <praks5432@gmail.com>
2018-04-24 17:46:54 -0400

    deal with no query for category page with product listings

--- src/plp/components/ProductListingsPage.js
+++ src/plp/components/ProductListingsPage.js
@@ -27,6 +27,14 @@ class ProductListingsPage extends Component {
       searchText = parsedQueryString.text;
     }
 
+    if (this.props.match.path === CATEGORY_PRODUCT_LISTINGS_WITH_PAGE) {
+      // I might have a q or no q.
+      if (!searchText) {
+        // if there are query params
+        searchText = `:relevance:category:${this.props.match.params[0].toUpperCase()}`;
+      }
+    }
+
     return encodeURIComponent(searchText);
   }
 
@@ -52,7 +60,7 @@ class ProductListingsPage extends Component {
     let page = null;
     if (this.props.match.path === CATEGORY_PRODUCT_LISTINGS_WITH_PAGE) {
       page = this.props.match.params[1];
-      const searchText = this.getSearchTextFromUrl();
+      let searchText = this.getSearchTextFromUrl();
       this.props.getProductListings(searchText, SUFFIX, page - 1);
       return;
     }
@@ -60,6 +68,9 @@ class ProductListingsPage extends Component {
     if (this.props.match.path === BRAND_AND_CATEGORY_PAGE) {
       const categoryId = this.props.match.params[0];
       const brandId = this.props.match.params[1];
+      // what if I have a brand and category page with a sort already there?
+      // I might have a sort
+
       const searchText = `:relevance:category:${categoryId}:brand:${brandId}`;
       this.props.getProductListings(searchText, SUFFIX, 0, false);
       return;
@@ -103,6 +114,17 @@ class ProductListingsPage extends Component {
 
       const searchText = this.getSearchTextFromUrl();
       this.props.getProductListings(searchText, SUFFIX, page);
+      return;
+    }
+
+    if (this.props.match.path === BRAND_AND_CATEGORY_PAGE) {
+      const categoryId = this.props.match.params[0];
+      const brandId = this.props.match.params[1];
+      // what if I have a brand and category page with a sort already there?
+      // I might have a sort
+
+      const searchText = `:relevance:category:${categoryId}:brand:${brandId}`;
+      this.props.getProductListings(searchText, SUFFIX, 0, false);
       return;
     }
     if (
