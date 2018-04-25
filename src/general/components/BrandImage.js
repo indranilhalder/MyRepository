a91b6695c32720340a66f3680bed70fd23ddca25
InshaNazeer <insha@xelpmoc.in>
2018-04-25 12:39:41 +0530

    solve Issues

--- src/general/components/BrandImage.js
+++ src/general/components/BrandImage.js
@@ -14,11 +14,19 @@ export default class BrandImage extends React.Component {
       iconClass = styles.checkIconActive;
     }
     return (
-      <div className={styles.base} onClick={() => this.handleClick()}>
-        <div className={iconClass} />
-        <div className={styles.imageHolder}>
-          <Image image={this.props.image} color="transparent" fit="contain" />
-        </div>
+      <div>
+        {this.props.isFollowing === "true" && (
+          <div className={styles.base} onClick={() => this.handleClick()}>
+            <div className={iconClass} />
+            <div className={styles.imageHolder}>
+              <Image
+                image={this.props.image}
+                color="transparent"
+                fit="contain"
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
