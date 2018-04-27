a06fa9ca18f7091c4341695b9b7021dd5f567237
ShitanshuRoy <shitanshuroy@gamil.com>
2018-04-27 19:10:46 +0530

    faded the seller card better

--- src/pdp/components/SellerCard.js
+++ src/pdp/components/SellerCard.js
@@ -18,46 +18,49 @@ export default class SellerCard extends React.Component {
     }
 
     return (
-      <div
-        className={this.props.disabled ? styles.faded : styles.base}
-        onClick={() => this.handleClick(this.props)}
-      >
+      <div className={styles.base} onClick={() => this.handleClick(this.props)}>
         <div className={styles.textBox}>
-          <div className={styles.heading}>
-            {this.props.heading}
-            {!this.props.disabled && (
-              <span className={styles.checkCircle}>
-                <CheckBox selected={this.props.selected} />
-              </span>
+          <div className={this.props.disabled ? styles.faded : styles.buffer}>
+            <div className={styles.heading}>
+              {this.props.heading}
+              {!this.props.disabled && (
+                <span className={styles.checkCircle}>
+                  <CheckBox selected={this.props.selected} />
+                </span>
+              )}
+            </div>
+            <div className={styles.priceTitle}>{this.props.priceTitle}:</div>
+            {this.props.discountPrice &&
+              this.props.discountPrice !== this.props.price && (
+                <div className={styles.discount}>
+                  {this.props.discountPrice}
+                </div>
+              )}
+            {this.props.price && (
+              <span className={priceClass}>{this.props.price}</span>
+            )}
+            {this.props.hasEmi && (
+              <div className={styles.offerText}>EMI Available</div>
             )}
           </div>
-          <div className={styles.priceTitle}>{this.props.priceTitle}:</div>
-          {this.props.discountPrice &&
-            this.props.discountPrice !== this.props.price && (
-              <div className={styles.discount}>{this.props.discountPrice}</div>
-            )}
-          {this.props.price && (
-            <span className={priceClass}>{this.props.price}</span>
-          )}
-          {this.props.hasEmi && (
-            <div className={styles.offerText}>EMI Available</div>
-          )}
         </div>
         <div className={styles.textBox2}>
-          <div className={styles.priceTitle}>{this.props.deliveryText}</div>
-          {this.props.eligibleDeliveryModes &&
-            this.props.eligibleDeliveryModes.map((val, i) => {
-              return (
-                <div className={styles.shippingText}>
-                  {val.name}
-                  {val.description && <span>-</span>}
-                  {val.description}
-                </div>
-              );
-            })}
-          {this.props.hasCod && (
-            <div className={styles.offerText}>{this.props.cashText}</div>
-          )}
+          <div className={this.props.disabled ? styles.faded : styles.buffer}>
+            <div className={styles.priceTitle}>{this.props.deliveryText}</div>
+            {this.props.eligibleDeliveryModes &&
+              this.props.eligibleDeliveryModes.map((val, i) => {
+                return (
+                  <div className={styles.shippingText}>
+                    {val.name}
+                    {val.description && <span>-</span>}
+                    {val.description}
+                  </div>
+                );
+              })}
+            {this.props.hasCod && (
+              <div className={styles.offerText}>{this.props.cashText}</div>
+            )}
+          </div>
         </div>
       </div>
     );
