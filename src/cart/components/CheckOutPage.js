2076a63050d1f02ed2c2102850d1ecfdee5ca5e0
Basavannavva <Basavannevva@xelpmoc.in>
2018-05-03 15:57:52 +0530

    Chages done

--- src/cart/components/CheckOutPage.js
+++ src/cart/components/CheckOutPage.js
@@ -354,7 +354,9 @@ class CheckOutPage extends React.Component {
               </div>
             );
           })}
+           <DummyTab title="Payment Method" number={3} />
       </div>
+
     );
   };
 
@@ -744,12 +746,12 @@ class CheckOutPage extends React.Component {
           nextProps.cart.cartDetailsCNC.cartAmount.couponDiscountAmount &&
           nextProps.cart.cartDetailsCNC.cartAmount.couponDiscountAmount.value
         ) {
-          this.setState(
+          this.setState({couponDiscount:
             Math.round(
               nextProps.cart.cartDetailsCNC.cartAmount.couponDiscountAmount
                 .value * 100
             ) / 100
-          );
+           } );
         }
       }
     }
