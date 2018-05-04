2530b567d0f3a3838777408a8f59df80c0faf105
john2193 <janardan@xelpmoc.in>
2018-05-04 18:40:08 +0530

    bugs for 2797

--- src/home/components/FollowingBrands.js
+++ src/home/components/FollowingBrands.js
@@ -1,13 +1,15 @@
 import React from "react";
 import BrandImage from "../../general/components/BrandImage";
-import Carousel from "../../general/components/Carousel";
+import DumbCarousel from "../../general/components/DumbCarousel";
 import styles from "./FollowingBrands.css";
 import PropTypes from "prop-types";
 import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
+import plusButton from "../../general/components/img/plusbutton.svg";
 import * as Cookie from "../../lib/Cookie";
 import {
   CUSTOMER_ACCESS_TOKEN,
-  LOGGED_IN_USER_DETAILS
+  LOGGED_IN_USER_DETAILS,
+  DEFAULT_BRANDS_LANDING_PAGE
 } from "../../lib/constants";
 export default class FollowingBrands extends React.Component {
   newFollow = () => {
@@ -33,28 +35,46 @@ export default class FollowingBrands extends React.Component {
     const followWidgetData = this.props.feedComponentData;
     const userDetails = Cookie.getCookie(LOGGED_IN_USER_DETAILS);
     const customerCookie = Cookie.getCookie(CUSTOMER_ACCESS_TOKEN);
+
+    if (followWidgetData.data && followWidgetData.data) {
+      let gotoMoreBands = followWidgetData.data;
+
+      if (gotoMoreBands) {
+        gotoMoreBands.push({
+          imageURL: plusButton,
+          isFollowing: "true",
+          fit: "1",
+          webURL: DEFAULT_BRANDS_LANDING_PAGE
+        });
+      }
+    }
+
     return (
       <div className={styles.base}>
         {userDetails &&
           customerCookie && (
-            <Carousel
+            <DumbCarousel
               header={this.props.feedComponentData.title}
-              elementWidthMobile={30}
+              elementWidth={30}
             >
               {followWidgetData.data &&
-                followWidgetData.data.map((datum, i) => {
-                  return (
-                    <BrandImage
-                      key={i}
-                      image={datum.imageURL}
-                      value={datum.webURL}
-                      fit={datum.type}
-                      isFollowing={datum.isFollowing}
-                      onClick={this.handleBrandImageClick}
-                    />
-                  );
-                })}
-            </Carousel>
+                followWidgetData.data
+                  .filter(val => {
+                    return val.isFollowing === "true";
+                  })
+                  .map((datum, i) => {
+                    return (
+                      <BrandImage
+                        key={i}
+                        image={datum.imageURL}
+                        value={datum.webURL}
+                        fit={datum.fit}
+                        isFollowing={datum.isFollowing}
+                        onClick={this.handleBrandImageClick}
+                      />
+                    );
+                  })}
+            </DumbCarousel>
           )}
       </div>
     );
