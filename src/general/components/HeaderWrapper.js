9a1b17c369b74b7aafcfab9f26e1c8503cc89ed9
john2193 <janardan@xelpmoc.in>
2018-05-08 13:19:12 +0530

    remove const file

--- src/general/components/HeaderWrapper.js
+++ src/general/components/HeaderWrapper.js
@@ -24,8 +24,7 @@ import {
   JUS_PAY_PENDING,
   JUS_PAY_AUTHENTICATION_FAILED,
   CHECKOUT_ROUTER,
-  CHECKOUT_ROUTER_THANKYOU,
-  MY_ACCOUNT_PAGE_ROOT
+  CHECKOUT_ROUTER_THANKYOU
 } from "../../../src/lib/constants";
 import { SIGN_UP } from "../../auth/actions/user.actions";
 
