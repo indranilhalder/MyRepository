1bea7a400eb0fe8f400573eb8b5388db98994729
ShitanshuRoy <shitanshuroy@gmail.com>
2018-05-11 17:42:51 +0530

    more clean up

--- src/home/components/StoryModal.js
+++ src/home/components/StoryModal.js
@@ -1,9 +1,7 @@
 import React from "react";
 import StoryWidget from "./StoryWidget";
 import StoryProduct from "./StoryProduct";
-// import StoryWidgetContainer from "../containers/StoryWidgetContainer";
 import styles from "./StoryModal.css";
-
 export default class StoryModal extends React.Component {
   componentDidMount() {
     this.props.getItems(this.props.positionInFeed, this.props.itemIds);
