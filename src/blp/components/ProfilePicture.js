7630016fd025d076c78b884746e69a1407baf860
InshaNazeer <insha@xelpmoc.in>
2018-05-15 18:34:24 +0530

    solve undefined issue

--- src/blp/components/ProfilePicture.js
+++ src/blp/components/ProfilePicture.js
@@ -21,17 +21,15 @@ export default class ProfilePicture extends React.Component {
             >
               <Icon image={editIcon} size={18} />
             </div>
-            {this.props.firstName &&
-              this.props.lastName && (
-                <ProfileImage
-                  image={this.props.profileImageLink}
-                  size={3}
-                  initials={
-                    this.props.firstName.charAt(0) +
-                    this.props.lastName.charAt(0)
-                  }
-                />
-              )}
+            {(this.props.firstName || this.props.lastName) && (
+              <ProfileImage
+                image={this.props.profileImageLink}
+                size={3}
+                initials={
+                  this.props.firstName.charAt(0) + this.props.lastName.charAt(0)
+                }
+              />
+            )}
           </div>
           {!this.props.edit && (
             <div className={styles.nameOfProfileHolder}>
