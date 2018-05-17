a1e96e0e4684c025c95b9326501c3ededa8bb222
Basavannavva <Basavannevva@xelpmoc.in>
2018-05-17 16:39:07 +0530

    Chages done

--- src/cart/components/CheckOutPage.js
+++ src/cart/components/CheckOutPage.js
@@ -758,8 +758,8 @@ class CheckOutPage extends React.Component {
       this.setState({
         isRemainingAmount:
           nextProps.cart.cliqCashPaymentDetails.isRemainingAmount,
-        payableAmount: nextProps.cart.cliqCashPaymentDetails.paybleAmount
-          ? nextProps.cart.cliqCashPaymentDetails.paybleAmount
+        payableAmount: nextProps.cart.cartDetailsCNC.cartAmount.paybleAmount.value
+          ? nextProps.cart.cartDetailsCNC.cartAmount.paybleAmount.value
           : "0.00",
         cliqCashAmount:
           nextProps.cart.cliqCashPaymentDetails.cliqCashBalance.value > 0
@@ -768,13 +768,13 @@ class CheckOutPage extends React.Component {
                   100
               ) / 100
             : "0.00",
-        bagAmount: nextProps.cart.cliqCashPaymentDetails.totalAmount
-          ? nextProps.cart.cliqCashPaymentDetails.totalAmount
+        bagAmount: nextProps.cart.cartDetailsCNC.cartAmount.bagTotal.value
+          ? nextProps.cart.cartDetailsCNC.cartAmount.bagTotal.value
           : "0.00",
         totalDiscount:
-          nextProps.cart.cliqCashPaymentDetails.otherDiscount.value > 0
+        nextProps.cart.cartDetailsCNC.cartAmount.totalDiscountAmount.value > 0
             ? Math.round(
-                nextProps.cart.cliqCashPaymentDetails.otherDiscount.value * 100
+              nextProps.cart.cartDetailsCNC.cartAmount.totalDiscountAmount.value * 100
               ) / 100
             : "0.00",
         cliqCashPaidAmount:
