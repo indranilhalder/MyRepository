d91c8111eb69d10937ab4eea52d678cd3fdb02bb
InshaNazeer <insha@xelpmoc.in>
2018-05-17 10:49:08 +0530

    solveGrossWeightIssue

--- src/pdp/components/PriceBreakupModal.js
+++ src/pdp/components/PriceBreakupModal.js
@@ -12,13 +12,18 @@ export default class PriceBreakupModal extends React.Component {
             {this.props.data.map(val => {
               return (
                 <div className={styles.row}>
-                  <div className={styles.name}>{val.name}</div>
-                  {val.price && (
-                    <div className={styles.price}>
-                      {val.price.formattedValue}
-                    </div>
-                  )}
+                  {val.name &&
+                    val.name !== "Gross Weight" && (
+                      <div className={styles.name}>{val.name}</div>
+                    )}
+                  {val.price &&
+                    val.name !== "Gross Weight" && (
+                      <div className={styles.price}>
+                        {val.price.formattedValue}
+                      </div>
+                    )}
                   {val.weightRateList &&
+                    val.name !== "Gross Weight" &&
                     val.weightRateList.length > 0 &&
                     val.weightRateList.map(val => {
                       return <div className={styles.detail}>{val}</div>;
