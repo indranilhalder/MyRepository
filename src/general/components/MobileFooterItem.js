c248cce19df7d232c68d419e61ae5e9d3f900ebe
john2193 <janardan@xelpmoc.in>
2018-05-19 17:05:18 +0530

    condition added

--- src/general/components/MobileFooterItem.js
+++ src/general/components/MobileFooterItem.js
@@ -11,7 +11,7 @@ export default class MobileFooterItem extends React.Component {
   render() {
     return (
       <div className={styles.base} onClick={() => this.handleSelect()}>
-        {this.props.bagCount && (
+        {this.props.bagCount > 0 && (
           <div className={styles.count}>{this.props.bagCount}</div>
         )}
 
