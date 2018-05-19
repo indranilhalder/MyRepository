8eacab2e86779e4332111d1288666a75f72731f9
Basavannavva <Basavannevva@xelpmoc.in>
2018-05-19 17:24:44 +0530

    Added condition

--- src/account/components/ReturnAddressList.js
+++ src/account/components/ReturnAddressList.js
@@ -125,11 +125,12 @@ export default class ReturnAddressList extends React.Component {
     );
   };
   renderAddress = () => {
-    let defaultAddress = this.props.returnRequest.deliveryAddressesList.find(
-      address => {
+    let defaultAddress =
+      this.props.returnRequest &&
+      this.props.returnRequest.deliveryAddressesList &&
+      this.props.returnRequest.deliveryAddressesList.find(address => {
         return address.defaultAddress === true;
-      }
-    );
+      });
     if (this.props.returnRequest) {
       return (
         <ReturnsFrame
