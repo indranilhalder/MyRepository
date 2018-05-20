15ce86e211e9564977fc9cfb4bec1df96e275cdb
y-aakarsh <aakarsh@xelpmoc.in>
2018-05-20 15:01:37 +0530

    Adding hcnages for midle ware url

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -34,7 +34,7 @@ if (
   MIDDLEWARE_API_URL_ROOT = "https://app.tatacliq.com/marketplacewebservices";
 } else if (process.env.REACT_APP_STAGE === "p2") {
   API_URL_ROOT = "https://p2.tatacliq.com/marketplacewebservices";
-  MIDDLEWARE_API_URL_ROOT = "https://p2app.tatacliq.com/marketplacewebservices";
+  MIDDLEWARE_API_URL_ROOT = "https://p2.tatacliq.com/marketplacewebservices";
 } else if (process.env.REACT_APP_STAGE === "stage") {
   API_URL_ROOT = "https://stg.tatacliq.com/marketplacewebservices";
   MIDDLEWARE_API_URL_ROOT = "https://stg.tatacliq.com/marketplacewebservices";
