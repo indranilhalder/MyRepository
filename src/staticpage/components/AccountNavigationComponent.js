a043f21d758950cee37e7abfd0ccb6c16a9683b7
InshaNazeer <insha@xelpmoc.in>
2018-05-21 18:05:46 +0530

    solve dropdownIssue

--- src/staticpage/components/AccountNavigationComponent.js
+++ src/staticpage/components/AccountNavigationComponent.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import SelectBoxMobile2 from "../../general/components/SelectBoxMobile2";
 import styles from "./AccountNavigationComponent.css";
 import { TATA_CLIQ_ROOT } from "../../lib/apiRequest.js";
-
 export default class AccountNavigationComponent extends Component {
   handleItemClick = url => {
     const urlSuffix = url.value.replace(TATA_CLIQ_ROOT, "$1");
     this.props.history.push(urlSuffix);
   };
   render() {
+    let particularPage;
+    this.props.feedComponentData.nodeList.forEach(status => {
+      if (status.url.trim() === this.props.location.pathname.trim()) {
+        particularPage = status.linkName;
+      }
+    });
     return (
       <div className={styles.navigationHolder}>
         <SelectBoxMobile2
-          placeholder={"Select"}
-          value={this.props.location.pathname.replace("/", "")}
+          value={particularPage}
           label={this.props.feedComponentData.nodeList.linkName}
           height={40}
           options={this.props.feedComponentData.nodeList.map((val, i) => {
